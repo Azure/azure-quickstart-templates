768124f3914b7aca8ca32e9360bf09db95ed58df
Trent Swanson <trentmswanson@hotmail.com>
2015-04-26 19:52:32 -0700

    updated script to enable ssl in opscenter

--- datastax-on-ubuntu/opscenter.sh
+++ datastax-on-ubuntu/opscenter.sh
@@ -122,6 +122,11 @@ apt-get install opscenter
 # Enable authentication in /etc/opscenter/opscenterd.conf
 sed -i '/^\[authentication\]$/,/^\[/ s/^enabled = False/enabled = True/' /etc/opscenter/opscenterd.conf
 
+# Enable SSL - uncomment webserver SSL settings and leave them set to the default
+sed -i '/^\[webserver\]$/,/^\[/ s/^#ssl_keyfile/ssl_keyfile/' /etc/opscenter/opscenterd.conf
+sed -i '/^\[webserver\]$/,/^\[/ s/^#ssl_certfile/ssl_certfile/' /etc/opscenter/opscenterd.conf
+sed -i '/^\[webserver\]$/,/^\[/ s/^#ssl_port/ssl_port/' /etc/opscenter/opscenterd.conf
+
 # Start Ops Center
 sudo service opscenterd start
 
