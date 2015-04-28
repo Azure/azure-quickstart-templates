cfe3ef7dbd5b442530cfad56fa178d82311382b4
Kay Singh <kasing@microsoft.com>
2015-04-28 12:42:36 -0700

    Revert "Changed method for installing private key in cloudera-on-centos template"

--- cloudera-on-centos/scripts/initialize-node.sh
+++ cloudera-on-centos/scripts/initialize-node.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-ADMINUSER=$1
-
 # Disable the need for a tty when running sudo
 sed -i '/Defaults[[:space:]]\+!*requiretty/s/^/#/' /etc/sudoers
 
 # Mount and format the attached disks
-#sh ./prepareDisks.sh
-
-#use the key from the key vault as the SSH authorized key
-mkdir /home/$ADMINUSER/.ssh
-chown $ADMINUSER /home/$ADMINUSER/.ssh
-chmod 700 /home/$ADMINUSER/.ssh
-
-ssh-keygen -y -f /var/lib/waagent/*.prv > /home/$ADMINUSER/.ssh/authorized_keys
-chown $ADMINUSER /home/$ADMINUSER/.ssh/authorized_keys
-chmod 600 /home/$ADMINUSER/.ssh/authorized_keys
-
-#disable password authentication in ssh
-sed -i "s/UsePAM\s*yes/UsePAM no/" /etc/ssh/sshd_config
-sed -i "s/PasswordAuthentication\s*yes/PasswordAuthentication no/" /etc/ssh/sshd_config
-/etc/init.d/sshd restart
+sh ./prepareDisks.sh
