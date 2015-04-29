c415384bdfb8f6b010f1c6690c3faeb2e51bc350
Ercenk Keresteci <ercenk@ercenk.com>
2015-04-28 20:58:43 -0700

    added licenses

--- couchbase-on-ubuntu/couchbase-azure-install.sh
+++ couchbase-on-ubuntu/couchbase-azure-install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-### Ercenk Keresteci (Full Scale 180 Inc)
+### Licensed under the Apache License, Version 2.0 (the "License");
+### you may not use this file except in compliance with the License.
+### You may obtain a copy of the License at
+###   http://www.apache.org/licenses/LICENSE-2.0
+### 
+### Unless required by applicable law or agreed to in writing, software
+### distributed under the License is distributed on an "AS IS" BASIS,
+### WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+### 
+### See the License for the specific language governing permissions and
+### limitations under the License.
+
 ### 
 ### Warning! This script partitions and formats disk information be careful where you run it
 ###          This script is currently under development and has only been tested on Ubuntu images in Azure
 ###          This script is not currently idempotent and only works for provisioning
+###
 
 # Log method to control/redirect log output
 log()
