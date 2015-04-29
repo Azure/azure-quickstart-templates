92ac40bf95b5617162b41e6ee082d96f47c3ad55
Trent Swanson <trentmswanson@hotmail.com>
2015-04-28 21:02:55 -0700

    added license to script header

--- datastax-enterprise/dsenode.sh
+++ datastax-enterprise/dsenode.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-#########################################################
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Script Name: dsenode.sh
 # Author: Trent Swanson - Full Scale 180 Inc github:(trentmswanson)
 # Version: 0.1
@@ -10,8 +21,9 @@
 # Parameters :
 # Note : 
 # This script has only been tested on Ubuntu 14.04 LTS and must be root
-######################################################### 
 
+
+# Partitiong and format all attached data disks
 bash vm-disk-utils-0.1.sh
 
 # TEMP FIX - Re-evaluate and remove when possible
--- datastax-enterprise/opscenter.sh
+++ datastax-enterprise/opscenter.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-#########################################################
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Script Name: opscenter.sh
 # Author: Trent Swanson - Full Scale 180 Inc github:(trentmswanson)
 # Version: 0.1
@@ -19,7 +30,6 @@
 #  10- h  Help 
 # Note : 
 # This script has only been tested on Ubuntu 12.04 LTS and must be root
-######################################################### 
 
 help()
 {
