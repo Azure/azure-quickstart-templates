26374566e77a6f22517514d79250adb9e87424fe
Catherine Wang <cawa@microsoft.com>
2015-04-29 17:51:26 -0700

    Revising PowerShell DSC to not use Web PI to install windows features

--- web-app-dsc-vm/ConfigureWebServer.ps1
+++ web-app-dsc-vm/ConfigureWebServer.ps1
@@ -13,6 +13,213 @@ Configuration Main
 
   Node ($MachineName)
   {
+	   
+     WindowsFeature WebServerRole
+
+        {
+
+           
+
+            Name = "Web-Server"
+
+            Ensure = "Present"
+
+
+            }
+
+	 
+        WindowsFeature WebAppDev
+
+        {
+
+            Name = "Web-App-Dev"
+
+            Ensure = "Present"
+
+
+            DependsOn = "[WindowsFeature]WebServerRole"
+
+            }
+
+	   WindowsFeature WebAspNet45
+
+        {
+
+            Name = "Web-Asp-Net45"
+
+            Ensure = "Present"
+
+            Source = $Source
+
+            DependsOn = "[WindowsFeature]WebServerRole"
+
+            }
+
+        WindowsFeature WebNetExt35
+
+        {
+
+            Name = "Web-Net-Ext"
+
+            Ensure = "Present"
+
+            DependsOn = "[WindowsFeature]WebServerRole"
+
+            }
+
+	   WindowsFeature WebNetExt45
+
+        {
+
+            Name = "Web-Net-Ext45"
+
+            Ensure = "Present"
+
+            DependsOn = "[WindowsFeature]WebServerRole"
+
+            }
+
+	  WindowsFeature WebFtpServer
+	  {
+		Name = "Web-Ftp-Server"
+
+		Ensure = "Present"
+
+        DependsOn = "[WindowsFeature]WebServerRole"
+	  
+	  }
+
+	  WindowsFeature WebMgmtCompat
+	  {
+		Name = "Web-Mgmt-Compat"
+
+		Ensure = "Present"
+
+        DependsOn = "[WindowsFeature]WebServerRole"
+	  
+	  }
+
+        WindowsFeature WebISAPIExt
+
+        {
+
+            Name = "Web-ISAPI-Ext"
+
+            Ensure = "Present"
+
+
+            DependsOn = "[WindowsFeature]WebServerRole"
+
+            }
+
+        WindowsFeature WebISAPIFilter
+
+        {
+
+            Name = "Web-ISAPI-Filter"
+
+            Ensure = "Present"
+
+ 
+            DependsOn = "[WindowsFeature]WebServerRole"
+
+            }
+
+        WindowsFeature WebLogLibraries
+
+        {
+
+            Name = "Web-Log-Libraries"
+
+            Ensure = "Present"
+
+            DependsOn = "[WindowsFeature]WebServerRole"
+
+            }
+
+        WindowsFeature WebRequestMonitor
+
+        {
+
+            Name = "Web-Request-Monitor"
+
+            Ensure = "Present"
+
+            DependsOn = "[WindowsFeature]WebServerRole"
+
+            }
+
+        WindowsFeature WebMgmtTools
+
+        {
+
+            Name = "Web-Mgmt-Tools"
+
+            Ensure = "Present"
+
+            DependsOn = "[WindowsFeature]WebServerRole"
+
+            }
+
+        WindowsFeature WebMgmtConsole
+
+        {
+
+            Name = "Web-Mgmt-Console"
+
+            Ensure = "Present"
+
+            DependsOn = "[WindowsFeature]WebServerRole"
+
+            }
+
+	  WindowsFeature WAS
+
+        {
+
+            Name = "WAS"
+
+            Ensure = "Present"
+
+            DependsOn = "[WindowsFeature]WebServerRole"
+
+            }
+
+	  WindowsFeature WASProcessModel
+
+        {
+
+            Name = "WAS-Process-Model"
+
+            Ensure = "Present"
+
+            DependsOn = "[WindowsFeature]WebServerRole"
+
+            }
+
+	   WindowsFeature WASNetEnvironment
+
+        {
+
+            Name = "WAS-NET-Environment"
+
+            Ensure = "Present"
+
+            DependsOn = "[WindowsFeature]WebServerRole"
+
+            }
+
+	  WindowsFeature WASConfigAPIs
+
+        {
+
+            Name = "WAS-Config-APIs"
+
+            Ensure = "Present"
+
+            DependsOn = "[WindowsFeature]WebServerRole"
+
+            }
 
    #script block to download WebPI MSI from the Azure storage blob
     Script DownloadWebPIImage
@@ -48,7 +255,8 @@ Configuration Main
             Name = "Microsoft Web Deploy 3.5"
             Path = "$env:ProgramFiles\Microsoft\Web Platform Installer\WebPiCmd-x64.exe"
             ProductId = ''
-            Arguments = "/install /products:ASPNET45,ASPNET_REGIIS_NET4,DefaultDocument,DirectoryBrowse,HTTPErrors,HTTPLogging,IISManagementConsole,ISAPIExtensions,ISAPIFilters,ManagementService,NETFramework452,NETFramework4Update402,NetFx4,NetFx4Extended-ASPNET45,NetFxExtensibility45,RequestFiltering,SMO,StaticContent,StaticContentCompression,WASConfigurationAPI,WASProcessModel,WDeploy,WDeployNoSMO  /AcceptEula"
+            #Arguments = "/install /products:ASPNET45,ASPNET_REGIIS_NET4,NETFramework452,NETFramework4Update402,NetFx4,NetFx4Extended-ASPNET45,NetFxExtensibility45,DefaultDocument,DirectoryBrowse,StaticContent,StaticContentCompression,WDeploy  /AcceptEula"
+			Arguments = "/install /products:WDeploy  /AcceptEula"
 			DependsOn = @("[Package]WebPi_Installation")
         }
 	
@@ -85,4 +293,5 @@ Configuration Main
 
     
   }
-} 
+
+}
