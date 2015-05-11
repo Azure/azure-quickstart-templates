52ae07125685b72866bd5ae7da7a4317f8ef5536
Ivan Fioravanti <ivan.fioravanti@4ward.it>
2015-05-11 11:45:13 +0200

    Added kernel settings for THP management

--- mongodb-on-ubuntu/mongo-install-ubuntu.sh
+++ mongodb-on-ubuntu/mongo-install-ubuntu.sh
@@ -2,6 +2,11 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 
+# Disable THP
+sudo echo never > /sys/kernel/mm/transparent_hugepage/enabled
+sudo echo never > /sys/kernel/mm/transparent_hugepage/defrag
+sudo grep -q -F 'transparent_hugepage=never' /etc/default/grub || echo 'transparent_hugepage=never' >> /etc/default/grub
+
 # Install updates
 sudo apt-get -y update
 
