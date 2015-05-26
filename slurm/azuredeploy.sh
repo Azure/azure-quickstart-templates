2c36ec12c155348c8849e3ee16f29d4f8009f1bd
Yiding Zhou <yidingz@microsoft.com>
2015-05-25 18:52:03 -0700

    Fix script bugs

--- slurm/azuredeploy.sh
+++ slurm/azuredeploy.sh
@@ -6,13 +6,13 @@
 # This script will install SLURM on a Linux cluster deployed on a set of Azure VMs
 
 # Basic info
-date > /tmp/dummy 2>&1
-whoami >> /tmp/dummy 2>&1
-echo $@ >> /tmp/dummy 2>&1
+date > /tmp/azuredeploy.log.$$ 2>&1
+whoami >> /tmp/azuredeploy.log.$$ 2>&1
+echo $@ >> /tmp/azuredeploy.log.$$ 2>&1
 
 # Usage
 if [ "$#" -ne 9 ]; then
-  echo "Usage: $0 MASTER_NAME MASTER_IP WORKER_NAME WORKER_IP_BASE WORKER_IP_START NUM_OF_VM ADMIN_USERNAME ADMIN_PASSWORD TEMPLATE_BASE" >> /tmp/dummy
+  echo "Usage: $0 MASTER_NAME MASTER_IP WORKER_NAME WORKER_IP_BASE WORKER_IP_START NUM_OF_VM ADMIN_USERNAME ADMIN_PASSWORD TEMPLATE_BASE" >> /tmp/azuredeploy.log.$$
   exit 1
 fi
 
@@ -43,7 +43,7 @@ if ! [ -f /home/$ADMIN_USERNAME/.ssh/id_rsa ]; then
 fi
 
 # Install sshpass to automate ssh-copy-id action
-sudo apt-get install sshpass -y >> /tmp/dummy 2>&1
+sudo apt-get install sshpass -y >> /tmp/azuredeploy.log.$$ 2>&1
 
 # Loop through all worker nodes, update hosts file and copy ssh public key to it
 # The script make the assumption that the node is called %WORKER+<index> and have
@@ -52,7 +52,7 @@ i=0
 while [ $i -lt $NUM_OF_VM ]
 do
    workerip=`expr $i + $WORKER_IP_START`
-   echo 'I update host - '$WORKER_NAME$i >> /tmp/dummy 2>&1
+   echo 'I update host - '$WORKER_NAME$i >> /tmp/azuredeploy.log.$$ 2>&1
    echo $WORKER_IP_BASE$workerip $WORKER_NAME$i >> /etc/hosts
    sudo -u $ADMIN_USERNAME sh -c "sshpass -p '$ADMIN_PASSWORD' ssh-copy-id $WORKER_NAME$i"
    i=`expr $i + 1`
@@ -62,44 +62,44 @@ done
 ###################################
 
 # Install the package
-sudo apt-get update >> /tmp/dummy 2>&1
-sudo chmod g-w /var/log >> /tmp/dummy 2>&1 # Must do this before munge will generate key
-sudo apt-get install slurm-llnl -y >> /tmp/dummy 2>&1
+sudo apt-get update >> /tmp/azuredeploy.log.$$ 2>&1
+sudo chmod g-w /var/log >> /tmp/azuredeploy.log.$$ 2>&1 # Must do this before munge will generate key
+sudo apt-get install slurm-llnl -y >> /tmp/azuredeploy.log.$$ 2>&1
 
 # Download slurm.conf and fill in the node info
 SLURMCONF=/tmp/slurm.conf.$$
-wget $TEMPLATE_BASE/slurm.template.conf -O $SLURMCONF >> /tmp/dummy 2>&1
-sed -i -- 's/__MASTERNODE__/'"$MASTER_NAME"'/g' $SLURMCONF >> /tmp/dummy 2>&1
+wget $TEMPLATE_BASE/slurm.template.conf -O $SLURMCONF >> /tmp/azuredeploy.log.$$ 2>&1
+sed -i -- 's/__MASTERNODE__/'"$MASTER_NAME"'/g' $SLURMCONF >> /tmp/azuredeploy.log.$$ 2>&1
 lastvm=`expr $NUM_OF_VM - 1`
-sed -i -- 's/__WORKERNODES__/'"$WORKER_NAME"'[0-'"$lastvm"']/g' $SLURMCONF >> /tmp/dummy 2>&1
-sudo cp -f $SLURMCONF /etc/slurm-llnl/slurm.conf >> /tmp/dummy 2>&1
-sudo chown slurm /etc/slurm-llnl/slurm.conf >> /tmp/dummy 2>&1
+sed -i -- 's/__WORKERNODES__/'"$WORKER_NAME"'[0-'"$lastvm"']/g' $SLURMCONF >> /tmp/azuredeploy.log.$$ 2>&1
+sudo cp -f $SLURMCONF /etc/slurm-llnl/slurm.conf >> /tmp/azuredeploy.log.$$ 2>&1
+sudo chown slurm /etc/slurm-llnl/slurm.conf >> /tmp/azuredeploy.log.$$ 2>&1
 sudo chmod o+w /var/spool # Write access for slurmctld log. Consider switch log file to another location
-sudo -u slurm /usr/sbin/slurmctld >> /tmp/dummy 2>&1 # Start the master daemon service
-sudo munged --force >> /tmp/dummy 2>&1 # Start munged
-sudo slurmd >> /tmp/dummy 2>&1 # Start the node
+sudo -u slurm /usr/sbin/slurmctld >> /tmp/azuredeploy.log.$$ 2>&1 # Start the master daemon service
+sudo munged --force >> /tmp/azuredeploy.log.$$ 2>&1 # Start munged
+sudo slurmd >> /tmp/azuredeploy.log.$$ 2>&1 # Start the node
 
 # Install slurm on all nodes by running apt-get
 # Also push munge key and slurm.conf to them
-echo "Prepare the local copy of munge key" >> /tmp/dummy 2>&1 
+echo "Prepare the local copy of munge key" >> /tmp/azuredeploy.log.$$ 2>&1 
 
 mungekey=/tmp/munge.key.$$
 sudo cp -f /etc/munge/munge.key $mungekey
-sudo chown $ADMIN_USER $mungekey
+sudo chown $ADMIN_USERNAME $mungekey
 
-echo "Start looping all workers" >> /tmp/dummy 2>&1 
+echo "Start looping all workers" >> /tmp/azuredeploy.log.$$ 2>&1 
 
 i=0
 while [ $i -lt $NUM_OF_VM ]
 do
    worker=$WORKER_NAME$i
 
-   echo "SCP to $worker"  >> /tmp/dummy 2>&1 
-   sudo -u $ADMIN_USER scp $mungekey $ADMIN_USER@$worker:/tmp/munge.key >> /tmp/dummy 2>&1 
-   sudo -u $ADMIN_USER scp $SLURMCONF $ADMIN_USER@$worker:/tmp/slurm.conf >> /tmp/dummy 2>&1
+   echo "SCP to $worker"  >> /tmp/azuredeploy.log.$$ 2>&1 
+   sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key >> /tmp/azuredeploy.log.$$ 2>&1 
+   sudo -u $ADMIN_USERNAME scp $SLURMCONF $ADMIN_USERNAME@$worker:/tmp/slurm.conf >> /tmp/azuredeploy.log.$$ 2>&1
 
-   echo "Remote execute on $worker" >> /tmp/dummy 2>&1 
-   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker >> /tmp/dummy 2>&1 << 'ENDSSH1'
+   echo "Remote execute on $worker" >> /tmp/azuredeploy.log.$$ 2>&1 
+   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker >> /tmp/azuredeploy.log.$$ 2>&1 << 'ENDSSH1'
       sudo chmod g-w /var/log
       sudo apt-get update
       sudo apt-get install slurm-llnl -y
@@ -111,7 +111,7 @@ do
       sudo cp -f /tmp/slurm.conf /etc/slurm-llnl/slurm.conf
       sudo chown slurm /etc/slurm-llnl/slurm.conf
       sudo slurmd
-   ENDSSH1
+ENDSSH1
 
    i=`expr $i + 1`
 done
