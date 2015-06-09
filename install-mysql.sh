8d86d6fc598cf3e5f632c4022dd4f999d849f40d
m1028639 <sriprasanna.p@gmail.com>
2015-06-09 15:22:21 +0530

    Update install-mysql.sh

--- install-mysql.sh
+++ install-mysql.sh
@@ -2,30 +2,52 @@
 apt-get -y update
 
 # set up a silent install of MySQL
-$dbpass="welcome123"
 
 export DEBIAN_FRONTEND=noninteractive
-echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
-echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
 
 # install the LAMP stack
 apt-get -y install mysql-server
+
+# Create database & grant permission to root
+
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';" >permis.sql
+echo "flush privileges;" >>permis.sql
+echo "create database VehicleRental;" >>permis.sql
+echo "exit"
+
+#print
+
+echo ****************************************
+
+
+#restart mysql service
+
+sudo service mysql restart
+
+
+
+# call permis.sql
+mysql <permis.sql
+
+#print
+
+echo ****************************************
+
 
 #change the bind address
 
 sudo sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf 
 
 
-# Create database & grant permission to root
-
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';" >permis.sql
-echo "flush privileges;">>permis.sql
-echo "create database VehicleRental;" >>permis.sql
-echo "exit"
-
 #restart mysql service
 
 sudo service mysql restart
+
+
+#print
+
+echo ****************************************enddd
+
 
 
 
