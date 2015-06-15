98f8e3576ee66e0abf0bcc97be59a06cb54bd396
Beth Cooper <elcooper@microsoft.com>
2015-06-15 14:40:50 -0700

    Adding Azure Automation 101 deployment template

---
+++ 101-automation-runbook-getvms/DeployThroughARM.ps1
@@ -0,0 +1,27 @@
+﻿
+#Connect to your Azure account
+Add-AzureAccount
+
+#Select your subscription if you have more than one
+#Select-AzureSubscription -SubscriptionName "My Subscription Name"
+
+#Create a GUID for the job
+$JobGUID = [System.Guid]::NewGuid().toString()
+
+#Use Azure resource Manager to deploy template 
+Switch-AzureMode -Name AzureResourceManager
+
+#Set the parameter values for the template
+$Params = @{
+    "accountName" = "MyAccount" ;
+    "jobId" = $JobGUID;
+    "regionId" = "Japan East";
+    "credentialName" = "DefaultAzureCredential";
+	"userName" = "MyUserName"; 
+	"password" = "MyPassword"
+}
+
+$TemplateURI = "https://raw.githubusercontent.com/azureautomation/resources/master/automation-packs/101-get-vm-tutorial/deployAutomationResources.json"
+
+New-AzureResourceGroupDeployment -TemplateParameterObject $Params -ResourceGroupName "MyResourceGroup" -TemplateUri $TemplateURI
+
