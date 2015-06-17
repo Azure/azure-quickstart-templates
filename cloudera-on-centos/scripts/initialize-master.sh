f36e169f12139e5ae734fd8ded6bb6d64e17c51a
liupeirong <liupeirong@gmail.com>
2015-06-17 15:31:31 -0700

    revert extension split and fqdn

--- cloudera-on-centos/scripts/initialize-master.sh
+++ cloudera-on-centos/scripts/initialize-master.sh
@@ -19,6 +19,8 @@ NAMENODES=$4
 DATANODES=$5
 ADMINUSER=$6
 
+sh ./initialize-node.sh $ADMINUSER
+
 # Converts a domain like machine.domain.com to domain.com by removing the machine name
 NAMESUFFIX=`echo $NAMESUFFIX | sed 's/^[^.]*\.//'`
 
@@ -34,18 +36,18 @@ let "NAMEEND=NAMENODES-1"
 for i in $(seq 0 $NAMEEND)
 do 
   let "IP=i+10"
-  NODES+=("$IPPREFIX$IP:${NAMEPREFIX}-nn$i:${NAMEPREFIX}-nn$i")
+  NODES+=("$IPPREFIX$IP:${NAMEPREFIX}-nn$i.$NAMESUFFIX:${NAMEPREFIX}-nn$i")
 done
 
 let "DATAEND=DATANODES-1"
 for i in $(seq 0 $DATAEND)
 do 
   let "IP=i+20"
-  NODES+=("$IPPREFIX$IP:${NAMEPREFIX}-dn$i:${NAMEPREFIX}-dn$i")
+  NODES+=("$IPPREFIX$IP:${NAMEPREFIX}-dn$i.$NAMESUFFIX:${NAMEPREFIX}-dn$i")
 done
 
 IFS=',';NODE_IPS="${NODES[*]}";IFS=$' \t\n'
 
 echo "$(date): calling bootstrap_cloudera"
-sh bootstrap-cloudera.sh 'cloudera' "${IPPREFIX}9:${NAMEPREFIX}-mn:${NAMEPREFIX}-mn" $NODE_IPS false $ADMINUSER >> /home/$ADMINUSER/bootstrap-cloudera.log 2>&1
+sh bootstrap-cloudera.sh 'cloudera' "${IPPREFIX}9:${NAMEPREFIX}-mn.$NAMESUFFIX:${NAMEPREFIX}-mn" $NODE_IPS false $ADMINUSER >> /home/$ADMINUSER/bootstrap-cloudera.log 2>&1
 echo "$(date): done bootstrap_cloudera"
