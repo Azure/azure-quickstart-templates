c7b7fe25fa3ce89329f05e86f5d294f2b7245f6f
Paige Liu <liupeirong@gmail.com>
2015-06-25 17:59:25 -0700

    tuning based on Cloudera reco

--- cloudera-on-centos/scripts/prepareDisks.sh
+++ cloudera-on-centos/scripts/prepareDisks.sh
@@ -22,7 +22,7 @@ mountDrive() {
   driveId=$2
   echo "$(hostname) : /data${2} :About to mount drive"
   mount -o noatime,barrier=0 -t ext4 ${1} /data${2}
-  echo "$driveName   /data${2}    ext4   defaults,noatime,barrier=0 1 2" | sudo tee -a /etc/fstab
+  echo "$driveName   /data${2}    ext4   defaults,noatime,discard,barrier=0 0 0" | sudo tee -a /etc/fstab
   cat /etc/fstab
   echo "$(hostname) : /data${2} : Done mounting drive"
 
@@ -59,7 +59,7 @@ formatAndMountDrive() {
   chmod 777 /data${2}
   echo "$(hostname) : $1 : after data creation for id $2: $?"
   mount -o noatime,barrier=0 -t ext4 $drive /data${2}
-  echo "$drive   /data${2}    ext4   defaults,noatime,barrier=0 1 2" | sudo tee -a /etc/fstab
+  echo "$drive   /data${2}    ext4   defaults,noatime,discard,barrier=0 0 0" | sudo tee -a /etc/fstab
   echo "$(hostname) : $1 : after mounting for id $2 exit code: $?"
   echo "$(hostname) : $1 : Done operating on drive $1. Here is df -h"
   df -h
