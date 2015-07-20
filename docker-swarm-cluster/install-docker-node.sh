17759401e037ec58ab3bc5251e8e5302483f746c
jangelfdez <me@joseangelfernandez.es>
2015-07-20 17:59:29 +0200

    Added sudo to custom script

--- docker-swarm-cluster/install-docker-node.sh
+++ docker-swarm-cluster/install-docker-node.sh
@@ -1,6 +1,6 @@
-#/bin/bash
+#!/bin/bash
 sudo wget -qO- https://experimental.docker.com/ | sh
 sudo usermod -aG docker $1
 sudo systemctl stop docker
 sudo echo 'DOCKER_OPTS="-H 0.0.0.0:2375 -d"' > /etc/default/docker
-sudo systemctl stop docker
+sudo githgsystemctl stop docker
