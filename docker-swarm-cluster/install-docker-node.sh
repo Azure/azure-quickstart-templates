45f8bbe1667aa186bbb21a9ce5cbc6ebc4104729
jangelfdez <me@joseangelfernandez.es>
2015-07-20 18:19:27 +0200

    Error in command

--- docker-swarm-cluster/install-docker-node.sh
+++ docker-swarm-cluster/install-docker-node.sh
@@ -5,4 +5,4 @@ sudo ./install-docker.sh
 sudo usermod -aG docker $1
 sudo systemctl stop docker
 sudo echo 'DOCKER_OPTS="-H 0.0.0.0:2375 -d"' > /etc/default/docker
-sudo githgsystemctl stop docker
+sudo systemctl start docker
