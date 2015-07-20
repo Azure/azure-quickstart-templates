25835faa9ef6500bb890ce9c83f91af51c7bdf4a
jangelfdez <me@joseangelfernandez.es>
2015-07-20 19:04:45 +0200

    Autoload http connection on docker server

--- docker-swarm-cluster/install-docker-node.sh
+++ docker-swarm-cluster/install-docker-node.sh
@@ -4,5 +4,5 @@ sudo chmod +x install-docker.sh
 sudo ./install-docker.sh
 sudo usermod -aG docker $1
 sudo systemctl stop docker
-sudo echo 'DOCKER_OPTS="-H 0.0.0.0:2375 -d"' > /etc/default/docker
+sudo sed -i '/ExecStart/c\ExecStart=/usr/bin/docker -d -H 0.0.0.0:2375 -H fd://' /lib/systemd/system/docker.service
 sudo systemctl start docker
