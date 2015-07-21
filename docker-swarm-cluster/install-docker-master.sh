dc4e028788562d34f872551a459f5bbc52f7f943
jangelfdez <me@joseangelfernandez.es>
2015-07-21 15:12:03 +0200

    Error with docker cli parameter

--- docker-swarm-cluster/install-docker-master.sh
+++ docker-swarm-cluster/install-docker-master.sh
@@ -6,8 +6,12 @@ sudo usermod -aG docker $1
 
 sudo docker pull swarm
 
-CLUSTERID="$(sudo docker run --rm swarm create)"
+sudo docker run --rm swarm create > clusterId.txt
+
+CLUSTERID=$(cat clusterId.txt)
 
 docker -H tcp://10.0.1.4:2375 run -d swarm join --addr="10.0.1.4:2375" token://$CLUSTERID
 docker -H tcp://10.0.1.5:2375 run -d swarm join --addr="10.0.1.5:2375" token://$CLUSTERID
 docker -H tcp://10.0.1.6:2375 run -d swarm join --addr="10.0.1.6:2375" token://$CLUSTERID
+
+docker run -d -p 5005:2375 swarm manage token://$CLUSTERID
