d8fded971d8ac6cbbf11a10e083632c1bf47501b
jangelfdez <me@joseangelfernandez.es>
2015-07-24 19:25:18 +0200

    Change to use /etc/default/docker for options

--- docker-experimental-swarm-cluster/install-docker-node.sh
+++ docker-experimental-swarm-cluster/install-docker-node.sh
@@ -4,6 +4,6 @@ sudo chmod +x install-docker.sh
 sudo ./install-docker.sh
 sudo usermod -aG docker $1
 sudo systemctl stop docker
-sudo sed -i '/ExecStart/c\ExecStart=/usr/bin/docker -d -H 0.0.0.0:2375 -H fd://' /lib/systemd/system/docker.service
+sudo sed -i '/DOCKER_OPTS/c\DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix://var/run/docker.sock"' /etc/default/docker
 sudo systemctl daemon-reload
 sudo systemctl start docker
