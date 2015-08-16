bc0149b657638f9f10a1d336c24e38e0d75407c1
Oz <uzronen@microsoft.com>
2015-08-16 16:57:12 +0300

    fixes after review

--- node-mongodb-high-availability/nodeservice/app.js
+++ node-mongodb-high-availability/nodeservice/app.js
@@ -8,20 +8,25 @@ app.set('port', process.env.PORT || 8080);
 
 app.get('/', function (req, res) {
 
-    connString = "mongodb://youradminname:youradminpassword@10.0.0.10:27017,10.0.0.11:27017/tasks?replicaSet=rs0";
-    console.log(connString);
+    var connString = "mongodb://youradminname:youradminpassword@10.0.0.10:27017,10.0.0.11:27017/tasks?replicaSet=rs0";
     var MongoClient = mongodb.MongoClient;
     MongoClient.connect(connString, function (err, db) {
         if (err) {
-            console.log('Unable to connect to the mongoDB server. Error:', err);
-        } else {
+            return console.error('Unable to connect to the mongoDB server. Error:', err);
+        } 
 
-            console.log('Connection established to', connString);
+        console.log('Connection established to', connString);
 
-            // Workload
-            var collection = db.collection('tasks');
-            //Clean
-            collection.remove();
+        // Workload
+        var collection = db.collection('tasks');
+        //Clean
+        collection.remove(function (err) {
+
+            if(err)
+            {
+                return console.error('Unable to remove all data from collection. Error:', err);
+            }
+        
             //Create Tasks
             var task1 = { itemName: 'Item1', itemCategory: 'Cat2', itemCompleted: false, itemDate: Date.now };
             var task2 = { itemName: 'Item2', itemCategory: 'Cat2', itemCompleted: false, itemDate: Date.now };
@@ -29,24 +34,24 @@ app.get('/', function (req, res) {
 
             collection.insert([task1, task2, task3], function (err, result) {
                 if (err) {
-                    console.log(err);
+                    return console.error(err);
                 }
-                else {
-                    console.log('Inserted 3 documents into the "tasks" collection. The documents inserted with "_id" are:', result.length, result);
-                }
+                console.log('Inserted 3 documents into the "tasks" collection. The documents inserted with "_id" are:', result.length, result);
             });
             var all = collection.find().toArray(function (err, result) {
                 if (err) {
-                    console.log(err);
-                } else if (result.length) {
+                    return console.error(err);
+                }
+                else if (result.length) {
                     console.log('Found:', result);
                     res.status(200).json(result);
-                } else {
+                }
+                else {
                     console.log('No document(s) found with defined "find" criteria!');
                 }
                 db.close();
             });
-        }
+        });
     });
 });
 
