eea0d46af9cd588e7097a91955b25acee7fb9331
Oguz Pastirmaci <oguzp@OGUZP-X1.middleeast.corp.microsoft.com>
2015-08-28 16:04:13 +0300

    Fixed problem about unzipping the file downloaded with wget

--- vertx-openjdk-apache-mysql-on-ubuntu/install.sh
+++ vertx-openjdk-apache-mysql-on-ubuntu/install.sh
@@ -8,18 +8,17 @@ mysql_password="Passw0rd!"
 # Set MySQL Server version to install
 mysql_version="5.5"
 
-# Set Vert.x filename to download. Change the name if you want to install a different version
-vertx_filename="vert.x-2.1.5.zip"
+# Set Vert.x filename to download. Change the location as necessary
+vertx_download_location="https://bintray.com/artifact/download/vertx/downloads/vert.x-2.1.5.zip"
 
 # Install Vert.x, OpenJDK 7, and Apache
 apt-get update
 apt-get install unzip -y
 
 # Default download location for Vert.x, change it if you want to use an alternative location
-wget https://bintray.com/artifact/download/vertx/downloads/$vertx_filename
-unzip $vertx_filename
 apt-get install openjdk-7-jdk -y
 apt-get install apache2-utils -y
+wget -qO- -O tmp.zip $vertx_download_location && unzip tmp.zip && rm tmp.zip
 
 # Install MySQL Server in non-interactive mode
 export DEBIAN_FRONTEND=noninteractive
@@ -29,3 +28,6 @@ apt-get install mysql-server -y
 
 # Edit crontab to start MySQL Server service automatically on boot
 crontab -l | { cat; echo "@reboot service mysql start"; } | crontab -
+
+
+wget -qO- -O tmp.zip https://bintray.com/artifact/download/vertx/downloads/$vertx_filename && unzip tmp.zip && rm tmp.zip
