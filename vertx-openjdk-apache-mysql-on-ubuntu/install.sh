1b0723e2f607dd248d370f56296dbd7ab7d513dd
Oguz Pastirmaci <oguzp@OGUZP-X1.middleeast.corp.microsoft.com>
2015-08-28 16:59:09 +0300

    Adding Vertx to path

--- vertx-openjdk-apache-mysql-on-ubuntu/install.sh
+++ vertx-openjdk-apache-mysql-on-ubuntu/install.sh
@@ -9,7 +9,8 @@ mysql_password="Passw0rd!"
 mysql_version="5.5"
 
 # Set Vert.x filename to download. Change the location as necessary
-vertx_download_location="https://bintray.com/artifact/download/vertx/downloads/vert.x-2.1.5.zip"
+vertx_version="vert.x-2.1.5"
+vertx_download_location="https://bintray.com/artifact/download/vertx/downloads/$vertx_version".zip
 
 # Install Vert.x, OpenJDK 7, and Apache
 apt-get update
@@ -18,8 +19,9 @@ apt-get install unzip -y
 # Default download location for Vert.x, change it if you want to use an alternative location
 apt-get install openjdk-7-jdk -y
 apt-get install apache2-utils -y
-mkdir vertx && cd vertx
+mkdir /usr/local/vertx && cd /usr/local/vertx
 wget -qO- -O tmp.zip $vertx_download_location && unzip tmp.zip && rm tmp.zip
+export PATH=/usr/local/vertx/$vertx_version/bin:$PATH
 
 # Install MySQL Server in non-interactive mode
 export DEBIAN_FRONTEND=noninteractive
@@ -28,4 +30,4 @@ echo "mysql-server-$mysql_version mysql-server/root_password_again password $mys
 apt-get install mysql-server -y
 
 # Edit crontab to start MySQL Server service automatically on boot
-(crontab -l 2>/dev/null; echo "@reboot service mysql start") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot service mysql start") | crontab -echo
