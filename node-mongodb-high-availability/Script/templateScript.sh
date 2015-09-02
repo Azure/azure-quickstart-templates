8717a03863bf55ad78c3f92d93843da750d0396b
OzGitele <uzronen@microsoft.com>
2015-09-02 16:22:10 +0300

    Fix in script2

--- node-mongodb-high-availability/Script/templateScript.sh
+++ node-mongodb-high-availability/Script/templateScript.sh
@@ -1,9 +1,12 @@
 
-
 cd /
 sudo mv /var/lib/waagent/Microsoft.OSTCExtensions.CustomScriptForLinux-1.2.2.0/download/0/app.js /opt/app.js
 
 cd /opt
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
+
 
 sudo apt-get -y install nodejs
 sudo apt-get -y install npm
