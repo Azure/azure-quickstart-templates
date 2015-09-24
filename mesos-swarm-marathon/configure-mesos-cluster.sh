e86c23ace497261891395cbfcd9f72e69f58c2fa
anhowe <ahowe_ca@yahoo.ca>
2015-09-23 21:41:02 -0700

    added hostname to agents, and adjusted storage account packing

--- mesos-swarm-marathon/configure-mesos-cluster.sh
+++ mesos-swarm-marathon/configure-mesos-cluster.sh
@@ -111,8 +111,29 @@ ensureAzureNetwork()
     ip a
     exit 2
   fi
+  # ensure the host ip can resolve
+  networkHealthy=1
+  for i in {1..120}; do
+    hostname -i
+    if [ $? -eq 0 ]
+    then
+      # hostname has been found continue
+      networkHealthy=0
+      echo "the network is healthy"
+      break
+    fi
+    sleep 1
+  done
+  if [ $networkHealthy -ne 0 ]
+  then
+    echo "the network is not healthy, cannot resolve ip address, aborting install"
+    ifconfig
+    ip a
+    exit 2
+  fi
 }
 ensureAzureNetwork
+HOSTADDR=`hostname -i`
 
 ismaster ()
 {
@@ -169,6 +190,12 @@ zkconfig()
   zkconfigstr="zk://${zkhosts}/${postfix}"
   echo $zkconfigstr
 }
+
+######################
+# resolve self in DNS
+######################
+
+echo "$HOSTADDR $VMNAME" | sudo tee -a /etc/hosts
 
 ################
 # Install Docker
@@ -272,7 +299,7 @@ if ismaster  && [ "$MARATHONENABLED" == "true" ] ; then
   echo $zkmarathonconfig | sudo tee /etc/marathon/conf/zk
   # enable marathon to failover tasks to other nodes immediately
   echo 0 | sudo tee /etc/marathon/conf/failover_timeout
-  echo false | sudo tee /etc/marathon/conf/checkpoint
+  #echo false | sudo tee /etc/marathon/conf/checkpoint
 fi
 
 #########################################
@@ -284,6 +311,7 @@ if ismaster ; then
   sudo wget https://github.com/mesosphere/mesos-dns/releases/download/v0.2.0/mesos-dns-v0.2.0-linux-amd64.tgz
   sudo tar zxvf mesos-dns-v0.2.0-linux-amd64.tgz
   sudo mv mesos-dns-v0.2.0-linux-amd64 /usr/local/mesos-dns/mesos-dns
+  RESOLVER=`cat /etc/resolv.conf | grep nameserver | tail -n 1 | awk '{print $2}'`
 
   echo "
 {
@@ -295,7 +323,7 @@ if ismaster ; then
   \"timeout\": 1,
   \"listener\": \"0.0.0.0\",
   \"email\": \"root.mesos-dns.mesos\",
-  \"externalon\": false
+  \"resolvers\": [\"$RESOLVER\"]
 }
 " > mesos-dns.json
   sudo mv mesos-dns.json /usr/local/mesos-dns/mesos-dns.json
