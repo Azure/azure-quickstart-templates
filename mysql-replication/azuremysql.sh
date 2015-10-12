9c4956404bbf6e538628bf27fae6b2ae08d805d2
liupeirong <liupeirong@gmail.com>
2015-10-11 19:28:09 -0700

    default to 6.6

--- mysql-replication/azuremysql.sh
+++ mysql-replication/azuremysql.sh
@@ -207,7 +207,8 @@ install_mysql_centos() {
     echo "installing mysql"
     wget https://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-5.6.26-1.el6.x86_64.rpm-bundle.tar
     tar -xvf MySQL-5.6.26-1.el6.x86_64.rpm-bundle.tar
-    rpm -e --nodeps mysql-libs-5.1.73-3.el6_5.x86_64
+	curlib=$(rpm -qa |grep mysql-libs-)
+    rpm -e --nodeps $curlib
     rpm -ivh MySQL-server-5.6.26-1.el6.x86_64.rpm
     rpm -ivh MySQL-client-5.6.26-1.el6.x86_64.rpm
 	wget http://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.0.4-1.el6.noarch.rpm
