32a172bc6f2e48a279fb1cf39bd6abd4e3488145
Ravikanth <Ravikanth@Ravichaganti.com>
2015-10-30 20:45:57 +0530

    uploading configuration.zip and LCMConfiguration.ps1

---
+++ 201-vm-domain-join/LCMConfiguration.ps1
@@ -0,0 +1,13 @@
+﻿[DscLocalConfigurationManager()]
+Configuration DCLCMConfig
+{
+    Settings
+    {
+        RebootNodeIfNeeded = $true
+        ActionAfterReboot = 'ContinueConfiguration'
+    }
+
+}
+
+DCLCMConfig -OutputPath "${env:Temp}\DCLCMConfig"
+Set-DscLocalConfigurationManager -Path "${env:Temp}\DCLCMConfig"
