b17133ae70bd4886e6cccd609b19c233ded391e7
GarethBradshawMSFT <garbrad@microsoft.com>
2015-11-03 12:48:09 +0000

    Adding Web App example

---
+++ 101-traffic-manager-webapp/TrafficManagerWebApp.ps1
@@ -0,0 +1,17 @@
+﻿# parameters 
+$rgName = "TrafficManagerWebAppExample"
+
+#  set ARM mode
+Switch-AzureMode AzureResourceManager
+
+#  login and select subscription context
+Add-AzureAccount
+
+# create the resource from the template - pass names as parameters
+$scriptDir = Split-Path $MyInvocation.MyCommand.Path
+New-AzureResourceGroup -Verbose -Force -Name $rgName -Location "northeurope" -TemplateFile "$scriptDir\azuredeploy.json" -TemplateParameterFile "$scriptDir\azuredeploy-parameters.json"
+
+#  display the end result
+$x = Get-AzureTrafficManagerProfile -ResourceGroupName $rgName
+$x
+$x.Endpoints
