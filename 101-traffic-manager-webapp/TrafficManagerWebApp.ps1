4bca7f46e7ce431d17b276dccf98d593a17b4b2d
GarethBradshawMSFT <garbrad@microsoft.com>
2015-11-03 17:17:19 +0000

    Fixing name of azuredeploy.parameters.json

--- 101-traffic-manager-webapp/TrafficManagerWebApp.ps1
+++ 101-traffic-manager-webapp/TrafficManagerWebApp.ps1
@@ -11,7 +11,7 @@ Login-AzureRmAccount
 # create the resource from the template - pass names as parameters
 $scriptDir = Split-Path $MyInvocation.MyCommand.Path
 New-AzureRmResourceGroup -Location "northeurope" -Name $rgName
-New-AzureRmResourceGroupDeployment -Verbose -Force -ResourceGroupName $rgName -TemplateFile "$scriptDir\azuredeploy.json" -TemplateParameterFile "$scriptDir\azuredeploy-parameters.json"
+New-AzureRmResourceGroupDeployment -Verbose -Force -ResourceGroupName $rgName -TemplateFile "$scriptDir\azuredeploy.json" -TemplateParameterFile "$scriptDir\azuredeploy.parameters.json"
 
 #  display the end result
 $x = Get-AzureRmTrafficManagerProfile -ResourceGroupName $rgName
