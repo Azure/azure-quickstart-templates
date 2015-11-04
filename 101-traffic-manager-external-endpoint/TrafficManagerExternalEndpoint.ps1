04d194f5bbd671b469a3abe7a9d57bbc151a990e
GarethBradshawMSFT <garbrad@microsoft.com>
2015-11-04 16:55:35 +0000

    Applying feedback

--- 101-traffic-manager-external-endpoint/TrafficManagerExternalEndpoint.ps1
+++ 101-traffic-manager-external-endpoint/TrafficManagerExternalEndpoint.ps1
@@ -10,7 +10,7 @@ Login-AzureRmAccount
 
 # create the resource from the template - pass names as parameters
 $scriptDir = Split-Path $MyInvocation.MyCommand.Path
-$params = @{"dnsname"="myexample"}
+$params = @{"uniqueDnsName"="myexample"}
 New-AzureRmResourceGroup -Location "northeurope" -Name $rgName
 New-AzureRmResourceGroupDeployment -Verbose -Force -ResourceGroupName $rgName -TemplateFile "$scriptDir\azuredeploy.json" -TemplateParameterObject $params
 
