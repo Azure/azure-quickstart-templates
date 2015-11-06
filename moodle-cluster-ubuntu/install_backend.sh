6ea5ca918d0346309a22d56c9c4fa9f48d964d41
vinhub <vin.bhalerao@gmail.com>
2015-11-06 15:53:36 -0800

    avoid unnecessary restarts

--- moodle-cluster-ubuntu/install_backend.sh
+++ moodle-cluster-ubuntu/install_backend.sh
@@ -17,6 +17,9 @@ apt-get -y install apache2 mysql-client mysql-server php5
 # install moodle requirements
 apt-get -y install graphviz aspell php5-pspell php5-curl php5-gd php5-intl php5-mysql php5-xmlrpc php5-ldap
 
+# Allow remote connection
+sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+
 # create moodle database
 MYSQL=`which mysql`
 
@@ -27,20 +30,7 @@ SQL="${Q1}${Q2}${Q3}"
 
 $MYSQL -uroot -p$dbpass -e "$SQL"
 
-# restart MySQL
-service mysql restart
-
-# Allow remote connection
-echo "Updating mysql configs in /etc/mysql/my.cnf."
-sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
-echo "Updated mysql bind address in /etc/mysql/my.cnf to 0.0.0.0 to allow external connections."
-service mysql stop
-service mysql start
-
-# restart Apache
-apachectl restart
-
-# Create file share that will be used by front end VM's for moodledata directory
+# Create Azure file share that will be used by front end VM's for moodledata directory
 SharedStorageAccountName=$2
 SharedAzureFileName=$3
 SharedStorageAccountKey=$4
@@ -50,3 +40,9 @@ apt-get -y install npm
 npm install -g azure-cli
 
 azure storage share create $SharedAzureFileName -a $SharedStorageAccountName -k $SharedStorageAccountKey
+
+# restart MySQL
+service mysql restart
+
+# restart Apache
+apachectl restart
