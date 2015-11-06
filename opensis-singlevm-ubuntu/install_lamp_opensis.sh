7b3d602518605ed14f433d60c90d1790f92b051a
vinhub <vin.bhalerao@gmail.com>
2015-11-06 13:41:44 -0800

    updated permissions

--- opensis-singlevm-ubuntu/install_lamp_opensis.sh
+++ opensis-singlevm-ubuntu/install_lamp_opensis.sh
@@ -13,12 +13,13 @@ apt-get -y install apache2 mysql-server php5 php5-mysql
 
 # install OpenSIS
 cd /var/www/html
-sudo wget http://sourceforge.net/projects/opensis-ce/files/opensis6.0.zip/download -O opensis.zip
-sudo apt-get install unzip
-sudo unzip opensis.zip
+wget http://sourceforge.net/projects/opensis-ce/files/opensis6.0.zip/download -O opensis.zip
+apt-get install unzip
+unzip opensis.zip
 
 # make the opensis-ce directory writable
-sudo chmod -R +755 opensis-ce
+chown -R www-data opensis-ce
+chmod -R 770 opensis-ce
 
 # restart Apache
 apachectl restart
