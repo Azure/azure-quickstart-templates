9e8ecf863a8d5976408ca9a2ad7a8c1ef560694d
Harris Boyce <harris.boyce@incyclesoftware.com>
2015-11-13 18:18:46 -0500

    modifying script to run tfsconfig as setup account via invoke-command

--- tfs2013up4-muititier/Configure-TeamFoundationServer.ps1
+++ tfs2013up4-muititier/Configure-TeamFoundationServer.ps1
@@ -12,17 +12,26 @@
 	[string]$serviceAccountPassword= "password#1"
 )
 
-Set-Location -Path (Get-Content Env:\ProgramFiles)
-Set-Location -Path "Microsoft Team Foundation Server 12.0\Tools"
-
 $setupPassword = ConvertTo-SecureString -String $setupAccountPassword -AsPlainText -Force
 $setupCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($setupAccountName,$setupPassword)
 
 $servicePassword = ConvertTo-SecureString -String $serviceAccountPassword -AsPlainText -Force
 $serviceCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($serviceAccountName,$servicePassword)
 
+Enable-WSManCredSSP -Role Server
+Enable-WSManCredSSP -Role Client -DelegateComputer $Env:COMPUTERNAME
+
+Invoke-Command -ComputerName $Env:COMPUTERNAME -Authentication Credssp -ScriptBlock {
+	Set-Location -Path (Get-Content Env:\ProgramFiles)
+	Set-Location -Path "Microsoft Team Foundation Server 12.0\Tools"
+
+	& ".\tfsconfig.exe" unattend /configure /type:standard /inputs:"UseWss=$using:useWss;UseReporting=$using:useReporting;ConfigureWss=$false;SqlInstance=$using:sqlInstance;UseSqlAlwaysOn=$using:useSqlAlwaysOn;IsServiceAccountBuiltIn=$using:isServiceAccountBuiltIn;ServiceAccountName=$using:serviceAccountName;ServiceAccountPassword=$(($using:serviceCred).GetNetworkCredential().Password)"  /verify 2>&1 | Write-Verbose
+	& ".\tfsconfig.exe" unattend /configure /type:standard /inputs:"UseWss=$using:useWss;UseReporting=$using:useReporting;ConfigureWss=$false;SqlInstance=$using:sqlInstance;UseSqlAlwaysOn=$using:useSqlAlwaysOn;IsServiceAccountBuiltIn=$using:isServiceAccountBuiltIn;ServiceAccountName=$using:serviceAccountName;ServiceAccountPassword=$(($using:serviceCred).GetNetworkCredential().Password)"  2>&1 | Write-Verbose
+
+} -Verbose -Credential $setupCred
+
 # start the configuration of the app-tier
-$inputArgs = "UseWss=$useWss;UseReporting=$useReporting;ConfigureWss=$false;SqlInstance=$sqlInstance;UseSqlAlwaysOn=$useSqlAlwaysOn;IsServiceAccountBuiltIn=$isServiceAccountBuiltIn;ServiceAccountName=$serviceAccountName;ServiceAccountPassword=$($serviceCred.GetNetworkCredential().Password)"
-$tfsConfigArgs = "unattend /configure /type:standard /inputs:`"$inputArgs`""
-Start-Process -FilePath ".\tfsconfig.exe" -ArgumentList $tfsConfigArgs -Credential $setupCred -Wait -WindowStyle Normal -Verbose
+# $inputArgs = "UseWss=$useWss;UseReporting=$useReporting;ConfigureWss=$false;SqlInstance=$sqlInstance;UseSqlAlwaysOn=$useSqlAlwaysOn;IsServiceAccountBuiltIn=$isServiceAccountBuiltIn;ServiceAccountName=$serviceAccountName;ServiceAccountPassword=$($serviceCred.GetNetworkCredential().Password)"
+#$tfsConfigArgs = "unattend /configure /type:standard /inputs:`"$inputArgs`""
+#Start-Process -FilePath ".\tfsconfig.exe" -ArgumentList $tfsConfigArgs -Credential $setupCred -Wait -WindowStyle Normal -Verbose
 #tfsconfig.exe unattend /configure /type:standard /inputs:"UseWss=$useWss;UseReporting=$useReporting;ConfigureWss=$false;SqlInstance=$sqlInstance;UseSqlAlwaysOn=$useSqlAlwaysOn;IsServiceAccountBuiltIn=$isServiceAccountBuiltIn;ServiceAccountName=$serviceAccountName" /verify
