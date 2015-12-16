4352ead5ab58c8fe1f614eb3ea59b6d3d1063467
Steven Edouard <sedouard@microsoft.com>
2015-12-16 12:18:47 -0800

    test travis commit range

--- test/tests.js
+++ test/tests.js
@@ -13,7 +13,7 @@ function getModifiedPaths() {
   assert.ok(process.env.TRAVIS_COMMIT_RANGE, 'VALIDATE_MODIFIED_ONLY requires TRAVIS_COMMIT_RANGE to be set to [START_COMMIT_HASH]...[END_COMMIT_HASH]');
   var rangeStart = process.env.TRAVIS_COMMIT_RANGE.split('...')[0];
   var rangeEnd = process.env.TRAVIS_COMMIT_RANGE.split('...')[1];
-  var stdout = execSync('git diff --name-only ' + rangeStart + ' ' + rangeEnd, {
+  var stdout = execSync('git diff --name-only ' + process.env.TRAVIS_COMMIT_RANGE, {
     encoding: 'utf8'
   });
   var lines = stdout.split('\n');
