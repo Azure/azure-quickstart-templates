70296992730c916f60caea4c96d3a1fa4d4b53b5
251744647 <251744647@qq.com>
2015-12-18 14:49:42 +0800

    Update lamp_ubuntu.sh

--- lamp-stack-on-ubuntu/lamp_ubuntu.sh
+++ lamp-stack-on-ubuntu/lamp_ubuntu.sh
@@ -1,23 +1,11 @@
-#!/bin/bash
-apt-get update
-#no password prompt while installing mysql server
-export DEBIAN_FRONTEND=noninteractive
-#install php mysql apache
-apt-get -y install apache2 php5 libapache2-mod-php5 mysql-server libapache2-mod-auth-mysql php5-mysql
-#php test file
-#cat > /var/www/html/info.php <<EOF
-#<?php
-#phpinfo();
-#?>
-#EOF
+#!/bin/bash 
+apt-get update 
 
-#mysql connection test file
-#cat > /var/www/html/mysql.php <<EOF
-#<?php
-#\$link = mysql_connect('localhost', 'root', '');
-#if (!\$link) {
-#    die('Could not connect:' . mysql_error());
-#}
-#echo 'Connected sucessfully';
-#?>
-#EOF
+# install Apache2 
+apt-get -y install apache2  
+
+# write some HTML 
+echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html 
+
+# restart Apache 
+apachectl restart 
