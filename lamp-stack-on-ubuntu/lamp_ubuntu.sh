f5dc85a8517c0002815f97ff4c9c993503aebb9e
Shuiping He (Wicresoft) <v-shuihe@microsoft.com>
2015-12-18 14:14:52 +0800

    modified extension script

--- lamp-stack-on-ubuntu/lamp_ubuntu.sh
+++ lamp-stack-on-ubuntu/lamp_ubuntu.sh
@@ -3,10 +3,8 @@ apt-get update
 
 #no password prompt while installing mysql server
 export DEBIAN_FRONTEND=noninteractive
-
 #install php mysql apache
 apt-get -y install apache2 php5 libapache2-mod-php5 mysql-server libapache2-mod-auth-mysql php5-mysql
-
 #php test file
 cat > /var/www/html/info.php <<EOF
 <?php
