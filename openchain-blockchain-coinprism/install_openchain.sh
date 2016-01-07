a9d51110e01e1fb1fe0ac777e494965a84ba409f
Flavien Charlon <flavien@charlon.net>
2016-01-07 20:06:20 +0100

    Update the install_openchain.sh script to work with Openchain 0.5

--- openchain-blockchain-coinprism/install_openchain.sh
+++ openchain-blockchain-coinprism/install_openchain.sh
@@ -10,11 +10,12 @@ cd /opt
 git clone https://github.com/openchain/docker.git openchain
 cd openchain
 cp templates/docker-compose-proxy.yml docker-compose.yml
+cp templates/project.json openchain/project.json
 cp templates/nginx.conf nginx/nginx.conf
 mkdir data
 cp templates/config.json data/config.json
 
-sed -i "s#\"root_url\": \"\"#\"root_url\": \"$1\"#g" data/config.json
+sed -i "s#\"instance_seed\": \"\"#\"instance_seed\": \"$1\"#g" data/config.json
 sed -i "s#\"admin_addresses\": \[#\"admin_addresses\": [ \"$2\" #g" data/config.json
 
 sed -i "s#\"allow_p2pkh_accounts\": true#\"allow_p2pkh_accounts\": $3#g" data/config.json
