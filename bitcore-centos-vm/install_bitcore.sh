53b6d3624560fd3edbe3695cecacc85d871b9ada
Chris Kleeschulte <chrisk@bitpay.com>
2016-01-18 13:59:38 -0500

    Repaired the deploy script for testing.

--- bitcore-centos-vm/install_bitcore.sh
+++ bitcore-centos-vm/install_bitcore.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+set -x
+echo "Initializing Bitcore installation"
+
+#############
+# Parameters
+#############
+AZUREUSER=$1
+HOMEDIR="/home/$AZUREUSER"
+VMNAME=`hostname`
+echo "User: $AZUREUSER"
+echo "User home dir: $HOMEDIR"
+echo "vmname: $VMNAME"
+
 yum -y install git curl which xz tar findutils
-groupadd bitcore
-useradd bitcore -m -s /bin/bash -g bitcore
+groupadd $AZUREUSER
+useradd $AZUREUSER -m -s /bin/bash -g $AZUREUSER
 cat >/etc/systemd/system/bitcored.conf <<"EOL"
 [Unit]
 Description=bitcored.service
@@ -9,7 +22,7 @@ After=network.target
 
 [Service]
 Type=simple
-User=bitcore
+User=AZUREUSER
 Environment="PATH=$PATH:/home/bitcore/.nvm/versions/node/v4.2.4/bin"
 ExecStart=/home/bitcore/.nvm/versions/node/v4.2.4/bin/bitcored
 ExecReload=/bin/kill -2 $MAINPID
@@ -19,9 +32,12 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.agent
 EOL
-sudo su - bitcore
+sed -i "/AZUREUSER/$ASUREUSER/g" "/etc/systemd/system/bitcored.conf"
+sudo su - $AZUREUSER
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 /bin/bash -l -c "nvm install v4 && nvm alias default v4"
 /bin/bash -l -c "npm install bitcore -g"
 exit
 systemctl start bitcored
+echo "Completed Bitcore install $$"
+
