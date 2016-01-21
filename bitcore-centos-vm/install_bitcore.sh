e360d81ae687243dad683dc240bafb9e679a46ac
Chris Kleeschulte <chrisk@bitpay.com>
2016-01-21 12:03:50 -0500

    Merge branch 'master' into bitcore-centos-vm

--- bitcore-centos-vm/install_bitcore.sh
+++ bitcore-centos-vm/install_bitcore.sh
@@ -6,38 +6,32 @@ echo "Initializing Bitcore installation"
 # Parameters
 #############
 AZUREUSER=$1
-HOMEDIR="/home/$AZUREUSER"
-VMNAME=`hostname`
+HOME="/home/$AZUREUSER"
 echo "User: $AZUREUSER"
-echo "User home dir: $HOMEDIR"
-echo "vmname: $VMNAME"
+echo "User home dir: $HOME"
 
-yum -y install git curl which xz tar findutils
-groupadd $AZUREUSER
-useradd $AZUREUSER -m -s /bin/bash -g $AZUREUSER
-cat >/etc/systemd/system/bitcored.conf <<"EOL"
+yum -y install epel-release
+yum -y install npm git curl which xz tar findutils
+npm install -g n
+n 4.2.4
+cat >/etc/systemd/system/bitcored.service <<EOL
 [Unit]
 Description=bitcored.service
 After=network.target
 
 [Service]
 Type=simple
-User=AZUREUSER
-Environment="PATH=$PATH:/home/bitcore/.nvm/versions/node/v4.2.4/bin"
-ExecStart=/home/bitcore/.nvm/versions/node/v4.2.4/bin/bitcored
-ExecReload=/bin/kill -2 $MAINPID
+User=$AZUREUSER
+ExecStart=/usr/local/bin/bitcored
+ExecReload=/bin/kill -2 \$MAINPID
 KillMode=process
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.agent
 EOL
-sed -i "/AZUREUSER/$ASUREUSER/g" "/etc/systemd/system/bitcored.conf"
-sudo su - $AZUREUSER
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
-/bin/bash -l -c "nvm install v4 && nvm alias default v4"
-/bin/bash -l -c "npm install bitcore -g"
-exit
+/bin/bash -l -c "export PATH=/usr/local/bin:\$PATH; export HOME=$HOME; npm install -g bitcore"
+/bin/bash -l -c "parted -s /dev/sdc mklabel gpt unit s mkpart primary `parted -s /dev/sdc mklabel gpt unit s print free | grep 'Free Space' | tail -n 1 | awk '{print $1}'` `parted -s /dev/sdc mklabel gpt unit s print free | grep 'Free Space' | tail -n 1 | awk '{print $2}'` && mkfs.ext4 /dev/sdc1 && mkdir $HOME/.bitcore && mount /dev/sdc1 $HOME/.bitcore"
+/bin/bash -l -c "chown -R $AZUREUSER $HOME/.bitcore"
 systemctl start bitcored
 echo "Completed Bitcore install $$"
-
