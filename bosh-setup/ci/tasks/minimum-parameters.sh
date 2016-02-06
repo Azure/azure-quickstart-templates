87377ab29dff871e06d5436dee4c4a7dddf5b6ac
Bin Xia <binxi@microsoft.com>
2016-02-06 13:23:39 +0000

    Add new features and refine the template
    - New features
      - Support deploying Bosh automatically
    - Update to the official BOSH release, Azure CPI release and Azure stemcell(http://bosh.io/docs/init-azure.html)
    - Parameters and Variables
      - Remove the parameter "newStorageAccountName" and generate it by uniqueString()
      - Create the dev-box with SSH Keys
      - Make service principal parameters required and fixed-length
      - Move the parameter "vmSize" into a variable
      - Move the parameters about vnet & subnet to variables
      - Change the CIDR of the subnet for Cloud Foundry to /20
    - Render the manifest of Bosh
      - Autofill the service principal
    - Render the manifest of Cloud Foundry
      - Autofill the virtual network name, the subnet name and so on.
    - Upgrade versions
      - Upgrade bosh_cli version to 1.3169.0
      - Upgrade bosh-init version to 0.0.81
      - Upgrade API version to the latest 2015-06-15
      - Upgrade to Ubuntu Server 14.04.3 LTS
      - Upgrade the default "storageAccountType" into Standard_RAGRS
      - Upgrade the version of CustomScript Extension to 1.4
        - Download the scripts and manifests via fileUris
        - Put commandToExecute into protectedSettings to protect users' credentials

---
+++ bosh-setup/ci/tasks/minimum-parameters.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e
+
+azure login --service-principal -u ${AZURE_CLIENT_ID} -p ${AZURE_CLIENT_SECRET} --tenant ${AZURE_TENANT_ID}
+azure config mode arm
+
+UUID=$(cat /proc/sys/kernel/random/uuid)
+UUID=${UUID//-}
+UUID="cf${UUID:0:22}"
+
+cat > parameters.json << EOF
+{
+  "\$schema": "http://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {
+    "vmName": {
+      "value": "$UUID"
+    },
+    "adminUsername": {
+      "value": "azureuser"
+    },
+    "sshKeyData": {
+      "value": "${SSH_KEY_DATA}"
+    },
+    "tenantID": {
+      "value": "${AZURE_TENANT_ID}"
+    },
+    "clientID": {
+      "value": "${AZURE_CLIENT_ID}"
+    },
+    "clientSecret": {
+      "value": "${AZURE_CLIENT_SECRET}"
+    }
+  }
+}
+EOF
+
+AZURE_GROUP_NAME=$UUID
+echo azure group create ${AZURE_GROUP_NAME} "${AZURE_REGION_NAME}"
+azure group create ${AZURE_GROUP_NAME} "${AZURE_REGION_NAME}"
+azure group deployment create ${AZURE_GROUP_NAME} --template-file ./bosh-setup-template/bosh-setup/azuredeploy.json --parameters-file ./parameters.json -vv
+
+azure group delete ${AZURE_GROUP_NAME} --quiet
