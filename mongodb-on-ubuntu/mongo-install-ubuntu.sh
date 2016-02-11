2e99ce2ef763d582114a6a35c6560633809d0fa9
Davide Icardi <davide.icardi@gmail.com>
2016-02-11 22:35:10 +0100

    Added optional configuration to bind to all ip

--- mongodb-on-ubuntu/mongo-install-ubuntu.sh
+++ mongodb-on-ubuntu/mongo-install-ubuntu.sh
@@ -15,3 +15,7 @@ sudo bash -c "sudo echo net.ipv4.tcp_keepalive_time = 120 >> /etc/sysctl.conf"
 
 #Install Mongo DB
 sudo apt-get install -y mongodb-org
+
+# Uncomment this to bind to all ip addresses
+# sudo sed -i -e 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/mongod.conf
+# sudo service mongod restart
