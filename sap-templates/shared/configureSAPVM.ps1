cd2edf936433a9bc4835c16c20c89c3ed6afc357
sedusch <sedusch@microsoft.com>
2016-02-16 16:39:25 +0100

    fix script execution, add Linux custom script name and publisher

--- sap-templates/shared/configureSAPVM.ps1
+++ sap-templates/shared/configureSAPVM.ps1
@@ -1,11 +1,12 @@
 ﻿param
 (
-    [String] $DBDataLUNS = "0,1,2",
-    [String] $DBLogLUNS = "3",
+    [int] $SQLDataFilesCount = 4,    
+    [String] $SQLDataLUNS = "0,1,2",
+    [String] $SQLLogLUNS = "3",
     [long] $DataFileSizeMB = 1024,
     [long] $LogSizeMB = 1024,
-    [string] $DBDataDrive = "S:",
-    [string] $DBLogDrive = "L:"
+    [string] $SQLDrive = "S:",
+    [string] $LogDrive = "L:"
 )
 
 $ErrorActionPreference = "Stop";
@@ -65,5 +66,5 @@ function Create-Pool
     }
 }
 
-Create-Pool -arraystring $DBDataLUNS -name "sqldata" -path $DBDataDrive;
-Create-Pool -arraystring $DBLogLUNS -name "sqllog" -path $DBLogDrive;
+Create-Pool -arraystring $SQLDataLUNS -name "dbdata" -path $SQLDrive;
+Create-Pool -arraystring $SQLLogLUNS -name "dblog" -path $LogDrive;
