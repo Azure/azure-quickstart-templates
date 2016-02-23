c69544542a7d295c0ac15f1b59fd8e9aca2caa71
Neil Sant Gat <negat@microsoft.com>
2016-02-23 00:15:17 +0000

    added retry loop around apt-get

--- 201-vmss-lapstack-autoscale/install_lap.sh
+++ 201-vmss-lapstack-autoscale/install_lap.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-# wait for Linux Diagnostic Extension to complete
-for i in {1..60}; do
-  if [ -e /var/lib/cloud/instance/boot-finished ]
-  then
-    echo "the provisioning has complete"
-    break
-  fi
-  sleep 10
+
+# install Apache and PHP (in a loop because a lot of installs happen
+# on VM init, so won't be able to grab the dpkg lock immediately)
+until apt-get -y update && apt-get -y install apache2 php5
+do
+  echo "Try again"
+  sleep 2
 done
 
-# install Apache and PHP
-apt-get -y update
-apt-get -y install apache2 php5
+
+
 
 # write some PHP
 cd /var/www/html
