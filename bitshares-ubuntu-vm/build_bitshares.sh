62fdc8ca540470e5aa8611442c180380a34f3a47
ryanrfox <ryan@ryanrfox.com>
2016-02-25 21:35:08 +1100

    Fixup: install script

--- bitshares-ubuntu-vm/build_bitshares.sh
+++ bitshares-ubuntu-vm/build_bitshares.sh
@@ -21,19 +21,23 @@ echo "nproc: $NPROC"
 ################################################################
 # Update Ubuntu and install all necessary prerequisites        #
 ################################################################
-
 time apt-get -y update
 time apt-get -y install git ntp cmake libbz2-dev libdb++-dev libdb-dev libssl-dev openssl libreadline-dev autoconf libtool libboost-all-dev
+wait $!
 
 ################################################################
 # Build BitShares from source, then start witness node         #
 ################################################################
 cd ~/ 
-time sudo git clone https://github.com/bitshares/bitshares-2.git 
+time git clone https://github.com/bitshares/bitshares-2.git 
+wait $!
 cd ~/bitshares-2 
 time git submodule update --init --recursive --force 
+wait $!
 time cmake -DCMAKE_BUILD_TYPE=Release . 
+wait $!
 time make -j$NPROC
+wait $!
 
 cd ~/bitshares-2/programs/witness_node
 ./witness_node --rpc-endpoint=127.0.0.1:8090
