ca98138aaa87810c55b04a163eb325aae3aea8a0
ryanrfox <ryan@ryanrfox.com>
2016-02-25 14:20:00 +1100

    Remove: BitShares installer

--- bitshares-ubuntu-vm/build_bitshares.sh
+++ bitshares-ubuntu-vm/build_bitshares.sh
@@ -19,19 +19,19 @@ echo "nproc: $NPROC"
 #######################################################
 
 time sudo apt-get -y update
-time sudo apt-get -y --force-yes install libbz2-dev libdb++-dev libdb-dev libssl-dev openssl libreadline-dev autoconf libtool git cmake ntp libboost-all-dev
+#time sudo apt-get -y --force-yes install libbz2-dev libdb++-dev libdb-dev libssl-dev openssl libreadline-dev autoconf libtool git cmake ntp libboost-all-dev
 
 ###############################
 # Install BitShares from source
 ###############################
-sudo screen 
-
-cd ~/ 
-time sudo git clone https://github.com/bitshares/bitshares-2.git 
-cd ~/bitshares-2 
-time sudo git submodule update --init --recursive --force 
-time sudo cmake -DCMAKE_BUILD_TYPE=Release . 
-time sudo make -j $NPROC
-
-cd ~/bitshares-2/programs/witness_node
-sudo ./witness_node --rpc-endpoint=127.0.0.1:8090
+#sudo screen 
+#
+#cd ~/ 
+#time sudo git clone https://github.com/bitshares/bitshares-2.git 
+#cd ~/bitshares-2 
+#time sudo git submodule update --init --recursive --force 
+#time sudo cmake -DCMAKE_BUILD_TYPE=Release . 
+#time sudo make -j $NPROC
+#
+#cd ~/bitshares-2/programs/witness_node
+#sudo ./witness_node --rpc-endpoint=127.0.0.1:8090
