74512cdd369a10bdd15a8c9dbc3a550e526a91e6
ryanrfox <ryan@ryanrfox.com>
2016-02-25 17:57:19 +1100

    Fixup: install script

--- bitshares-ubuntu-vm/build_bitshares.sh
+++ bitshares-ubuntu-vm/build_bitshares.sh
@@ -7,31 +7,33 @@ set -x
 date
 ps axjf
 
+################################################################
+# Start a screen session to allow viewing of progress via SSH  #
+################################################################
+sudo screen 
+
 #############
 # Parameters
 #############
-
 NPROC=$(nproc)
 echo "nproc: $NPROC"
 
-#######################################################
-# Update Ubuntu and install all necessary prerequisites
-#######################################################
+################################################################
+# Update Ubuntu and install all necessary prerequisites        #
+################################################################
 
 time apt-get -y update
 time apt-get -y install git ntp cmake libbz2-dev libdb++-dev libdb-dev libssl-dev openssl libreadline-dev autoconf libtool libboost-all-dev
 
-###############################
-# Install BitShares from source
-###############################
-#sudo screen 
-#
-#cd ~/ 
-#time sudo git clone https://github.com/bitshares/bitshares-2.git 
-#cd ~/bitshares-2 
-#time sudo git submodule update --init --recursive --force 
-#time sudo cmake -DCMAKE_BUILD_TYPE=Release . 
-#time sudo make -j $NPROC
-#
-#cd ~/bitshares-2/programs/witness_node
-#sudo ./witness_node --rpc-endpoint=127.0.0.1:8090
+################################################################
+# Build BitShares from source, then start witness node         #
+################################################################
+cd ~/ 
+time sudo git clone https://github.com/bitshares/bitshares-2.git 
+cd ~/bitshares-2 
+time sudo git submodule update --init --recursive --force 
+time sudo cmake -DCMAKE_BUILD_TYPE=Release . 
+time sudo make -j$NPROC
+
+cd ~/bitshares-2/programs/witness_node
+sudo ./witness_node --rpc-endpoint=127.0.0.1:8090
