a1d53f8610daf9bf75897b3623d4ecbb6a0b243c
ryanrfox <ryan@ryanrfox.com>
2016-02-26 11:15:30 +1100

    Fixup: bash script

--- bitshares-ubuntu-vm/build_bitshares.sh
+++ bitshares-ubuntu-vm/build_bitshares.sh
@@ -12,11 +12,6 @@ ps axjf
 ################################################################
 time apt-get -y update && apt-get -y install dphys-swapfile git ntp cmake libbz2-dev libdb++-dev libdb-dev libssl-dev openssl libreadline-dev autoconf libtool libboost-all-dev
 
-################################################################
-# Start a screen session to allow viewing of progress via SSH  #
-################################################################
-sudo screen 
-.
 #############
 # Parameters
 #############
@@ -26,8 +21,13 @@ echo "nproc: $NPROC"
 ################################################################
 # Build BitShares from source, then start witness node         #
 ################################################################
-cd ~/ 
-time git clone https://github.com/bitshares/bitshares-2.git && cd ~/bitshares-2/ && time git submodule update --init --recursive --force && time cmake -DCMAKE_BUILD_TYPE=Release . && time make -j$NPROC && cd ~/bitshares-2/programs/witness_node/
+/usr/local
+time git clone https://github.com/bitshares/bitshares-2.git
+cd bitshares-2/
+time git submodule update --init --recursive --force
+time cmake -DCMAKE_BUILD_TYPE=Release .
+time make -j$NPROC
+cd programs/witness_node/
 ./witness_node --rpc-endpoint=127.0.0.1:8090
 
 ################################################################
