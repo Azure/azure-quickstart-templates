05fb81512159cd68abfdb2de0d4f37476f97370d
Tyler Pinckard <tyjapi@aol.com>
2016-03-18 11:51:10 -0700

    Update install_manifold.sh
    Add '-noprompt' to keytool command to add manifold cert

--- manifold-endpoint/install_manifold.sh
+++ manifold-endpoint/install_manifold.sh
@@ -11,7 +11,7 @@ curl -X GET https://s3-us-west-2.amazonaws.com/rpmhost/rest-server-0.1-1.x86_64.
 yum install -y rest-server-0.1-1.x86_64.rpm
 
 curl -X GET https://s3-us-west-2.amazonaws.com/rpmhost/manifold.crt > manifold.crt
-sudo keytool -import -alias manifold -trustcacerts -file manifold.crt -keystore /usr/java/latest/jre/lib/security/cacerts -storepass changeit
+sudo keytool -import -noprompt -alias manifold -trustcacerts -file manifold.crt -keystore /usr/java/latest/jre/lib/security/cacerts -storepass changeit
 
 systemctl start rest-server
 cd /home/
