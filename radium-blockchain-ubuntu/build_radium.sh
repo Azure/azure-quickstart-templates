2b796e2054cc7ef0b84c3f3e440f5cad27e4f836
JJ12880 <JJ12880@users.noreply.github.com>
2016-03-20 13:56:08 -0400

    Initial commit

---
+++ radium-blockchain-ubuntu/build_radium.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e 
+date
+ps axjf
+NPROC=$(nproc)
+echo "nproc: $NPROC"
+
+#################################################################
+# Update Ubuntu and install prerequisites for running Radium #
+#################################################################
+
+time apt-get update
+time apt-get install -y ntp wget git miniupnpc build-essential libssl-dev libdb++-dev libboost-all-dev libqrencode-dev libtool autotools-dev autoconf pkg-config
+
+#################################################################
+# Build config file                                             #
+#################################################################
+
+file=$HOME/.Radium
+if [ ! -e "$file" ]
+then
+sudo mkdir $HOME/.Radium
+fi
+
+sudo printf 'rpcuser=%s\n' $2  >> $HOME/.Radium/Radium.conf
+sudo printf 'rpcpassword=%s\n' $3 >> $HOME/.Radium/Radium.conf
+sudo printf 'rpcport=%s\n' $4 >> $HOME/.Radium/Radium.conf
+sudo printf 'rpcallowip=%s\n' $5 >> $HOME/.Radium/Radium.conf
+sudo printf 'server=1' >> $HOME/.Radium/Radium.conf
+
+
+#################################################################
+# Git Clone Radium Source                                       #
+#################################################################
+
+cd /usr/local
+time git clone https://github.com/tm2013/Radium.git
+chmod -R 777 /usr/local/Radium/
+
+#################################################################
+# Build Radium from source                                      #
+#################################################################
+
+cd /usr/local/Radium/src 
+make -f makefile.unix USE_UPNP=-
+cp /usr/local/Radium/src/Radiumd /usr/bin/Radiumd
+
+################################################################
+# Configure Radium node to auto start at boot       #
+#################################################################
+
+printf '%s\n%s\n' '#!/bin/sh' '/usr/bin/Radiumd --rpc-endpoint=127.0.0.1:8090 -d /usr/local/Radium/programs/radiumd/'>> /etc/init.d/radium
+chmod +x /etc/init.d/radium
+update-rc.d radium defaults
+/usr/bin/Radiumd --rpc-endpoint=127.0.0.1:8090  & exit 0
