a1e5fd3aa60466bef65f2ed95282b52331d31fa2
Julian Yap <julianokyap@gmail.com>
2016-03-21 10:01:24 -1000

    Create binary install method and set From_Binaries install option as default. Change default VM size to Standard_A1. Set random RPC password.

--- jumbucks-blockchain-ubuntu/build_jbs.sh
+++ jumbucks-blockchain-ubuntu/build_jbs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set -e 
+set -x
+
+echo "initializing Jumbucks installation"
 
 date
 ps axjf
@@ -8,54 +10,63 @@ ps axjf
 
 if [ $1 = 'From_Source' ]; then
 #################################################################
-# Update Ubuntu and install prerequisites for running Jumbucks    #
+# Update Ubuntu and install prerequisites for running Jumbucks  #
 #################################################################
 sudo apt-get update
 #################################################################
-# Build Jumbucks from source                                      #
+# Build Jumbucks from source                                    #
 #################################################################
 NPROC=$(nproc)
 echo "nproc: $NPROC"
 #################################################################
-# Install all necessary packages for building Jumbucks            #
+# Install all necessary packages for building Jumbucks          #
 #################################################################
-sudo apt-get install -y qt4-qmake libqt4-dev libminiupnpc-dev libdb++-dev libdb-dev libcrypto++-dev libqrencode-dev libboost-all-dev build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libssl-dev ufw git
+sudo apt-get -y install git build-essential libssl-dev libdb-dev libdb++-dev libboost-all-dev libqrencode-dev pwgen
 sudo add-apt-repository -y ppa:bitcoin/bitcoin
 sudo apt-get update
 sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 
-cd /usr/local
-file=/usr/local/Jumbucks
-if [ ! -e "$file" ]
-then
-	sudo git clone https://github.com/jyap808/jumbucks.git Jumbucks
-fi
+cd /usr/local/src/
+sudo git clone https://github.com/jyap808/jumbucks.git
 
-cd /usr/local/Jumbucks/src
-file=/usr/local/Jumbucks/src/jumbucksd
+cd /usr/local/jumbucks/src
+file=/usr/local/jumbucks/src/jumbucksd
 if [ ! -e "$file" ]
 then
 	sudo make -j$NPROC -f makefile.unix
 fi
 
-sudo cp /usr/local/Jumbucks/src/jumbucksd /usr/bin/jumbucksd
+sudo cp /usr/local/jumbucks/src/jumbucksd /usr/bin/jumbucksd
+
+else
+## Download Binaries
+sudo apt-get update
+sudo apt-get -y install git build-essential libssl-dev libdb-dev libdb++-dev libboost-all-dev libqrencode-dev pwgen
+cd /usr/local/src/
+DOWNLOADFILE=$(curl -s https://api.github.com/repos/jyap808/jumbucks/releases | grep browser_download_url | grep linux64 | head -n 1 | cut -d '"' -f 4)
+DOWNLOADNAME=$(curl -s https://api.github.com/repos/jyap808/jumbucks/releases | grep name | grep linux64 | head -n 1 | cut -d '"' -f 4)
+DIRNAME=$(echo $DOWNLOADNAME | sed 's/.tar.gz//')
+sudo wget $DOWNLOADFILE
+sudo tar zxf $DOWNLOADNAME
+sudo cp $DIRNAME/jumbucksd /usr/bin/jumbucksd
 fi
 
 ################################################################
-# Configure to auto start at boot		               #
+# Configure to auto start at boot		                       #
 ################################################################
 file=$HOME/.jumbucks 
 if [ ! -e "$file" ]
 then
 	sudo mkdir $HOME/.jumbucks
 fi
-printf '%s\n%s\n%s\n%s\n' 'daemon=1' 'server=1' 'rpcuser=u' 'rpcpassword=p' | sudo tee $HOME/.jumbucks/jumbucks.conf
-file=/etc/init.d/Jumbucks
+rpcp=$(pwgen -ncsB 35 1)
+printf '%s\n%s\n%s\nrpcpassword=%s\n' 'daemon=1' 'server=1' 'rpcuser=jumbucksrpc' $rpcp | sudo tee $HOME/.jumbucks/jumbucks.conf
+file=/etc/init.d/jumbucks
 if [ ! -e "$file" ]
 then
-	printf '%s\n%s\n' '#!/bin/sh' 'sudo jumbucksd' | sudo tee /etc/init.d/Jumbucks
-	sudo chmod +x /etc/init.d/Jumbucks
-	sudo update-rc.d Jumbucks defaults	
+	printf '%s\n%s\n' '#!/bin/sh' 'sudo jumbucksd' | sudo tee /etc/init.d/jumbucks
+	sudo chmod +x /etc/init.d/jumbucks
+	sudo update-rc.d jumbucks defaults
 fi
 
 /usr/bin/jumbucksd
