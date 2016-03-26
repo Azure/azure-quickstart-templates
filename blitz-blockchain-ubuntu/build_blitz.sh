75f3741c5c832299063c511f7f86f19ec646d522
KemBits <bitalize@gmail.com>
2016-03-26 10:28:09 -0400

    Update build_blitz.sh

--- blitz-blockchain-ubuntu/build_blitz.sh
+++ blitz-blockchain-ubuntu/build_blitz.sh
@@ -18,9 +18,9 @@ echo "nproc: $NPROC"
 # Install all necessary packages for building Blitz             #
 #################################################################
 sudo apt-get install -y git qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev-tools build-essential libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libevent-dev libminiupnpc-dev libqrencode-dev
-#sudo add-apt-repository -y ppa:bitcoin/bitcoin
-#sudo apt-get update
-#sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+sudo add-apt-repository -y ppa:bitcoin/bitcoin
+sudo apt-get update
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 
 cd /usr/local
 file=/usr/local/blitz
