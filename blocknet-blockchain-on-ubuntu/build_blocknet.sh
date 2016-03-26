855b55a4043f3b3f29991cab7be2a82995830c5a
sigwo <steven@sigwo.com>
2016-03-25 21:15:35 -0400

    Update build_blocknet.sh

--- blocknet-blockchain-on-ubuntu/build_blocknet.sh
+++ blocknet-blockchain-on-ubuntu/build_blocknet.sh
@@ -17,7 +17,7 @@ echo "nproc: $NPROC"
 #################################################################
 # Install all necessary packages for building Blocknet          #
 #################################################################
-sudo apt-get install -y --force qt4-qmake libqt4-dev libminiupnpc-dev libdb++-dev libdb-dev libcrypto++-dev libqrencode-dev libboost-all-dev build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libssl-dev ufw git
+sudo apt-get install -y libminiupnpc-dev libdb++-dev libdb-dev libcrypto++-dev libqrencode-dev libboost-all-dev build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev git
 sudo add-apt-repository -y ppa:bitcoin/bitcoin
 sudo apt-get update
 sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
