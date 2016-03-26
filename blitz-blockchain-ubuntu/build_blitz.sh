e0d9ae9bc352cb1538166dbb10160d06afa5fca6
KemBits <bitalize@gmail.com>
2016-03-26 10:06:11 -0400

    Update build_blitz.sh

--- blitz-blockchain-ubuntu/build_blitz.sh
+++ blitz-blockchain-ubuntu/build_blitz.sh
@@ -17,16 +17,16 @@ echo "nproc: $NPROC"
 #################################################################
 # Install all necessary packages for building Blitz             #
 #################################################################
-sudo apt-get install -y --force qt4-qmake libqt4-dev libminiupnpc-dev libdb++-dev libdb-dev libcrypto++-dev libqrencode-dev libboost-all-dev build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev build-essential libtool autotools-dev autoconf pkg-config libssl-dev libssl-dev libdb++-dev libssl-dev ufw git libevent-dev
-sudo add-apt-repository -y ppa:bitcoin/bitcoin
-sudo apt-get update
-sudo apt-get install -y --force libdb4.8-dev libdb4.8++-dev
+sudo apt-get install -y git qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev-tools build-essential libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libevent-dev libminiupnpc-dev libqrencode-dev
+#sudo add-apt-repository -y ppa:bitcoin/bitcoin
+#sudo apt-get update
+#sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 
 cd /usr/local
 file=/usr/local/blitz
 if [ ! -e "$file" ]
 then
-	sudo git clone https://bitbucket.org/blitz-dev/blitz-public.git blitz
+	sudo git clone https://github.com/KemBits/blitz-coin.git blitz
 fi
 
 cd /usr/local/blitz/src
