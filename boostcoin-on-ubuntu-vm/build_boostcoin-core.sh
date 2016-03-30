746ac1d8a0f8a11958326ec29726b43d551028d3
mammix2 <mammix2@hotmail.com>
2016-03-30 13:35:49 +0100

    Main deployment updates

--- boostcoin-on-ubuntu-vm/build_boostcoin-core.sh
+++ boostcoin-on-ubuntu-vm/build_boostcoin-core.sh
@@ -5,22 +5,23 @@ set -e
 date
 ps axjf
 
-
-if [ $1 = 'From_Source' ]; then
-#################################################################
+########################################################################
 # Update Ubuntu and install prerequisites for running BoostCoin-core   #
-#################################################################
+########################################################################
 sudo apt-get update
-#################################################################
+sudo apt-get upgrade -y
+########################################################################
 # Build BoostCoin-core from source                                     #
-#################################################################
+########################################################################
 NPROC=$(nproc)
 echo "nproc: $NPROC"
-#################################################################
+########################################################################
 # Install all necessary packages for building BoostCoin-core           #
-#################################################################
-sudo apt-get -y install qt4-qmake libqt4-dev build-essential libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb4.8-dev libdb4.8++-dev libminiupnpc-dev libevent-dev git
+########################################################################
+sudo apt-get -y install qt4-qmake libqt4-dev build-essential libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libminiupnpc-dev libevent-dev git
+sudo add-apt-repository -y ppa:bitcoin/bitcoin
 sudo apt-get update
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 
 cd /usr/local
 file=/usr/local/boostcoin-core
@@ -29,7 +30,7 @@ then
 	sudo git clone https://github.com/mammix2/boostcoin-core.git
 fi
 
-cd /usr/local/boostcoin-core
+cd /usr/local/boostcoin-core/src
 file=/usr/local/boostcoin-core/src/boostcoind
 if [ ! -e "$file" ]
 then
@@ -37,27 +38,18 @@ then
 	sudo strip boostcoind 
 fi
 
-sudo cp /usr/local/boostcoin/src/boostcoind /usr/bin/boostcoind
+sudo cp /usr/local/boostcoin-core/src/boostcoind /usr/bin/boostcoind
 
-else    
+
 #################################################################
-# Install BoostCoin-core from PPA                                      #
+# Configure to auto start at boot				#
 #################################################################
-sudo add-apt-repository -y ppa:boostcoin/boostcoin
-sudo apt-get update
-sudo apt-get install -y boostcoin
-
-fi
-
-################################################################
-# Configure to auto start at boot					    #
-################################################################
 file=$HOME/.boostcoin 
 if [ ! -e "$file" ]
 then
 	sudo mkdir $HOME/.boostcoin
 fi
-printf '%s\n%s\n%s\n%s\n' 'daemon=1' 'server=1' 'rpcuser=u' 'rpcpassword=p' | sudo tee $HOME/.boostcoin/boostcoin.conf
+printf '%s\n%s\n%s\n%s\n%s\n' 'daemon=1' 'server=1' 'rpcuser=f343gf88ja1WWEQ53t!!sg' 'rpcpassword=aRudu!MMetgdhheg1' 'torproxy=1' | sudo tee $HOME/.boostcoin/boostcoin.conf
 file=/etc/init.d/boostcoind
 if [ ! -e "$file" ]
 then
@@ -66,6 +58,6 @@ then
 	sudo update-rc.d boostcoind defaults	
 fi
 
-/usr/bin/boostcoind
+sudo /usr/bin/boostcoind
 echo "BoostCoin-core has been setup successfully and is running..."
 exit 0
