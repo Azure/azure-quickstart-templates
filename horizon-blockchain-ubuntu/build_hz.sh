4351967240339c8836a33ed33e90beefeb7c2fb0
sigwo <steven@sigwo.com>
2016-03-29 22:15:30 -0400

    Update build_hz.sh

--- horizon-blockchain-ubuntu/build_hz.sh
+++ horizon-blockchain-ubuntu/build_hz.sh
@@ -6,13 +6,11 @@ set -e
 # Build Horizon from latest release                             #
 # Install all necessary packages for building Horizon           #
 #################################################################
-sudo apt-get install -y software-properties-common
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> sudo /etc/apt/sources.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> sudo /etc/apt/sources.list
+sudo apt-get install -y software-properties-common unzip
+sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
-echo sudo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo apt-get install -y --force-yes oracle-java8-installer
-
 cd /usr/local
 sudo wget https://github.com/NeXTHorizon/hz-source/releases/download/hz-v5.4/hz-v5.4-node.zip
 sudo unzip hz-v5.4-node.zip
@@ -20,16 +18,9 @@ sudo unzip hz-v5.4-node.zip
 ################################################################
 # Configure to auto start at boot                              #
 ################################################################
-
-file=/etc/init.d/Horizon
-if [ ! -e "$file" ]
-then
-        printf '%s\n%s\n' '#!/bin/sh' 'sudo nohup ./usr/local/hz-v5.4-node/run.sh &' | sudo tee /etc/init.d/Horizon
-        sudo chmod +x /etc/init.d/Horizon
-        sudo update-rc.d Horizon default
-fi
-
+echo "#!/bin/bash sudo nohup ./usr/local/hz-v5.4-node/run.sh &" | sudo tee /etc/init.d/Horizon
 cd /usr/local/hz-v5.4-node/
 sudo nohup ./run.sh &
 echo "Horizon has been setup successfully and is running..."
+echo "Thanks!"
 exit 0
