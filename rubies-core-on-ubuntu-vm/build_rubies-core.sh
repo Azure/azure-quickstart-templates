cbda5ba181413642fc8e9e5b5b90de72c8c1050e
mammix2 <mammix2@hotmail.com>
2016-03-30 07:34:44 -0700

    Rubies - Updates

--- rubies-core-on-ubuntu-vm/build_rubies-core.sh
+++ rubies-core-on-ubuntu-vm/build_rubies-core.sh
@@ -18,7 +18,7 @@ echo "nproc: $NPROC"
 #####################################################################
 # Install all necessary packages for building Rubies                #
 #####################################################################
-sudo apt-get -y install qt4-qmake libqt4-dev build-essential libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libminiupnpc-dev git
+sudo apt-get -y install qt4-qmake libqt4-dev build-essential libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libminiupnpc-dev git
 sudo add-apt-repository -y ppa:bitcoin/bitcoin
 sudo apt-get update
 sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
@@ -29,6 +29,9 @@ if [ ! -e "$file" ]
 then
 	sudo git clone https://github.com/BetterBetsLobos/rubies-core.git
 fi
+
+cd /usr/local/rubies-core/src/leveldb
+sudo chmod +x ./build_detect_platform
 
 cd /usr/local/rubies-core/src
 file=/usr/local/rubies-core/src/rubiesd
@@ -49,7 +52,7 @@ if [ ! -e "$file" ]
 then
 	sudo mkdir $HOME/.rubies
 fi
-printf '%s\n%s\n%s\n%s\n' 'daemon=1' 'server=1' 'rpcuser=j34GG%^dfhekk88fhgeger4' 'rpcpassword=asdfgh4erfvhjy5rxEFH!!!!ff' | sudo tee $HOME/.rubies/rubies.conf
+printf '%s\n%s\n%s\n%s\n' 'daemon=1' 'server=1' 'rpcuser=j34GGdfhekk88fhgeger4' 'rpcpassword=asdfgh4erfvhjy5rxEFH!!!!ff' | sudo tee $HOME/.rubies/rubies.conf
 file=/etc/init.d/rubiesd
 if [ ! -e "$file" ]
 then
