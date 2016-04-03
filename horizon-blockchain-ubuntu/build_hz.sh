68b44847c98b3701bca55ddbeca26385f3c697e9
sigwo <steven@sigwo.com>
2016-04-03 11:48:11 -0400

    Update build_hz.sh

--- horizon-blockchain-ubuntu/build_hz.sh
+++ horizon-blockchain-ubuntu/build_hz.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-set -e
-#################################################################
-# Build Horizon from latest release                             #
-# Install all necessary packages for building Horizon           #
-#################################################################
 sudo apt-get update
 sudo apt-get install -y software-properties-common unzip
 sudo add-apt-repository ppa:webupd8team/java -y
@@ -13,11 +8,7 @@ sudo apt-get install -y --force-yes oracle-java8-installer
 cd /usr/local
 sudo wget https://github.com/NeXTHorizon/hz-source/releases/download/hz-v5.4/hz-v5.4-node.zip
 sudo unzip hz-v5.4-node.zip
-
-################################################################
-# Configure to auto start at boot                              #
-################################################################
-echo "#!/bin/bash sudo nohup ./usr/local/hz-v5.4-node/run.sh &" | sudo tee /etc/init.d/Horizon
+#echo "#!/bin/bash sudo nohup ./usr/local/hz-v5.4-node/run.sh &" | sudo tee /etc/init.d/Horizon
 cd /usr/local/hz-v5.4-node/
 sudo nohup ./run.sh &
 echo "Horizon has been setup successfully and is running..."
