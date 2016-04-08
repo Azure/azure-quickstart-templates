6de89ae56d6134332ea58c237ae678d64e591a41
Sergii Vakula <sv@aspanta.com>
2016-04-08 16:05:37 +0000

    fix

--- emercoin-ubuntu/configure-emercoin.sh
+++ emercoin-ubuntu/configure-emercoin.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sudo npm install azure-cli -g
+sudo update-alternatives --install /usr/bin/node nodejs /usr/bin/nodejs 100
+
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv B58C58F4
 sudo add-apt-repository 'deb http://download.aspanta.com/ubuntu trusty emercoin'
 sudo apt-get -y update
