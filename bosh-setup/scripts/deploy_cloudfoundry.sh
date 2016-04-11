978221fc92f67b210855280f2c896f6692adfca7
Bin Xia <binxi@microsoft.com>
2016-04-11 02:27:40 +0000

    Skip uploading if exists

--- bosh-setup/scripts/deploy_cloudfoundry.sh
+++ bosh-setup/scripts/deploy_cloudfoundry.sh
@@ -7,9 +7,28 @@ fi
 
 set -e
 
-stemcell_url=`grep azure-hyperv-ubuntu-trusty-go_agent bosh.yml | awk '{print $2}'`
-bosh upload stemcell $stemcell_url
-bosh upload release REPLACE_WITH_CF_RELEASE_URL
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+eval $(parse_yaml bosh.yml "config_")
+
+stemcell_url=$config_resource_pools_stemcell_url
+
+bosh upload stemcell $stemcell_url --skip-if-exists
+bosh upload release REPLACE_WITH_CF_RELEASE_URL --skip-if-exists
 
 manifest=$1
 bosh deployment $manifest
