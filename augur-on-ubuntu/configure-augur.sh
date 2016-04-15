11a21a0f7991c8710d8c8d2e9e97f56394e0d42e
Kevin Day <kevin.r.day@gmail.com>
2016-04-15 17:51:47 -0500

    geth now starts and mines in screen (commented out for now.) Working on configuring augur front end

--- augur-on-ubuntu/configure-augur.sh
+++ augur-on-ubuntu/configure-augur.sh
@@ -19,54 +19,54 @@ echo "User home dir: $HOMEDIR"
 echo "vmname: $VMNAME"
 
 #####################
-# setup the Azure CLI
+# install tools
 #####################
 time sudo npm install azure-cli -g
-time sudo update-alternatives --install /usr/bin/node nodejs /usr/bin/nodejs 100
+time sudo apt-get install nodejs-legacy
+time sudo apt-get update && sudo apt-get install screen -y
+time sudo apt-get update && sudo apt-get install npm -y
+time sudo npm install grunt --save-dev
+time sudo npm install -g grunt-cli
 
 ####################
-# Setup Geth
+# Intsall Geth
 ####################
-time sudo apt-get -y git
+time sudo apt-get -y install git
 time sudo apt-get install -y software-properties-common
 time sudo add-apt-repository -y ppa:ethereum/ethereum
 time sudo add-apt-repository -y ppa:ethereum/ethereum-dev
 time sudo apt-get update
 time sudo apt-get install -y ethereum
 
-####################
-# Install sol compiler
-####################
-time sudo add-apt-repository ppa:ethereum/ethereum -y
-time sudo apt-get update
-time sudo apt-get install solc -y
-
+###############################
 # Fetch Genesis and Private Key
+###############################
 cd $HOMEDIR
-wget https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/go-ethereum-on-ubuntu/genesis.json
-wget https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/go-ethereum-on-ubuntu/priv_genesis.key
+wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/genesis.json
+wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/priv_genesis.key
 
-
+####################
+# Setup Geth
+####################
 geth init genesis.json
-echo "password" > pw.txt  #TODO:prompt for separate pw, or just pass in one from auguruser?
+echo "password" > pw.txt  #TODO:prompt for separate pw in tempalte, or just pass in one from auguruser?
 geth --password pw.txt account import priv_genesis.key
  
 #Pregen DAG so miniing can start immediately, no delay between when front end is useable
 mkdir ~/.ethash
-geth makedag 0 ~/.ethash
+#geth makedag 0 ~/.ethash
 
-geth --maxpeers 0 --networkid 1101011 --rpc --rpccorsdomain "*" console
-
+#start geth+mining using screen
+#screen -dmS geth geth --unlock --maxpeers 0 --networkid 1101011 --rpc --rpccorsdomain "*" --mine
 
 ####################
 #Install Augur Front End
 ####################
-
 git clone https://github.com/AugurProject/augur.git
 cd augur
-npm install
-grunt
-npm start
+#npm install
+#grunt
+#npm start
 
 date
 echo "completed geth install $$"
