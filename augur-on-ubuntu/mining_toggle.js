fb558b6140c33e5debf86550487c7079acfd4f47
Kevin Day <kevin.r.day@gmail.com>
2016-04-19 14:08:49 -0500

    adding script to toggle mining on/off when transactions appear

---
+++ augur-on-ubuntu/mining_toggle.js
@@ -0,0 +1,17 @@
+var mining_threads = 1
+
+function checkWork() {
+    if (eth.getBlock("pending").transactions.length > 0) {
+        if (eth.mining) return;
+        console.log("== Pending transactions! Mining...");
+        miner.start(mining_threads);
+    } else {
+        miner.stop(0);  // This param means nothing
+        console.log("== No transactions! Mining stopped.");
+    }
+}
+
+eth.filter("latest", function(err, block) { checkWork(); });
+eth.filter("pending", function(err, block) { checkWork(); });
+
+checkWork();
