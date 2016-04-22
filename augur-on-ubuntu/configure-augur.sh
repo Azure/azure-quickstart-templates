728df79c0e5316117f8bd3281fdf16e843da9dd9
Kevin Day <kevin.r.day@gmail.com>
2016-04-22 16:01:27 -0500

    submitting work so far

--- augur-on-ubuntu/configure-augur.sh
+++ augur-on-ubuntu/configure-augur.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # print commands and arguments as they are executed
-set -xv
+#set -x
 
 echo "initializing geth installation"
 date
-ps axjf
 
 #############
 # Parameters
@@ -14,6 +13,7 @@ ps axjf
 AZUREUSER=$1
 HOMEDIR="/home/$AZUREUSER"
 VMNAME=`hostname`
+ETHDIR="$HOMEDIR/.ethereum"
 echo "User: $AZUREUSER"
 echo "User home dir: $HOMEDIR"
 echo "vmname: $VMNAME"
@@ -22,10 +22,10 @@ echo "vmname: $VMNAME"
 # install tools
 #####################
 time sudo apt-get update && sudo apt-get install npm -y
-time sudo npm install azure-cli -g
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 time sudo apt-get update && sudo apt-get install screen -y
 time sudo apt-get -y install git
+time sudo apt-get -y install libssl-dev
 
 ####################
 # Intsall Geth
@@ -33,6 +33,7 @@ time sudo apt-get -y install git
 time sudo apt-get install -y software-properties-common
 time sudo add-apt-repository -y ppa:ethereum/ethereum
 time sudo add-apt-repository -y ppa:ethereum/ethereum-dev
+time sudo apt-get update
 time sudo apt-get install -y ethereum
 
 ####################
@@ -40,7 +41,7 @@ time sudo apt-get install -y ethereum
 ####################
 time sudo apt-get install -y python-dev
 time sudo apt-get install -y python-pip
-time apt-get install -y build-essential automake pkg-config libtool libffi-dev libgmp-dev -y
+time sudo apt-get install -y build-essential automake pkg-config libtool libffi-dev libgmp-dev -y
 time sudo pip install ethereum-serpent
 time sudo pip install ethereum
 time sudo pip install requests --upgrade
@@ -51,7 +52,7 @@ time sudo apt-get update
 ###############################
 # Fetch Genesis and Private Key
 ###############################
-cd $HOMEDIR
+#cd $HOMEDIR
 wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/genesis.json
 wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/priv_genesis.key
 wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/mining_toggle.js
@@ -59,22 +60,22 @@ wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/maste
 ####################
 # Setup Geth
 ####################
-geth init genesis.json
-
+geth init genesis.json 
 echo "password" > pw.txt  #TODO:prompt for separate pw in tempalte, or just pass in one from auguruser?
-geth --password pw.txt account import priv_genesis.key¯
- 
+geth --password pw.txt account import priv_genesis.key
+
 #Pregen DAG so miniing can start immediately, no delay between when front end is useable
-mkdir .ethash #todo: this fails..
-#geth makedag 0 ~/.ethash
+mkdir .ethash
+geth makedag 0 .ethash
+
 
 #start geth+mining using screen
-#screen -dmS geth geth --password ~/pw.txt --unlock 0 --preload ~/mining_toggle.js --maxpeers 0 --networkid 1101011 --rpc --rpccorsdomain "*" console
+screen -dmS geth sudo geth --password $HOMEDIR/pw.txt --unlock 0 --maxpeers 0 --networkid 1101011 --rpc --rpccorsdomain "*" js $HOMEDIR/mining_toggle.js
 
 ####################
 #Install Augur Front End
 ####################
-git clone https://github.com/AugurProject/augur.git
+#git clone https://github.com/AugurProject/augur.git
 #cd augur
 #sudo npm install
 #npm start
@@ -82,8 +83,7 @@ git clone https://github.com/AugurProject/augur.git
 ####################
 #Install Augur Contracts
 ####################
-cd $HOMEDIR
-git clone https://github.com/AugurProject/augur-core.git
+#git clone https://github.com/AugurProject/augur-core.git
 #cd  augur-core
 #python load_contracts.py
 
