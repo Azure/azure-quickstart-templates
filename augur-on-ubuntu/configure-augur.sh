d6460a3b8d10bb8d66ab2f24b94f812af7f8d22f
Kevin Day <kevin.r.day@gmail.com>
2016-04-26 10:07:30 -0500

    blank script for testing

--- augur-on-ubuntu/configure-augur.sh
+++ augur-on-ubuntu/configure-augur.sh
@@ -18,54 +18,54 @@ echo "vmname: $VMNAME"
 #####################
 # install tools
 #####################
-time sudo apt-get update && sudo apt-get install npm -y
-sudo ln -s /usr/bin/nodejs /usr/bin/node
-time sudo apt-get update && sudo apt-get install screen -y
-time sudo apt-get -y install git
-time sudo apt-get -y install libssl-dev
-time curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-time sudo apt-get install -y nodejs
+#time sudo apt-get update && sudo apt-get install npm -y
+#sudo ln -s /usr/bin/nodejs /usr/bin/node
+#time sudo apt-get update && sudo apt-get install screen -y
+#time sudo apt-get -y install git
+#time sudo apt-get -y install libssl-dev
+#time curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+#time sudo apt-get install -y nodejs
 
 ####################
 # Intsall Geth
 ####################
-time sudo apt-get install -y software-properties-common
-time sudo add-apt-repository -y ppa:ethereum/ethereum
-time sudo add-apt-repository -y ppa:ethereum/ethereum-dev
-time sudo apt-get update
-time sudo apt-get install -y ethereum
+#time sudo apt-get install -y software-properties-common
+#time sudo add-apt-repository -y ppa:ethereum/ethereum
+#time sudo add-apt-repository -y ppa:ethereum/ethereum-dev
+#time sudo apt-get update
+#time sudo apt-get install -y ethereum
 
 ####################
 # Install Serpent
 ####################
-time sudo apt-get install -y python-dev
-time sudo apt-get install -y python-pip
-time sudo apt-get install -y build-essential automake pkg-config libtool libffi-dev libgmp-dev -y
-time sudo pip install ethereum-serpent
-time sudo pip install ethereum
-time sudo pip install requests --upgrade
-time sudo pip install pyethapp
+#time sudo apt-get install -y python-dev
+#time sudo apt-get install -y python-pip
+#time sudo apt-get install -y build-essential automake pkg-config libtool libffi-dev libgmp-dev -y
+#time sudo pip install ethereum-serpent
+#time sudo pip install ethereum
+#time sudo pip install requests --upgrade
+#time sudo pip install pyethapp
 
-time sudo apt-get update
+#time sudo apt-get update
 
 ###############################
 # Fetch Genesis and Private Key
 ###############################
-cd /root
-wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/genesis.json
-wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/priv_genesis.key
-wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/mining_toggle.js
-wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/geth.conf
+#cd /root
+#wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/genesis.json
+#wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/priv_genesis.key
+#wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/mining_toggle.js
+#wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/geth.conf
 
 ####################
 # Setup Geth
 ####################
-geth init genesis.json 
-echo "password" > pw.txt
-geth --password pw.txt account import priv_genesis.key
+#geth init genesis.json 
+#echo "password" > pw.txt
+#geth --password pw.txt account import priv_genesis.key
 
 #Pregen DAG so miniing can start immediately
-mkdir .ethash
+#mkdir .ethash
 #geth makedag 0 .ethash
 
 #make geth a service, turn on.
@@ -83,12 +83,12 @@ mkdir .ethash
 ####################
 #Install Augur Front End
 ####################
-export HOME=/root
-git clone https://github.com/AugurProject/augur.git
-cd augur
-npm install
-npm run build
-npm start
+#export HOME=/root
+#git clone https://github.com/AugurProject/augur.git
+#cd augur
+#npm install
+#npm run build
+#npm start
 
 
 date
