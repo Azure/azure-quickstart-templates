4a282a210f600482e47bb99c61646d8eecae33b0
Shuiping He (Wicresoft) <v-shuihe@microsoft.com>
2016-04-29 16:09:50 +0800

    allows users to choose data disks

--- mongodb-replica-set-centos/scripts/primary.sh
+++ mongodb-replica-set-centos/scripts/primary.sh
@@ -7,7 +7,9 @@ mongoAdminUser=$4
 mongoAdminPasswd=$5
 
 
-#create repo
+install_mongo3() {
+
+	#create repo
 cat > /etc/yum.repos.d/mongodb-org-3.2.repo <<EOF
 [mongodb-org-3.2]
 name=MongoDB Repository
@@ -16,26 +18,87 @@ gpgcheck=0
 enabled=1
 EOF
 
-#install
-yum install -y mongodb-org
+	#install
+	yum install -y mongodb-org
 
-#ignore update
-sed -i '$a exclude=mongodb-org,mongodb-org-server,mongodb-org-shell,mongodb-org-mongos,mongodb-org-tools' /etc/yum.conf
+	#ignore update
+	sed -i '$a exclude=mongodb-org,mongodb-org-server,mongodb-org-shell,mongodb-org-mongos,mongodb-org-tools' /etc/yum.conf
 
-#disable selinux
-sed -i '/^[^#]/s/\(SELINUX=\)\([a-z]\+\)/\1disabled/' /etc/sysconfig/selinux
-setenforce 0
+	#disable selinux
+	sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/sysconfig/selinux
+	sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+	setenforce 0
 
-#kernel settings
-if [[ -f /sys/kernel/mm/transparent_hugepage/enabled ]];then
-    echo never > /sys/kernel/mm/transparent_hugepage/enabled
-fi
-if [[ -f /sys/kernel/mm/transparent_hugepage/defrag ]];then
-    echo never > /sys/kernel/mm/transparent_hugepage/defrag
-fi
+	#kernel settings
+	if [[ -f /sys/kernel/mm/transparent_hugepage/enabled ]];then
+		echo never > /sys/kernel/mm/transparent_hugepage/enabled
+	fi
+	if [[ -f /sys/kernel/mm/transparent_hugepage/defrag ]];then
+		echo never > /sys/kernel/mm/transparent_hugepage/defrag
+	fi
 
-#configure
-sed -i 's/\(bindIp\)/#\1/' /etc/mongod.conf
+	#configure
+	sed -i 's/\(bindIp\)/#\1/' /etc/mongod.conf
+}
+
+disk_format() {
+	cd /tmp
+	for ((j=1;j<=3;j++))
+	do
+		wget http://mirrors.blob.core.chinacloudapi.cn/tools/vm-disk-utils-0.1.sh 
+		if [[ -f /tmp/vm-disk-utils-0.1.sh ]]; then
+			bash /tmp/vm-disk-utils-0.1.sh -b /var/lib/mongo -s
+			if [[ $? -eq 0 ]]; then
+				sed -i 's/disk1//' /etc/fstab
+				umount /var/lib/mongo/disk1
+				mount /dev/md0 /var/lib/mongo
+			fi
+			break
+		else
+			echo "download vm-disk-utils-0.1.sh failed. try again."
+			continue
+		fi
+	done
+		
+}
+
+install_zabbix() {
+	#install zabbix agent
+	cd /tmp
+	yum install -y gcc wget > /dev/null 
+	wget http://jaist.dl.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/2.2.5/zabbix-2.2.5.tar.gz > /dev/null
+	tar zxvf zabbix-2.2.5.tar.gz
+	cd zabbix-2.2.5
+	groupadd zabbix
+	useradd zabbix -g zabbix -s /sbin/nologin
+	mkdir -p /usr/local/zabbix
+	./configure --prefix=/usr/local/zabbix --enable-agent
+	make install > /dev/null
+	cp misc/init.d/fedora/core/zabbix_agentd /etc/init.d/
+	sed -i 's/BASEDIR=\/usr\/local/BASEDIR=\/usr\/local\/zabbix/g' /etc/init.d/zabbix_agentd
+	sed -i '$azabbix-agent    10050/tcp\nzabbix-agent    10050/udp' /etc/services
+	sed -i '/^LogFile/s/tmp/var\/log/' /usr/local/zabbix/etc/zabbix_agentd.conf
+	hostName=`hostname`
+	sed -i "s/^Hostname=Zabbix server/Hostname=$hostName/" /usr/local/zabbix/etc/zabbix_agentd.conf
+	if [[ $zabbixServer =~ ([0-9]{1,3}.){3}[0-9]{1,3} ]];then
+		sed -i "s/^Server=127.0.0.1/Server=$zabbixServer/" /usr/local/zabbix/etc/zabbix_agentd.conf
+		sed -i "s/^ServerActive=127.0.0.1/ServerActive=$zabbixServer/" /usr/local/zabbix/etc/zabbix_agentd.conf
+		sed -i "s/^Server=127.0.0.1/Server=$zabbixServer/" /usr/local/zabbix/etc/zabbix_agent.conf
+	fi
+	touch /var/log/zabbix_agentd.log
+	chown zabbix:zabbix /var/log/zabbix_agentd.log
+
+	#start zabbix agent
+	chkconfig --add zabbix_agentd
+	chkconfig zabbix_agentd on
+	/etc/init.d/zabbix_agentd start
+}
+
+
+install_mongo3
+disk_format
+install_zabbix
+
 
 #start mongod
 mongod --dbpath /var/lib/mongo/ --logpath /var/log/mongodb/mongod.log --fork
@@ -63,8 +126,7 @@ fi
 #stop mongod
 sleep 15
 MongoPid=`ps -ef |grep -v grep |grep mongod|awk '{print $2}'`
-pkill mongod
-
+kill -2 $MongoPid
 
 
 #set keyfile
@@ -81,51 +143,14 @@ if [[ -z $MongoPid1 ]];then
     echo "shutdown mongod successfully"
 else
     echo "shutdown mongod failed!"
-    pkill mongod
+    kill $MongoPid1
     sleep 15
 fi
 
 #restart mongod with auth and replica set
 mongod --dbpath /var/lib/mongo/ --replSet $replSetName --logpath /var/log/mongodb/mongod.log --fork --config /etc/mongod.conf
 
-
-
-
-
-#install zabbix agent
-cd /tmp
-yum install -y gcc wget > /dev/null 
-wget http://jaist.dl.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/2.2.5/zabbix-2.2.5.tar.gz > /dev/null
-tar zxvf zabbix-2.2.5.tar.gz
-cd zabbix-2.2.5
-groupadd zabbix
-useradd zabbix -g zabbix -s /sbin/nologin
-mkdir -p /usr/local/zabbix
-./configure --prefix=/usr/local/zabbix --enable-agent
-make install > /dev/null
-cp misc/init.d/fedora/core/zabbix_agentd /etc/init.d/
-sed -i 's/BASEDIR=\/usr\/local/BASEDIR=\/usr\/local\/zabbix/g' /etc/init.d/zabbix_agentd
-sed -i '$azabbix-agent    10050/tcp\nzabbix-agent    10050/udp' /etc/services
-sed -i '/^LogFile/s/tmp/var\/log/' /usr/local/zabbix/etc/zabbix_agentd.conf
-hostName=`hostname`
-sed -i "s/^Hostname=Zabbix server/Hostname=$hostName/" /usr/local/zabbix/etc/zabbix_agentd.conf
-if [[ $zabbixServer =~ ([0-9]{1,3}.){3}[0-9]{1,3} ]];then
-    sed -i "s/^Server=127.0.0.1/Server=$zabbixServer/" /usr/local/zabbix/etc/zabbix_agentd.conf
-    sed -i "s/^ServerActive=127.0.0.1/ServerActive=$zabbixServer/" /usr/local/zabbix/etc/zabbix_agentd.conf
-    sed -i "s/^Server=127.0.0.1/Server=$zabbixServer/" /usr/local/zabbix/etc/zabbix_agent.conf
-fi
-touch /var/log/zabbix_agentd.log
-chown zabbix:zabbix /var/log/zabbix_agentd.log
-
-#start zabbix agent
-chkconfig --add zabbix_agentd
-chkconfig zabbix_agentd on
-/etc/init.d/zabbix_agentd start
-
-
 #initiate replica set
-
-
 for((i=1;i<=3;i++))
     do
         sleep 15
@@ -174,3 +199,46 @@ for((i=1;i<=$secondaryNodes;i++))
             echo "adding server 10.0.1.${a} failed!"
         fi
     done
+	
+	
+#set mongod auto start
+cat > /etc/init.d/mongod1 <<EOF
+#!/bin/bash
+#chkconfig: 35 84 15
+#description: mongod auto start
+. /etc/init.d/functions
+
+Name=mongod1
+start() {
+if [[ ! -d /var/run/mongodb ]];then
+mkdir /var/run/mongodb
+chown -R mongod:mongod /var/run/mongodb
+fi
+mongod --dbpath /var/lib/mongo/ --replSet $replSetName --logpath /var/log/mongodb/mongod.log --fork --config /etc/mongod.conf
+}
+stop() {
+pkill mongod
+}
+restart() {
+stop
+sleep 15
+start
+}
+
+case "\$1" in 
+    start)
+	start;;
+	stop)
+	stop;;
+	restart)
+	restart;;
+	status)
+	status \$Name;;
+	*)
+	echo "Usage: service mongod1 start|stop|restart|status"
+esac
+EOF
+chmod +x /etc/init.d/mongod1
+chkconfig mongod1 on
+
+
