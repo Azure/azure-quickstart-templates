9865e4f70713dbc60dab45f3ffe39db12568d7d1
Kay Singh <singhkay@users.noreply.github.com>
2016-04-28 19:07:23 -0700

    Update install.sh

--- nylas-email-sync-engine/install.sh
+++ nylas-email-sync-engine/install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+set -x
+echo "Initializing Nylas Open Source Sync Engine installation"
+
+#############
+# Parameters
+#############
+AZUREUSER=$1
+HOME="/home/$AZUREUSER"
+echo "User: $AZUREUSER"
+echo "User home dir: $HOME"
 
 # update os
 apt-get update
@@ -7,11 +17,15 @@ apt-get update
 apt-get install -y git
 
 # clone the sync engine repo
-cd ~
-cd /usr/local
+cd $HOME
+
 git clone https://github.com/singhkay/sync-engine.git
-cd /usr/local/sync-engine/
+/bin/bash -l -c "chown -R $AZUREUSER $HOME/sync-engine"
+
+cd $HOME/sync-engine/
 
 # kick off the setup script
-chmod +x ./setup.sh
-sudo ./setup.sh
+/bin/bash -l -c "chmod +x ./setup.sh"
+/bin/bash -l -c "sudo ./setup.sh"
+
+/bin/bash -l -c "nohup bin/inbox-start &"
