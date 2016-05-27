a36326c1470e88f92dc918ba4cb3be5c4001c5a3
Brijraj singh <brijrajsingh@gmail.com>
2016-05-27 20:55:56 +0530

    Fixed silent installation of Java packages

--- 101-lap-neo4j-ubuntu/install-neo4j.sh
+++ 101-lap-neo4j-ubuntu/install-neo4j.sh
@@ -11,12 +11,13 @@ apt-get -y update
 export DEBIAN_FRONTEND=noninteractive
 
 #install java
-sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get install -y python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get -y install oracle-java8-installer
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
 
 #install neo4j server
 sudo apt-get -y install neo4j
-service neo4j-service start
 
 
