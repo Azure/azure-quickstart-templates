7d7bf1df4d3f2d343ce37b0a16ba424e77e2836f
Brijraj singh <brijrajsingh@gmail.com>
2016-05-30 11:32:29 +0530

    updated the composer install process

--- 101-lap-neo4j-ubuntu/install-lap.sh
+++ 101-lap-neo4j-ubuntu/install-lap.sh
@@ -11,7 +11,13 @@ apt-get -y install apache2
 cd /var/www/html
 #install composer
 #curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/var/www/html
+#curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/var/www/html
+echo "starting to install composer"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '070854512ef404f16bac87071a6db9fd9721da1684cd4589b1196c3faf71b9a2682e2311b36a5079825e155ac7ce150d') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+echo "Composer is installed"
 
 apt-get -y install libapache2-mod-php5
 
