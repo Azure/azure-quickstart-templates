986b3bc5f9e496df9298640950d07eb3df26f4ae
Jamie D <daltskin@hotmail.com>
2016-06-01 14:29:23 +0100

    Update SetupChocolatey.ps1

--- visual-studio-dev-vm-chocolatey/SetupChocolatey.ps1
+++ visual-studio-dev-vm-chocolatey/SetupChocolatey.ps1
@@ -1,44 +1,61 @@
 param([Parameter(Mandatory=$true)][string]$chocoPackages)
-
 cls
-# Grab the choco installation script
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#New-Item "c:\jdchoco" -type Directory -force | Out-Null
+#$LogFile = "c:\jdchoco\JDScript.log"
+#$chocoPackages | Out-File $LogFile -Append
 
 # Get username/password & machine name
-#$userName = "artifactInstaller"
-#[Reflection.Assembly]::LoadWithPartialName("System.Web") 
-#$password = $([System.Web.Security.Membership]::GeneratePassword(12,4))
-#$cn = [ADSI]"WinNT://$env:ComputerName"
+$userName = "artifactInstaller"
+[Reflection.Assembly]::LoadWithPartialName("System.Web") | Out-Null
+$password = $([System.Web.Security.Membership]::GeneratePassword(12,4))
+$cn = [ADSI]"WinNT://$env:ComputerName"
 
 # Create new user
-#$user = $cn.Create("User", $userName)
-#$user.SetPassword($password)
-#$user.SetInfo()
-#$user.description = "Choco artifact installer"
-#$user.SetInfo()
+$user = $cn.Create("User", $userName)
+$user.SetPassword($password)
+$user.SetInfo()
+$user.description = "Choco artifact installer"
+$user.SetInfo()
 
 # Add user to the Administrators group
-#$group = [ADSI]"WinNT://$env:ComputerName/Administrators,group"
-#$group.add("WinNT://$env:ComputerName/$userName")
+$group = [ADSI]"WinNT://$env:ComputerName/Administrators,group"
+$group.add("WinNT://$env:ComputerName/$userName")
 
-#$secPassword = ConvertTo-SecureString $password -AsPlainText -Force
-#$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($username)", $secPassword)
-
-# Create installation command for the passed in packages
-#$command = "cinst " + $chocoPackages + " -y -force"
-#$sb = [scriptblock]::Create("$command")
-
-# Run Chocolatey as the artifactInstaller user
-#Enable-PSRemoting -Force -SkipNetworkProfileCheck
+# Create pwd and new $creds for remoting
+$secPassword = ConvertTo-SecureString $password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($username)", $secPassword)
 
 # Ensure that current process can run scripts. 
-#Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force 
+#"Enabling remoting" | Out-File $LogFile -Append
+Enable-PSRemoting -Force -SkipNetworkProfileCheck
 
-#Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential
-#Disable-PSRemoting -Force
+#"Changing ExecutionPolicy" | Out-File $LogFile -Append
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+
+# Install Choco
+#"Installing Chocolatey" | Out-File $LogFile -Append
+$sb = { iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) }
+Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential | Out-Null
+
+#"Disabling UAC" | Out-File $LogFile -Append
+$sb = { Set-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -name EnableLua -value 0 }
+Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential
+
+#"Install each Chocolatey Package"
+$chocoPackages.Split(";") | ForEach {
+    $command = "cinst " + $_ + " -y -force"
+    $command | Out-File $LogFile -Append
+    $sb = [scriptblock]::Create("$command")
+
+    # Use the current user profile
+    Invoke-Command -ScriptBlock $sb -ArgumentList $chocoPackages -ComputerName $env:COMPUTERNAME -Credential $credential | Out-Null
+}
+
+Disable-PSRemoting -Force
 
 # Delete the artifactInstaller user
-#$cn.Delete("User", $userName)
+$cn.Delete("User", $userName)
 
 # Delete the artifactInstaller user profile
-#gwmi win32_userprofile | where { $_.LocalPath -like "*$userName*" } | foreach { $_.Delete() }
+gwmi win32_userprofile | where { $_.LocalPath -like "*$userName*" } | foreach { $_.Delete() }
