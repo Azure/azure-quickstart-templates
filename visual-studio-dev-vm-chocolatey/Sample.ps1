e8f0027cbb6648e330c1916cd243e61112581df1
Jamie D <daltskin@hotmail.com>
2016-06-01 14:28:55 +0100

    Update Sample.ps1

--- visual-studio-dev-vm-chocolatey/Sample.ps1
+++ visual-studio-dev-vm-chocolatey/Sample.ps1
@@ -2,7 +2,8 @@
 $RGName = "RG-VS-Dev";
 $VMName = "jdvs2015vm";
 $VMUsername = "jmd";
-$ChocoPackages = "'linqpad;sysinternals;agentransack;beyondcompare;fiddler4;visualstudiocode;imageresizerapp;gimp'";
+$DeployLocation = "West Europe"
+$ChocoPackages = "linqpad;sysinternals;agentransack;beyondcompare;fiddler4;visualstudiocode;imageresizerapp;gimp";
 $ARMTemplate = "C:\@SourceControl\GitHub\ARMChocolatey\AzureDeploy.json"
 
 # 1. Login
@@ -13,7 +14,7 @@ New-AzureRmResourceGroup -Name $RGName -Location $DeployLocation -Force
 
 #3. Create resources within RG
 $sw = [system.diagnostics.stopwatch]::startNew()
-New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile $ARMTemplate -vmName $VMName -vmAdminUserName $VMUsername -dnsLabelPrefix $VMName -vmVisualStudioVersion VS-2015-Ent-AzureSDK-2.8-WS2012R2 -chocoPackages $ChocoPackages -Mode Complete -Force 
+New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile $ARMTemplate -deployLocation $DeployLocation -vmName $VMName -vmAdminUserName $VMUsername -vmIPPublicDnsName $VMName -chocoPackages $ChocoPackages -Mode Complete -Force 
 $sw | Format-List -Property *
 
 #4. Get the RDP file
