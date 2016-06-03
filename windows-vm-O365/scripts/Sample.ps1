21dd3ee86190eaca6281f7b5e246e21da3a613f6
Jamie D <daltskin@hotmail.com>
2016-06-03 16:12:27 +0100

    Reset names

--- windows-vm-O365/scripts/Sample.ps1
+++ windows-vm-O365/scripts/Sample.ps1
@@ -1,16 +1,16 @@
 ﻿cls
-$RGName = "RG-o365-W8"; 
-$VMName = "o365devw8";
+$RGName = "RG-o365-W7"; 
+$VMName = "o365devw7";
 $VMUsername = "o365dev";
 
 $ARMTemplatePath = (get-item $PSScriptRoot).parent.FullName + "\azuredeploy.json"
-$DeployLocation = "North Europe"
+$DeployLocation = "West Europe"
 
 # 1. Set which version of Office you want installed Office2013 or Office 2016
 $OfficeVersion  = "Office2013"; # Office2016
 
 # 2. Login
-Login-AzureRmAccount
+#Login-AzureRmAccount
 
 # 3. Create a resource group
 New-AzureRmResourceGroup -Name $RGName -Location $DeployLocation -Force
@@ -18,7 +18,7 @@ New-AzureRmResourceGroup -Name $RGName -Location $DeployLocation -Force
 # 4. Create resources - select which vmOSVersion you want to use
 $sw = [system.diagnostics.stopwatch]::startNew()
 New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile $ARMTemplatePath -vmName $VMName `
-    -vmAdminUserName $VMUsername -dnsLabelPrefix $VMName -vmOSVersion 8.1-Enterprise-N `
+    -vmAdminUserName $VMUsername -dnsLabelPrefix $VMName -vmOSVersion 7.0-Enterprise-N `
     -officeVersion $OfficeVersion -Mode Complete -Force | Out-Null
 $sw | Format-List -Property *
 
