b359579bc74f41dbf15b76f2a6a168df984055ce
Kevin Day <kevin.r.day@gmail.com>
2016-06-10 15:24:54 -0500

    Updating ui deployment

--- augur-on-ubuntu/configure-augur.sh
+++ augur-on-ubuntu/configure-augur.sh
@@ -9,7 +9,6 @@ date
 #############
 # Parameters
 #############
-
 AZUREUSER=$1
 LOCATION=$2
 VMNAME=`hostname`
@@ -27,6 +26,8 @@ cd $HOMEDIR
 time sudo apt-get install -y build-essential automake pkg-config libtool libffi-dev libgmp-dev
 time sudo apt-get -y install git
 time sudo apt-get -y install libssl-dev
+time curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+time sudo apt-get -y install nodejs
 
 ####################
 # Intsall Geth
@@ -55,8 +56,8 @@ time sudo apt-get update
 sudo -u $AZUREUSER wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/genesis.json
 sudo -u $AZUREUSER wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/priv_genesis.key
 sudo -u $AZUREUSER wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/mining_toggle.js
-sudo -u $AZUREUSER wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/web/augur-on-ubuntu/geth.conf
-sudo -u $AZUREUSER wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/web/augur-on-ubuntu/augur_ui.conf
+sudo -u $AZUREUSER wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/geth.conf
+sudo -u $AZUREUSER wget https://raw.githubusercontent.com/kevinday/azure-quickstart-templates/master/augur-on-ubuntu/augur_ui.conf
 sudo -u $AZUREUSER sed -i "s/auguruser/$AZUREUSER/g" geth.conf
 sudo -u $AZUREUSER sed -i "s/auguruser/$AZUREUSER/g" augur_ui.conf
 
@@ -87,7 +88,10 @@ sudo -i -u $AZUREUSER geth makedag 0 .ethash
 sudo -i -u $AZUREUSER git clone https://github.com/AugurProject/augur-core.git
 cd  augur-core/load_contracts
 python load_contracts.py
-cd ..
+contracts="`python generate_gospel.py -j`"
+contracts=${contracts//\"/\\\"}
+contracts=${contracts//[$'\t\r\n ']}
+cd ../..
 
 ####################
 #Make a swap file (node can get hungry)
@@ -102,17 +106,25 @@ swapon /swapfile
 ####################
 sudo -i -u $AZUREUSER git clone https://github.com/AugurProject/augur.git
 sudo -i -u $AZUREUSER mkdir ui
-sudo -i -u $AZUREUSER cp -r augur/azure/2.0.0 ui/2.0.0
+udo -i -u $AZUREUSER cp -r augur/azure ui
 rm -rf augur
-#TODO: search/replace UI.
+sudo -u $AZUREUSER find ui -type f -exec sed -i "s|\"{{ \$BUILD_AZURE_WSURL }}\"|null|g" {} \;
+sudo -u $AZUREUSER find ui -type f -exec sed -i "s|{{ \$BUILD_AZURE_LOCALNODE }}|$ETHEREUM_HOST_RPC|g" {} \;
+sudo -u $AZUREUSER find ui -type f -exec sed -i "s|\"{{ \$BUILD_AZURE_CONTRACTS }}\"|'$contracts'|g" {} \;
+
+
+###################
+#Install augur webserver
+####################
+git clone https://github.com/AugurProject/augur-ui-webserver.git
+sudo -i -u $AZUREUSER  bash -c "cd augur-ui-webserver; npm install"
 
 #allow nodejs to run on port 80 w/o sudo
 setcap 'cap_net_bind_service=+ep' /usr/bin/nodejs
 
-#TODO: modify to serve static files.
 #Make augur_ui a service, turn on.
-#cp augur_ui.conf /etc/init/
-#start augur_ui 
+cp augur_ui.conf /etc/init/
+start augur_ui 
 
 date
 echo "completed augur install $$"
