221e6c78cf802ab4c925103dcaf6c9194ccc682d
Sebastian Max Dusch <sedusch@microsoft.com>
2016-06-20 18:54:29 +0200

    remove mdadm

--- sap-2-tier-marketplace-image/shared/configureSAPVM.sh
+++ sap-2-tier-marketplace-image/shared/configureSAPVM.sh
@@ -47,91 +47,8 @@ function getdevicepath()
 	log "getdevicepath done"
 }
 
-function createmdadm()
-{	
-	log "createmdadm"
-	
-	lunsA=(${1//,/ })	
-	mdadmPath=$2
-	mountPath=$3
-	
-	arraynum=${#lunsA[@]}
-	echo "count $arraynum"
-	if [[ $arraynum -gt 1 ]]
-	then
-		log "createmdadm - creating mdadm"
-		
-		numRaidDevices=0
-		raidDevices=""
-		num=${#lunsA[@]}
-		log "num luns $num"
-		for ((i=0; i<num; i++))
-		do
-			log "trying to find device path"
-			lun=${lunsA[$i]}
-			getdevicepath $lun
-			devicePath=$getdevicepathresult;
-			if [ -n "$devicePath" ];
-			then
-				log " Device Path is $devicePath"
-				numRaidDevices=$((numRaidDevices + 1))
-				raidDevices="$raidDevices $devicePath""1 "
-				# http://superuser.com/questions/332252/creating-and-formating-a-partition-using-a-bash-script
-				$(echo -e "n\np\n1\n\n\nw" | fdisk $devicePath)
-				log "changing partition type"
-				$(echo -e "t\nfd\nw" | fdisk $devicePath)
-			else
-				log "no device path for LUN $lun"
-				exit -1;
-			fi
-		done
-		log "num: $numRaidDevices paths: '$raidDevices'"
-		$(mdadm --create $mdadmPath --level 0 --raid-devices $numRaidDevices $raidDevices --force)
-		$(mkfs -t xfs $mdadmPath)
-
-		local vLinux=$(cat /etc/os-release)		
-		if [[ $vLinux =~ ID=\"rhel\" ]];
-		then
-			#RHEL
-			log "createmdadm - RHEL"
-			$(chkconfig --add boot.md)
-			$(echo 'DEVICE /dev/sd*[0-9]' >> /etc/mdadm.conf)
-		fi
-
-		$(mkdir $mountPath)
-		addtofstab $mdadmPath		
-	else
-		log "createmdadm - creating single disk"
-		
-		lun=${lunsA[0]}
-		getdevicepath $lun;
-		devicePath=$getdevicepathresult;
-		if [ -n "$devicePath" ];
-		then
-			log " Device Path is $devicePath"
-			# http://superuser.com/questions/332252/creating-and-formating-a-partition-using-a-bash-script
-			$(echo -e "n\np\n1\n\n\nw" | fdisk $devicePath)
-			partPath="$devicePath""1"
-			$(mkfs -t xfs $partPath)
-			$(mkdir $mountPath)	
-
-			addtofstab $partPath
-		else
-			log "no device path for LUN $lun"
-			exit -1;
-		fi
-	fi
-
-	log "createmdadm done"
-}
-
 function createlvm()
 {
-	# pvcreate /dev/sdf
-	# vgcreate data-vg01 /dev/sd[cde]
- 	# lvcreate --extents 100%FREE --stripes 3 --name data-lv01 data-vg01
-	# mkfs -t ext4 /dev/data-vg01/data-lv01
-
 	log "createlvm"
 	
 	lunsA=(${1//,/ })	
@@ -220,12 +137,10 @@ done
 
 if [[ -n "$dbluns" ]];
 then
-	#createmdadm $dbluns "/dev/md127" "/$dbname";
 	createlvm $dbluns "vg-$dbname" "lv-$dbname" "/$dbname";
 fi
 
 if [[ -n "$logluns" ]];
 then
-	#createmdadm $logluns "/dev/md128" "/$logname";
 	createlvm $logluns "vg-$logname" "lv-$logname" "/$logname";
 fi
