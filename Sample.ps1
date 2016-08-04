8dc40d5f3aa88c9c2f375bfacefd6b45fcf74696
Jamie D <daltskin@hotmail.com>
2016-08-04 13:59:39 +0100

    Changes as per code review

--- Sample.ps1
+++ Sample.ps1
@@ -1,9 +1,8 @@
-﻿cls
-$RGName = "RG-o365-W7"; 
-$VMName = "o365devw7";
+﻿$RGName = "PR-devo365-W7"; 
+$VMName = "jdo365devw7";
 $VMUsername = "o365dev";
 
-$ARMTemplatePath = (get-item $PSScriptRoot).parent.FullName + "\azuredeploy.json"
+$ARMTemplatePath = (get-item $PSScriptRoot).parent.FullName + "\Choco\visual-studio-dev-vm-O365\azuredeploy.json"
 $DeployLocation = "West Europe"
 
 # 1. Set which version of Office you want installed Office2013 or Office 2016
@@ -11,17 +10,15 @@ $OfficeVersion  = "Office2013"; # Office2016
 
 # 2. Login
 #Login-AzureRmAccount
+Write-Host $ARMTemplatePath 
 
 # 3. Create a resource group
 New-AzureRmResourceGroup -Name $RGName -Location $DeployLocation -Force
 
 # 4. Create resources - select which vmOSVersion you want to use
 $sw = [system.diagnostics.stopwatch]::startNew()
-New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile $ARMTemplatePath -vmName $VMName `
-    -vmAdminUserName $VMUsername -dnsLabelPrefix $VMName -vmOSVersion 7.0-Enterprise-N `
-    -officeVersion $OfficeVersion -Mode Complete -Force | Out-Null
+New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile $ARMTemplatePath -vmAdminUserName $VMUsername -dnsLabelPrefix $VMName -vmVisualStudioVersion VS-2015-Comm-VSU3-AzureSDK-291-Win10-N -officeVersion $OfficeVersion -Mode Complete -Force | Out-Null
 $sw | Format-List -Property *
 
 # 5. Get the RDP file
 Get-AzureRmRemoteDesktopFile -ResourceGroupName $RGName -Name $VMName -Launch
-
