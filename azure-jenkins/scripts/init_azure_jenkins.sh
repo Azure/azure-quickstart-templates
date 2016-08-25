bb2df558370fc17aaa7c14cfa1ea399e2520e8fc
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2016-08-24 17:53:11 -0700

    fix arm template to download correct setup script

--- azure-jenkins/scripts/init_azure_jenkins.sh
+++ azure-jenkins/scripts/init_azure_jenkins.sh
@@ -2,11 +2,12 @@
 
 CURRENT_USER=$(whoami)
 SETUP_SCRIPTS_LOCATION="/tmp/"
-SETUP_SCRIPT="initSetup.sh"
+SETUP_SCRIPT="config_storage.sh"
 SOURCE_URI="https://raw.githubusercontent.com/arroyc/azure-quickstart-templates/master/azure-jenkins/setup-scripts/"
 
 sudo apt-get -y update
 sudo apt-get -y install jq
+sudo npm install -y -g azure-cli
 
 #downloading initSetup script
 sudo wget -O $SETUP_SCRIPTS_LOCATION$SETUP_SCRIPT $SOURCE_URI$SETUP_SCRIPT
