f610be9d8079260001f088616f39dd5e1a393b51
consultguru <gururaj.pandurangi@gmail.com>
2016-08-25 18:18:42 -0700

    Trils for Copy Blob
    Works only for blobs with no spaces in their names.

---
+++ avyan-vm-script-copy-storage-blob/TestImageTransferPS.ps1
@@ -0,0 +1,3 @@
+﻿Set-ExecutionPolicy Unrestricted 
+
+Invoke-Expression ".\ImageTransfer.ps1 -SourceImage https://avyanignite201611.blob.core.windows.net/datameersamples/Log%20Format.xlsx -SourceSAKey 2EGPt9o4h070nqDRMo83iq4MfTOISrg/I4oYr4uuOj3JFUqWhjdPpG11Cjs+9HIioYY9S7yClZQsCj0EoXCmiQ== -DestinationURI https://avyanhdicopyblob1sa.blob.core.windows.net/datameersamples/Log%20Format.xlsx -DestinationSAKey dAN6+VAtV2S14OUNTPTAyI1Kwfl/ly75RFd0XovxIm65GirVZWl4lDoVbiAwZT0V0bBvE1O+hlu532rxZ2oGkw==" -Verbose
