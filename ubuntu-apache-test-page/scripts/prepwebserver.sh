3a011edbd38d1468f5e4994de3b49e89c74e51c8
Aleksei Polkovnikov <alpolko@outlook.com>
2016-08-28 12:40:05 +0200

    PHP-related updates

--- ubuntu-apache-test-page/scripts/prepwebserver.sh
+++ ubuntu-apache-test-page/scripts/prepwebserver.sh
@@ -4,5 +4,9 @@ sudo apt-get update
 sudo apt-get install -y apache2
 if [ "$1" = "true" ]; then
 	sudo apt-get install -y php5
+	if [ "$3" = "index.php" ] ; then
+		sudo rm /var/www/html/index.html
+	fi
+	sudo service apache2 restart
 fi
 echo $2 | sudo tee /var/www/html/$3
