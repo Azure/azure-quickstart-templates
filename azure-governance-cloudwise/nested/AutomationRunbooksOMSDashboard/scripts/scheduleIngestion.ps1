cab92cd462118066b1482466954f423e9c0cd7ab
consultguru <gururaj.pandurangi@gmail.com>
2016-09-01 00:43:46 -0700

    Adding SQLDB OMS integration

--- azure-governance-cloudwise/nested/AutomationRunbooksOMSDashboard/scripts/scheduleIngestion.ps1
+++ azure-governance-cloudwise/nested/AutomationRunbooksOMSDashboard/scripts/scheduleIngestion.ps1
@@ -14,10 +14,38 @@ Select-AzureRmSubscription -SubscriptionId $Conn.SubscriptionID -TenantId $Conn.
 
 $AAResourceGroup = Get-AutomationVariable -name "AzureAutomationResourceGroup"
 $AAAccount = Get-AutomationVariable -name "AzureAutomationAccount"
+
+
+# ---------- Create Schedules for WebApp ---------------
+
 $RunbookName = "webAzureIngestion"
 $ScheduleName = "webappazure"
 
 $RunbookStartTime = $Date = $([DateTime]::Now.AddMinutes(10))
+
+[int]$RunFrequency = 10
+$NumberofSchedules = 60 / $RunFrequency
+"$NumberofSchedules schedules will be created"
+
+$Count = 0
+While ($count -lt $NumberofSchedules)
+{
+    $count ++
+
+    "Creating schedule $ScheduleName-$Count for $RunbookStartTime for runbook $RunbookName"
+    $Schedule = New-AzureRmAutomationSchedule -Name "$ScheduleName-$Count" -StartTime $RunbookStartTime -HourInterval 1 -AutomationAccountName $AAAccount -ResourceGroupName $AAResourceGroup
+    $Sch = Register-AzureRmAutomationScheduledRunbook -RunbookName $RunbookName -AutomationAccountName $AAAccount -ResourceGroupName $AAResourceGroup -ScheduleName "$ScheduleName-$Count"
+    $RunbookStartTime = $RunbookStartTime.AddMinutes($RunFrequency)
+}
+
+
+# ---------- Create Schedules for SQLAzure ---------------
+
+
+$RunbookName = "sqlazureIngestion"
+$ScheduleName = "sqlazure"
+
+$RunbookStartTime = $Date = $([DateTime]::Now.AddMinutes(11))
 
 [int]$RunFrequency = 10
 $NumberofSchedules = 60 / $RunFrequency
