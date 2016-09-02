c5ceaf1ba2befb50f9d68571527a86c509ad6aa5
Michael Marchenko <mimarche@microsoft.com>
2016-09-01 19:22:25 -0700

    test script

--- rds-update-rdsh-collection/Scripts/Test.ps1
+++ rds-update-rdsh-collection/Scripts/Test.ps1
@@ -8,6 +8,19 @@ param(
 	[string]$password 
 	)
 
+get-date
+
+$collection = 'Desktop Collection'
+
 write-host "username: $username;  password:  $password"
 
 whoami
+
+ipmo remotedesktop
+
+get-rdserver
+
+get-rdsessionhost -collectionname $collection
+
+get-rdusersession -collectionname $collection
+
