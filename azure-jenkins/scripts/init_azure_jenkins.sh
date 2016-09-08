7d1901d9a6a16b2ede9611fd615b124e5744e07b
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2016-09-08 11:30:38 -0700

    added setup_azure_credentials script in download list

--- azure-jenkins/scripts/init_azure_jenkins.sh
+++ azure-jenkins/scripts/init_azure_jenkins.sh
@@ -4,37 +4,15 @@ CURRENT_USER=$(whoami)
 SETUP_SCRIPTS_LOCATION="/opt/azure_jenkins_config/"
 SETUP_SCRIPT="config_storage.sh"
 CLEAN_STORAGE_SCRIPT="clear_storage_config.sh"
+SETUP_AZURE_CREDENTIALS="set_azure_credentials.sh"
 SOURCE_URI="https://raw.githubusercontent.com/arroyc/azure-quickstart-templates/master/azure-jenkins/setup-scripts/"
 
-# #java
-# echo "installing java"
-# sudo apt-get -y update
-# sudo apt-get -y install openjdk-7-jre
-# sudo apt-get -y install openjdk-7-jdk
+#azure-cli
+sudo npm install -y -g azure-cli
 
-# #npm
-# echo "installing npm"
-
-# sudo apt-get -y update
-# sudo apt-get -y install nodejs-legacy
-
-# sudo apt-get -y install npm
-
- #azure-cli
- echo "installing azure-cli"
- sudo npm install -y -g azure-cli
-
- #install jq
- sudo apt-get -y update
- sudo apt-get -y install jq
-
-# # install jenkins
-# echo  "Installing Jenkins CI Server"
-
-# wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-# sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-# sudo apt-get -y update
-# sudo apt-get -y install jenkins
+#install jq
+sudo apt-get -y update
+sudo apt-get -y install jq
 
 #delete any existing config script
 sudo rm -f /opt/config_storage.sh
@@ -42,9 +20,13 @@ sudo rm -f /opt/config_storage.sh
 if [ ! -d "$SETUP_SCRIPTS_LOCATION" ]; then
   sudo mkdir $SETUP_SCRIPTS_LOCATION
   
-  #downloading clear_storage_config script
+  #downloading set_azure_credentials script
   sudo wget -O $SETUP_SCRIPTS_LOCATION$CLEAN_STORAGE_SCRIPT $SOURCE_URI$CLEAN_STORAGE_SCRIPT
   sudo chmod +x $SETUP_SCRIPTS_LOCATION$CLEAN_STORAGE_SCRIPT
+  
+  #downloading clear_storage_config script
+  sudo wget -O $SETUP_SCRIPTS_LOCATION$SETUP_AZURE_CREDENTIALS $SOURCE_URI$SETUP_AZURE_CREDENTIALS
+  sudo chmod +x $SETUP_SCRIPTS_LOCATION$SETUP_AZURE_CREDENTIALS
   
   #downloading storage_config script
   sudo wget -O $SETUP_SCRIPTS_LOCATION$SETUP_SCRIPT $SOURCE_URI$SETUP_SCRIPT
