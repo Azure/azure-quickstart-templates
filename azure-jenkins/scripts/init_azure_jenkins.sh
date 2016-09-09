53f3d6388ece400c9cadd7602c9fc750132d583b
Huanhuan Sun <husun@microsoft.com>
2016-09-08 17:04:51 -0700

    Fix scripts names

--- azure-jenkins/scripts/init_azure_jenkins.sh
+++ azure-jenkins/scripts/init_azure_jenkins.sh
@@ -2,7 +2,7 @@
 
 CURRENT_USER=$(whoami)
 SETUP_SCRIPTS_LOCATION="/opt/azure_jenkins_config/"
-SETUP_SCRIPT="config_azure.sh"
+CONFIG_AZURE_SCRIPT="config_azure.sh"
 CLEAN_STORAGE_SCRIPT="clear_storage_config.sh"
 SOURCE_URI="https://raw.githubusercontent.com/arroyc/azure-quickstart-templates/master/azure-jenkins/setup-scripts/"
 
@@ -20,12 +20,12 @@ if [ ! -d "$SETUP_SCRIPTS_LOCATION" ]; then
   sudo mkdir $SETUP_SCRIPTS_LOCATION
 
   #downloading clear_storage_config script
-  sudo wget -O $SETUP_SCRIPTS_LOCATION$SETUP_AZURE_CREDENTIALS $SOURCE_URI$SETUP_AZURE_CREDENTIALS
-  sudo chmod +x $SETUP_SCRIPTS_LOCATION$SETUP_AZURE_CREDENTIALS
+  sudo wget -O $SETUP_SCRIPTS_LOCATION$CLEAN_STORAGE_SCRIPT $SOURCE_URI$CLEAN_STORAGE_SCRIPT
+  sudo chmod +x $SETUP_SCRIPTS_LOCATION$CLEAN_STORAGE_SCRIPT
 
   #downloading config_azure script
-  sudo wget -O $SETUP_SCRIPTS_LOCATION$SETUP_SCRIPT $SOURCE_URI$SETUP_SCRIPT
-  sudo chmod +x $SETUP_SCRIPTS_LOCATION$SETUP_SCRIPT
+  sudo wget -O $SETUP_SCRIPTS_LOCATION$CONFIG_AZURE_SCRIPT $SOURCE_URI$CONFIG_AZURE_SCRIPT
+  sudo chmod +x $SETUP_SCRIPTS_LOCATION$CONFIG_AZURE_SCRIPT
 
 fi
 
