9b73571c0ad9302cf1b372b4aab08cbbc9b25cb4
Huanhuan Sun <husun@microsoft.com>
2016-09-14 14:23:03 -0700

    Try if the init script fails at some point which causes config script downloading not executed at all

--- azure-jenkins/scripts/set_azure_jenkins.sh
+++ azure-jenkins/scripts/set_azure_jenkins.sh
@@ -6,16 +6,6 @@ CONFIG_AZURE_SCRIPT="config_azure.sh"
 CLEAN_STORAGE_SCRIPT="clear_storage_config.sh"
 SOURCE_URI="https://raw.githubusercontent.com/arroyc/azure-quickstart-templates/master/azure-jenkins/setup-scripts/"
 
-#azure-cli
-sudo npm install -y -g azure-cli
-
-#install jq
-sudo apt-get -y update
-sudo apt-get -y install jq
-
-#delete any existing config script
-sudo rm -f /opt/config_storage.sh
-
 if [ ! -d "$SETUP_SCRIPTS_LOCATION" ]; then
   sudo mkdir $SETUP_SCRIPTS_LOCATION
 
@@ -28,5 +18,21 @@ if [ ! -d "$SETUP_SCRIPTS_LOCATION" ]; then
   sudo chmod +x $SETUP_SCRIPTS_LOCATION$CONFIG_AZURE_SCRIPT
 
 fi
+
+#azure-cli
+sudo npm install -y -g azure-cli
+
+#install jq
+sudo apt-get -y update
+sudo apt-get -y install jq
+
+#delete any existing config script
+
+old_config_storage_file="/opt/config_storage.sh"
+if [ -f $old_config_storage_file ]
+then
+  sudo rm -f $old_config_storage_file
+fi
+
 
 
