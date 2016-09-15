318f1bdff937422483295c5a89e58102774f8d1c
Huanhuan Sun <husun@microsoft.com>
2016-09-15 14:38:24 -0700

    Change the URI from our own fork to Azure

--- azure-jenkins/scripts/set_azure_jenkins.sh
+++ azure-jenkins/scripts/set_azure_jenkins.sh
@@ -4,7 +4,7 @@ CURRENT_USER=$(whoami)
 SETUP_SCRIPTS_LOCATION="/opt/azure_jenkins_config/"
 CONFIG_AZURE_SCRIPT="config_azure.sh"
 CLEAN_STORAGE_SCRIPT="clear_storage_config.sh"
-SOURCE_URI="https://raw.githubusercontent.com/arroyc/azure-quickstart-templates/master/azure-jenkins/setup-scripts/"
+SOURCE_URI="https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/azure-jenkins/setup-scripts/"
 
 if [ ! -d "$SETUP_SCRIPTS_LOCATION" ]; then
   sudo mkdir $SETUP_SCRIPTS_LOCATION
