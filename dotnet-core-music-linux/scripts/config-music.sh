184dcfdb03689dbb5e83f3f2426bede6f03af7b5
neilpeterson <neilpeterson@outlook.com>
2016-09-18 20:49:11 -0700

    tempalte and script

---
+++ dotnet-core-music-linux/scripts/config-music.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# install dotnet core
+sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
+sudo apt-get update
+sudo apt-get install -y dotnet-dev-1.0.0-preview2-003121
+
+# download application
+sudo wget https://raw.github.com/neilpeterson/nepeters-azure-templates/master/dotnet-core-music-linux-vm-sql-db/music-app/music-store-azure-demo-pub.tar /
+sudo mkdir /opt/music
+sudo tar -xf music-store-azure-demo-pub.tar -C /opt/music
+
+# install nginx, update config file
+sudo apt-get install -y nginx
+sudo service nginx start
+sudo touch /etc/nginx/sites-available/default
+sudo wget https://raw.githubusercontent.com/neilpeterson/nepeters-azure-templates/master/dotnet-core-music-linux-vm-sql-db/music-app/nginx-config/default -O /etc/nginx/sites-available/default
+sudo cp /opt/music/nginx-config/default /etc/nginx/sites-available/
+sudo nginx -s reload
+
+# update and secure music config file
+sed -i "s/<replaceserver>/$1/g" /opt/music/config.json
+sed -i "s/<replaceuser>/$2/g" /opt/music/config.json
+sed -i "s/<replacepass>/$3/g" /opt/music/config.json
+sudo chown $2 /opt/music/config.json
+sudo chmod 0400 /opt/music/config.json
+
+# config supervisor
+sudo apt-get install -y supervisor
+sudo touch /etc/supervisor/conf.d/music.conf
+sudo wget https://raw.githubusercontent.com/neilpeterson/nepeters-azure-templates/master/dotnet-core-music-linux-vm-sql-db/music-app/supervisor/music.conf -O /etc/supervisor/conf.d/music.conf
+sudo service supervisor stop
+sudo service supervisor start
+
+# pre-create music store database
+/usr/bin/dotnet /opt/music/MusicStore.dll &
