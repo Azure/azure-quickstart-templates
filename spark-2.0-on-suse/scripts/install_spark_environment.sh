dbd3f8ce37674114b3f087bc48bc0b4e728e7f55
Roberto G <roberto.grandi@gmail.com>
2016-09-20 18:44:33 +0200

    Project Renamed

--- spark-2.0-on-suse/scripts/install_spark_environment.sh
+++ spark-2.0-on-suse/scripts/install_spark_environment.sh
@@ -4,8 +4,10 @@ echo "Starting Spark Setup"
 
 ISMASTER="-1"
 MASTERIP="10.0.0.4"
+STORAGEACCOUNTKEY=""
+STORAGEACCOUNTNAME=""
 
-while getopts :k:m:d:s:h optname; do
+while getopts :m:i:k:a:d:s:h optname; do
     echo "Option $optname set with value ${OPTARG}"
   case $optname in
     m)  #Master 1 Slave 0
@@ -14,13 +16,33 @@ while getopts :k:m:d:s:h optname; do
 	i)
 	  MASTERIP=${OPTARG}
 	  ;;
+	k)
+	  STORAGEACCOUNTKEY=${OPTARG}
+	  ;;
+	a)
+	  STORAGEACCOUNTNAME=${OPTARG}
+	  ;;
   esac
 done
 
-echo "Using $MASTERIP as master ip"
+show_info()
+{
+	echo "Is a Master: $ISMASTER"
+	echo "Using WASB account name: $STORAGEACCOUNTNAME"
+	echo "Using WASB Key: $STORAGEACCOUNTKEY"
+	echo "Using $MASTERIP as master ip"
+}
+
+download_extra_libraries()
+{
+	wget -O /tmp/azure-storage-2.0.0.jar https://valueamplifypublic.blob.core.windows.net/public/azure-storage-2.0.0.jar
+	wget -O /tmp/hadoop-azure-2.7.2.jar https://valueamplifypublic.blob.core.windows.net/public/hadoop-azure-2.7.2.jar
+	
+	mv /tmp/*.jar /srv/spark/lib
+}
 
 install_prerequisites()
-{
+{	
 	echo "Working as user $USER"
 	echo "Updating Suse"
 	JAVAC=$(which javac)
@@ -28,6 +50,10 @@ install_prerequisites()
 		echo "Installing OpenJDK"
 		sudo zypper install -y java-1_8_0-openjdk
 	fi
+	
+	echo "Downloading external libraries"
+	download_extra_libraries
+	
 }
 
 setup_spark_env_and_defaults()
@@ -40,9 +66,9 @@ setup_spark_env_and_defaults()
 	
 	# Main Parameters
 	echo "export SPARK_MASTER_IP=\"$MASTERIP\"" >> spark-env.sh
-	echo 'export SPARK_MASTER_PORT=7077' >> spark-env.sh
+	echo "export SPARK_MASTER_PORT=7077" >> spark-env.sh
 	echo "export SPARK_PUBLIC_DNS=\"$MASTERIP\"" >> spark-env.sh
-	echo "export SPARK_EXECUTOR_INSTANCES=\"1\"" >> spark-env.sh
+	#echo "export SPARK_EXECUTOR_INSTANCES=\"1\"" >> spark-env.sh
 	
 	# Other Paramters
 	echo 'export SPARK_CONF_DIR="/usr/local/spark/conf"' >> spark-env.sh
@@ -52,13 +78,31 @@ setup_spark_env_and_defaults()
 	echo 'export SPARK_WORKER_DIR="/srv/spark/work"' >> spark-env.sh
 	echo 'export SPARK_LOCAL_DIRS="/srv/spark/tmp"' >> spark-env.sh
 	
+	# Hadoop Related Configs
+	echo 'export HADOOP_CONF_DIR="/usr/local/spark/conf"' >> spark-env.sh
+	
+	touch core-site.xml
+	
+	echo '<?xml version="1.0" encoding="UTF-8"?>' >> core-site.xml
+	echo '<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>' >> core-site.xml
+	echo '<configuration>' >> core-site.xml
+	echo '<property>' >> core-site.xml
+	echo "<name>fs.azure.account.key.$STORAGEACCOUNTNAME.blob.core.windows.net</name>" >> core-site.xml
+	echo "<value>$STORAGEACCOUNTKEY</value>" >> core-site.xml
+	echo '</property>' >> core-site.xml
+	echo '</configuration>' >> core-site.xml
+	
+	# Spark DEFAULTS
 	cp -p spark-defaults.conf.template spark-defaults.conf
 	touch spark-defaults.conf
 	
-	echo "spark.master            spark://10.0.0.4:7077" >> spark-defaults.conf
+	echo "spark.master            spark://$MASTERIP:7077" >> spark-defaults.conf
 	echo 'spark.executor.memory   512m' >> spark-defaults.conf
 	echo 'spark.eventLog.enabled  false' >> spark-defaults.conf
 	echo 'spark.serializer        org.apache.spark.serializer.KryoSerializer' >> spark-defaults.conf
+	
+	echo 'spark.driver.extraClassPath	/srv/spark/lib/*' >> spark-defaults.conf
+	echo 'spark.executor.extraClassPath	/srv/spark/lib/*' >> spark-defaults.conf
 	
 	cd ~
 }
@@ -91,11 +135,15 @@ install_spark()
 	rm -f ~/.ssh/id_rsa
 	ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa && cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 	
-	sudo mkdir -p /srv/spark/{logs,work,tmp,pids}
+	sudo mkdir -p /srv/spark/{logs,work,tmp,pids,lib}
 	sudo chown -R spark:spark /srv/spark
 	sudo chmod 4755 /srv/spark/tmp
 	
+	
+	download_extra_libraries
+	
 	setup_spark_env_and_defaults
+	
 	
 	sudo su spark
 	rm -f ~/.ssh/id_rsa 
@@ -113,6 +161,8 @@ install_spark()
 		./sbin/start-slave.sh spark://$MASTERIP:7077
 	fi
 }
+
+show_info > /tmp/install_info.log
 
 install_prerequisites > /tmp/install_prerequisites.log
 
