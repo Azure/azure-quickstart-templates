4b23b45ec3433499aaddd7ab4947c216612374c1
Kamran Khan <kakhan@microsoft.com>
2016-09-19 18:14:14 -0700

    Remove sudo, requires interactive terminal

--- 101-vm-full-disk-encrypted-rhel/setup_raid.sh
+++ 101-vm-full-disk-encrypted-rhel/setup_raid.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo yum install -y mdadm
-sudo mdadm --create --verbose /dev/md0 --level=0 --raid-devices=2 /dev/sdc /dev/sdd
-sudo mkdir -p /etc/mdadm
-sudo mdadm --detail --scan > /etc/mdadm/mdadm.conf
+yum install -y mdadm
+mdadm --create --verbose /dev/md0 --level=0 --raid-devices=2 /dev/sdc /dev/sdd
+mkdir -p /etc/mdadm
+mdadm --detail --scan > /etc/mdadm/mdadm.conf
