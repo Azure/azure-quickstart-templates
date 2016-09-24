30d5f35bdf06877737b4fbd9f2306bdb703ed184
Aleksei Polkovnikov <alpolko@outlook.com>
2016-09-24 21:40:39 +0200

    Creating bw meter template

---
+++ vm-to-vm-bandwidth-meter/scripts/testBandwidth.ps1
@@ -0,0 +1,25 @@
+Param(
+  [string]$TestIPPort,
+  [int]$TestNumber,
+  [string]$PacketSize
+)
+
+$AppFolder = "bandwidthmeter"
+$AppPath = [Environment]::GetFolderPath("CommonApplicationData")+"\bandwidthmeter"
+
+$PsToolsSourceURL = "https://download.sysinternals.com/files/PSTools.zip"
+$PsToolsArchive = $AppPath+"\PSTools.zip"
+$PsPingFileName = "psping.exe"
+
+if (!(Test-Path $AppPath)){
+    mkdir $AppPath | Out-Null
+    Invoke-WebRequest $PsToolsSourceURL -OutFile $PsToolsArchive
+
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($PsToolsArchive, $AppPath)
+    rm $PsToolsArchive 
+}
+
+
+cd $AppPath
+.\psping.exe -b -q -l $PacketSize -n $TestNumber $TestIPPort | Select-String -Pattern "Minimum = *"
