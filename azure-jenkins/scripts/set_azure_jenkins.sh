49864da522a004f2e8ba46684e97ed6fff419ad2
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2016-09-25 18:29:13 -0700

    testing create jenkins user from deployment

--- azure-jenkins/scripts/set_azure_jenkins.sh
+++ azure-jenkins/scripts/set_azure_jenkins.sh
@@ -6,6 +6,12 @@ CONFIG_AZURE_SCRIPT="config_azure.sh"
 CLEAN_STORAGE_SCRIPT="clear_storage_config.sh"
 SOURCE_URI="https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/azure-jenkins/setup-scripts/"
 
+#download jenkins-cli and secured jenkins config to create new user
+wget http://localhost:8080/jnlpJars/jenkins-cli.jar
+wget -O /var/lib/jenkins/config.xml https://arroycsafestorage.blob.core.windows.net/testsafe/config.xml
+ 
+echo "hpsr=new hudson.security.HudsonPrivateSecurityRealm(false); hpsr.createAccount('$1', '$2')" | java -jar jenkins-cli.jar -s http://localhost:8080 groovy =
+
 if [ ! -d "$SETUP_SCRIPTS_LOCATION" ]; then
   sudo mkdir $SETUP_SCRIPTS_LOCATION
 fi
@@ -32,5 +38,5 @@ then
   sudo rm -f $old_config_storage_file
 fi
 
-
+exit 0
 
