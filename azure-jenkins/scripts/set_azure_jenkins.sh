840c752466d3fbeff9495b4f335d9be119ee15ba
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2016-09-28 00:51:38 -0700

    Update set_azure_jenkins.sh

--- azure-jenkins/scripts/set_azure_jenkins.sh
+++ azure-jenkins/scripts/set_azure_jenkins.sh
@@ -29,13 +29,6 @@ sudo chmod +x $SETUP_SCRIPTS_LOCATION$CONFIG_AZURE_SCRIPT
 sudo wget -O $SETUP_SCRIPTS_LOCATION$CLEAN_STORAGE_SCRIPT $SOURCE_URI$CLEAN_STORAGE_SCRIPT
 sudo chmod +x $SETUP_SCRIPTS_LOCATION$CLEAN_STORAGE_SCRIPT
 
-#azure-cli
-#sudo npm install -y -g azure-cli
-
-#install jq
-#sudo apt-get -y update
-#sudo apt-get -y install jq
-
 #delete any existing config script
 old_config_storage_file="/opt/config_storage.sh"
 if [ -f $old_config_storage_file ]
