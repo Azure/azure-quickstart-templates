07feaee9624426677a14d62f8d34fd4320ff66c0
Aleksei Polkovnikov <alpolko@outlook.com>
2016-10-01 13:34:58 +0200

    Structuring and parametrizing test script

--- vm-disk-performance-meter/scripts/disktest.sh
+++ vm-disk-performance-meter/scripts/disktest.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
+#init and mount data disk
 (echo n; echo p; echo 1; echo; echo; echo w) | fdisk /dev/sdc > /dev/null 
 mkfs -t ext4 /dev/sdc1 > /dev/null 
 mkdir /datadisk 
 mount /dev/sdc1 /datadisk
 
+#install fio
 apt-get update > /dev/null 
 apt-get -y install fio > /dev/null
 
-echo [global] > t 
-echo size=$1 >> t
-echo direct=1 >> t
-echo iodepth=256 >> t 
-echo ioengine=libaio >> t 
-echo bs=8k >> t
+#create test config
+cat << EOF > t
+[global]
+size=$1
+direct=1
+iodepth=256
+ioengine=libaio
+bs=$5
+EOF
 
-for i in `seq 1 4`; do
-	echo [w$i] >> t
-	echo rw=$2 >> t
-	echo directory=/datadisk >> t
-done 
+for i in `seq 1 $4`; do
+		echo "[w$i]" >> t
+		echo rw=$mode >> t
+		echo directory=/datadisk >> t
+done
 
+#run test
 fio --runtime $3 t | grep -E 'READ:|WRITE:' | tr '\n' ';' | tr -s [:space:] | sed 's/ :/:/g' | sed 's/= /=/g'
