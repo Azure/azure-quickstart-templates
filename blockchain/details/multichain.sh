a10b5c0d5a17e170342456f6c88bfc3be3e02791
negat <negat@microsoft.com>
2016-10-04 22:52:58 +0000

    removed buttons

--- blockchain/details/multichain.sh
+++ blockchain/details/multichain.sh
@@ -2,8 +2,6 @@
 
 This Microsoft Azure template deploys MultiChain to create a new private blockchain.
 
-[![Deploy to Azure](http://azuredeploy.net/deploybutton.png)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2Fazure-quickstart-templates%2Fmaster%2Fmultichain-on-ubuntu%2Fazuredeploy.json)
-
 Once your deployment is complete, a private blockchain named ``chain1`` will be running and you can start connecting other MultiChain nodes to the blockchain to send transactions and issue assets.
 
 ## Template Parameters
