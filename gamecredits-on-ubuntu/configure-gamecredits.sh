85892522f729d3ce2bb8328da8cff007fd057ab6
gamers-coin <kosmos30@web.de>
2016-10-09 12:58:29 +0200

    Fixed Azure Scripts :+1:
    Thanks OKtoshi :+1:

--- gamecredits-on-ubuntu/configure-gamecredits.sh
+++ gamecredits-on-ubuntu/configure-gamecredits.sh
@@ -1,83 +1,74 @@
 #!/bin/bash
+set -x
 
-set -e 
+echo "Initializing GameCredits Gaming Blockchain installation"
 
 date
 ps axjf
 
+AZUREUSER=$2
+HOMEDIR="/home/$AZUREUSER"
+GAMECREDITSPATH="$HOMEDIR/.gamecredits"
+VMNAME=$(hostname)
+echo "User: $AZUREUSER"
+echo "User home dir: $HOMEDIR"
+echo "User Gamecredits path: $GAMECREDITSPATH"
+echo "vmname: $VMNAME"
 
-if [ $1 = 'From_Source' ]; then
-#################################################################
-#Update Ubuntu and install prerequisites for running Gamecredits# 
-#################################################################
+
+if [ "$1" = 'From_Source' ]; then
+## Compile from Source
 sudo apt-get update
-#################################################################
-# Build Gamecredits from source                                 #
-#################################################################
-NPROC=$(nproc)
-echo "nproc: $NPROC"
-#################################################################
-# Install all necessary packages for building Gamecredits       #
-#################################################################
-sudo apt-get -y install git unzip wget build-essential libtool autotools-dev autoconf pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev libminiupnpc-dev libzmq3-dev
+sudo apt-get -y install git build-essential libssl-dev libdb-dev libdb++-dev libboost-all-dev libqrencode-dev unzip pwgen
 sudo add-apt-repository -y ppa:bitcoin/bitcoin
 sudo apt-get update
 sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 
-cd /usr/local
-file=/usr/local/GameCredits
-if [ ! -e "$file" ]
-then
-	sudo git clone https://github.com/gamecredits-project/GameCredits.git
-fi
+cd /usr/local/src/ || exit
+sudo git clone https://github.com/gamecredits-project/GameCredits.git 
+cd GameCredits/src || exit
 
-cd /usr/local/GameCredits
-file=/usr/local/GameCredits/src/gamecreditsd
-if [ ! -e "$file" ]
-then
-	sudo ./autogen.sh
-	sudo ./configure
-	sudo make -j$NPROC
-fi
-
-sudo cp /usr/local/GameCredits/src/gamecreditsd /usr/bin/gamecreditsd
-sudo cp /usr/local/GameCredits/src/gamecredits-cli /usr/bin/gamecredits-cli
+sudo ./autogen.sh
+sudo ./configure
+sudo make -j$NPROC
+sudo strip gamecreditsd
+sudo cp gamecreditsd /usr/bin/gamecreditsd
 
 else    
-#################################################################
-# Install Gamecredits from PPA                                  #
-#################################################################
-#sudo add-apt-repository -y ppa:gamecredits/gamecredits
-#sudo apt-get update
-#sudo apt-get install -y gamecredits
-cd /usr/local/src/
+## Download Binaries
+sudo apt-get update
+sudo apt-get -y install git wget build-essential libtool autotools-dev autoconf pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev libminiupnpc-dev libzmq3-dev unzip pwgen
+cd /usr/local/src/ || exit
 DOWNLOADFILE=$(curl -s https://api.github.com/repos/gamecredits-project/GameCredits/releases | grep browser_download_url | grep linux64 | head -n 1 | cut -d '"' -f 4)
 DOWNLOADNAME=$(curl -s https://api.github.com/repos/gamecredits-project/GameCredits/releases | grep name | grep linux64 | head -n 1 | cut -d '"' -f 4)
-sudo wget $DOWNLOADFILE
-sudo unzip $DOWNLOADNAME
+sudo wget "$DOWNLOADFILE"
+sudo unzip "$DOWNLOADNAME"
+sudo chmod 755 gamecreditsd
+sudo chmod 755 gamecredits-cli
 sudo cp gamecreditsd /usr/bin/gamecreditsd
 sudo cp gamecredits-cli /usr/bin/gamecredits-cli
-
 fi
 
-################################################################
-# Configure to auto start at boot					           #
-################################################################
-file=$HOME/.gamecredits 
-if [ ! -e "$file" ]
+# Create Client Directory
+if [ ! -e "$GAMECREDITSPATH" ]
 then
-	sudo mkdir $HOME/.gamecredits
-fi
-printf '%s\n%s\n%s\n%s\n' 'daemon=1' 'server=1' 'rpcuser=u' 'rpcpassword=p' | sudo tee $HOME/.gamecredits/gamecredits.conf
-file=/etc/init.d/gamecredits
-if [ ! -e "$file" ]
-then
-	printf '%s\n%s\n' '#!/bin/sh' 'sudo gamecreditsd' | sudo tee /etc/init.d/gamecredits
-	sudo chmod +x /etc/init.d/gamecredits
-	sudo update-rc.d gamecredits defaults	
+        su - "$AZUREUSER" -c "mkdir $GAMECREDITSPATH"
 fi
 
-/usr/bin/gamecreditsd & exit 0
+# Download GameCredits Blockchain
+su - "$AZUREUSER" -c "cd $GAMECREDITSPATH; wget https://github.com/gamers-coin/gamecredits-blockchain/releases/download/v1.0/blockchain.zip"
+su - "$AZUREUSER" -c "cd $GAMECREDITSPATH; unzip blockchain.zip"
 
-echo "Gamecredits Blockchain has been setup successfully and is running... Have Fun ..."
+# Create configuration File
+su - "$AZUREUSER" -c "touch $GAMECREDITSPATH/gamecredits.conf"
+rpcu=$(pwgen -ncsB 35 1)
+rpcp=$(pwgen -ncsB 75 1)
+echo "rpcuser=$rpcu
+rpcpassword=$rpcp
+daemon=1" > "$GAMECREDITSPATH"/gamecredits.conf
+
+# Start GameCredits Client
+su - "$AZUREUSER" -c "gamecreditsd"
+
+echo "Gamecredits Blockchain has been setup successfully and is running... $$"
 exit 0
