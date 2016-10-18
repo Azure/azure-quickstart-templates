9e172873cde743c82057a6d7ad7eb7e76bee92fd
sidkri <sid.krishna@microsoft.com>
2016-10-17 20:53:48 -0700

    Final release candidate version check-in (snapshot of ethereum-consortium-10-03-RC branch)

--- ethereum-consortium-blockchain-network/scripts/configure-geth.sh
+++ ethereum-consortium-blockchain-network/scripts/configure-geth.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-# print commands and arguments as they are executed
-set -x;
-
 #############
 # Parameters
 #############
 # Validate that all arguments are supplied
-if [ $# -lt 7 ]; then echo "Incomplete parameters supplied. Exiting"; exit 1; fi
+if [ $# -lt 4 ]; then echo "Insufficient parameters supplied. Exiting"; exit 1; fi
 
 AZUREUSER=$1;
 ARTIFACTS_URL_PREFIX=$4
+
+###########
+# Constants
+###########
+HOMEDIR="/home/$AZUREUSER";
+CONFIG_LOG_FILE_PATH="$HOMEDIR/config.log";
 
 #############
 # Get the script for running as Azure user
 #############
 cd "/home/$AZUREUSER";
-sudo -u $AZUREUSER sh -c "wget -N ${ARTIFACTS_URL_PREFIX}/scripts/configure-geth-azureuser.sh";
 
-sudo -u $AZUREUSER sh /home/$AZUREUSER/configure-geth-azureuser.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12;
+sudo -u $AZUREUSER /bin/bash -c "wget -N ${ARTIFACTS_URL_PREFIX}/scripts/configure-geth-azureuser.sh";
+sudo -u $AZUREUSER /bin/bash /home/$AZUREUSER/configure-geth-azureuser.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} >> $CONFIG_LOG_FILE_PATH 2>&1;
+echo "===== Completed $0 =====";
+exit $?;
