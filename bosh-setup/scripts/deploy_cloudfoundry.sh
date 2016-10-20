c4963f3c152d305a9a892dee819f518f17c537cc
Bin Xia <binxi@microsoft.com>
2016-10-20 02:25:37 +0000

    Move the contents of init.sh into setup_env; Use utils.sh

--- bosh-setup/scripts/deploy_cloudfoundry.sh
+++ bosh-setup/scripts/deploy_cloudfoundry.sh
@@ -1,26 +1,6 @@
 #!/usr/bin/env bash
 
-function retryop()
-{
-  retry=0
-  max_retries=10
-  interval=30
-  while [ ${retry} -lt ${max_retries} ]; do
-    echo "Operation: $1, Retry #${retry}"
-    eval $1
-    if [ $? -eq 0 ]; then
-      echo "Successfully."
-      break
-    else
-      let retry=retry+1
-      sleep $interval
-    fi
-  done
-  if [ ${retry} -eq ${max_retries} ]; then
-    echo "Operation failed: $1"
-    exit 1
-  fi
-}
+source utils.sh
 
 if [[ $# -ne 1 || -z "$1" ]]; then
     echo "Usage: ./deploy_cloudfoundry.sh <path-to-your-manifest>"
