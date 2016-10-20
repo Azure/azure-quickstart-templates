ef2a0e418d5de209e86a12fa84a0ee88938852b8
Dan Schulte <daschult@microsoft.com>
2016-10-20 11:31:16 -0700

    Add script to set environment variables and run tests locally

---
+++ localTest.cmd
@@ -0,0 +1,10 @@
+setlocal
+REM set VALIDATE_MODIFIED_ONLY=false
+set PARALLEL_DEPLOYMENT_NUMBER=6
+set VALIDATION_HOST=http://qst9494f4f5c44dd94d84049.westus.cloudapp.azure.com
+
+set VALIDATION_SKIP_VALIDATE=true
+set VALIDATION_SKIP_DEPLOY=true
+
+call mocha
+endlocal
