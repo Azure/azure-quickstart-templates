ebdf77b44c08f2e06ba1706907af5f809d308585
Dan Schulte <daschult@microsoft.com>
2016-10-24 10:26:11 -0700

    Adding more comments.

--- test/tests.js
+++ test/tests.js
@@ -189,7 +189,7 @@ function deployTemplate(requestBody) {
     deployPromise = new RSVP.Promise(function (resolve, reject) {
       return unirest.post(process.env.VALIDATION_HOST + '/deploy')
         .type('json')
-        .timeout(3600 * 1000) // template deploy can take some time
+        .timeout(3600 * 1000) // Templates can take a long time to deploy, so set the timeout to 1 hour
         .send(JSON.stringify(requestBody))
         .end(function (response) {
           timedOutput(false, intervalObj);
