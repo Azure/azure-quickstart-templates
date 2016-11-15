d14e50ecc05d45abf696b006e70fdecdf9538daa
Callum Coffin <CalCof@users.noreply.github.com>
2016-11-15 20:35:41 +0000

    Update deployGlassFish.sh

--- glassfish-on-suse/scripts/deployGlassFish.sh
+++ glassfish-on-suse/scripts/deployGlassFish.sh
@@ -8,14 +8,14 @@ PASSWORD=$3
 echo "Installing Java JDK"
 
 #zypper search java | grep openjdk
-sudo zypper --non-interactive install java-1_7_0-openjdk
+zypper --non-interactive install java-1_7_0-openjdk
 
 # Retrieve Glassfish source files 
 echo "Retrieving Glassfish source files"
 
-sudo wget http://download.java.net/glassfish/3.1.2.2/release/glassfish-$RELEASE.zip -P /tmp/
-sudo unzip /tmp/glassfish-$RELEASE.zip -d /opt
-sudo rm /tmp/glassfish-$RELEASE.zip
+wget http://download.java.net/glassfish/3.1.2.2/release/glassfish-$RELEASE.zip -P /tmp/
+unzip /tmp/glassfish-$RELEASE.zip -d /opt
+rm /tmp/glassfish-$RELEASE.zip
 
 # Start GlassFish Server 
 echo "Starting GlassFish server"
@@ -25,21 +25,21 @@ sudo /opt/glassfish$VERSION/bin/asadmin start-domain
 # Enable admin access 
 echo "Enabling admin Access"
 
-sudo touch /tmp/password.txt
-sudo chmod 600 /tmp/password.txt
-sudo echo "AS_ADMIN_PASSWORD=" > /tmp/password.txt
-sudo echo "AS_ADMIN_NEWPASSWORD=$PASSWORD" >> /tmp/password.txt
-sudo /opt/glassfish$VERSION/bin/asadmin --user admin --passwordfile /tmp/password.txt change-admin-password \
+touch /tmp/password.txt
+chmod 600 /tmp/password.txt
+echo "AS_ADMIN_PASSWORD=" > /tmp/password.txt
+echo "AS_ADMIN_NEWPASSWORD=$PASSWORD" >> /tmp/password.txt
+/opt/glassfish$VERSION/bin/asadmin --user admin --passwordfile /tmp/password.txt change-admin-password \
     --domain_name domain1
-sudo sed -i '/^AS_ADMIN_PASSWORD=/d' /tmp/password.txt
-sudo sed -i '/^AS_ADMIN_NEW_PASSWORD=/d' /tmp/password.txt
-sudo echo "AS_ADMIN_PASSWORD=$PASSWORD" > /tmp/password.txt
-sudo /opt/glassfish$VERSION/bin/asadmin --user admin --passwordfile /tmp/password.txt enable-secure-admin
-sudo rm /tmp/password.txt
+sed -i '/^AS_ADMIN_PASSWORD=/d' /tmp/password.txt
+sed -i '/^AS_ADMIN_NEW_PASSWORD=/d' /tmp/password.txt
+echo "AS_ADMIN_PASSWORD=$PASSWORD" > /tmp/password.txt
+/opt/glassfish$VERSION/bin/asadmin --user admin --passwordfile /tmp/password.txt enable-secure-admin
+rm /tmp/password.txt
 
 # Enable GlassFish as a service and quick initialization 
 echo "Enabling GlassFish as a service"
-sudo tee /etc/init.d/glassfish > /dev/null <<'EOF'
+tee /etc/init.d/glassfish > /dev/null <<'EOF'
 
 # Set path variable
 GLASSFISH_HOME=/opt/glassfish$VERSION
@@ -64,6 +64,6 @@ exit 0
 
 EOF
 
-sudo chmod 755 /etc/init.d/glassfish
+chmod 755 /etc/init.d/glassfish
 
 echo "Script complete"
