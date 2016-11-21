25bc3ee7bda1e9ea2cde6b43c4b401ac886f0fa5
troymc <troy@ascribe.io>
2016-11-21 16:51:51 +0100

    Updated command to prep for RethinkDB installation

--- bigchaindb-on-ubuntu/scripts/init.sh
+++ bigchaindb-on-ubuntu/scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-echo "deb http://download.rethinkdb.com/apt trusty main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
 wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
 sudo apt-get -y update
 sudo apt-get -y install rethinkdb
