56a15c95510fc8e7b67a049dd2e6304b733bb463
Julius davies <julius@hotmail.co.uk>
2016-11-21 12:38:30 +0000

    changes
    deletes

--- 100-blank-template/README.md
+++
@@ -1,3 +0,0 @@
-# Blank Template
-
-This is an empty template, with the schema reference and top-level properties defined.
--- netki-wns-api-server-on-ubuntu/get-build-run-dockerfile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl https://raw.githubusercontent.com/netkicorp/wns-api-server/master/DockerfileWithoutNamecoin -o "Dockerfile"
-
-docker build -t netki-wns-api-server-no-nc .
-
-docker run --name=netki-wns-api-server -i -p 80:5000 netki-wns-api-server-no-nc &
