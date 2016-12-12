af0e40268eb5e670639a1215f05d7105ce84e00a
troymc <troy@ascribe.io>
2016-12-12 15:53:34 +0100

    Removed trailing spaces after #!/bin/sh

--- bigchaindb-on-ubuntu/scripts/init.sh
+++ bigchaindb-on-ubuntu/scripts/init.sh
@@ -1,4 +1,4 @@
-#!/bin/sh 
+#!/bin/sh
 
 . /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
 wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
