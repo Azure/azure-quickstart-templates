f3707e7d5614ba5ed61ae3b3a0fda9339405c67f
troymc <troy@ascribe.io>
2016-12-12 15:23:21 +0100

    Change 'source' to '.' to work with sh

--- bigchaindb-on-ubuntu/scripts/init.sh
+++ bigchaindb-on-ubuntu/scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 
-source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+. /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
 wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
 sudo apt-get -y update
 sudo apt-get -y install rethinkdb
