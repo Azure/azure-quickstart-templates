55f53d9fe5c859d8304f55f4deef8d7333a2ec36
Hosam Kamel <hkamel@microsoft.com>
2016-12-19 15:28:47 +0400

    update

--- sonarqube-azuresql/scripts/secureSonarQube.ps1
+++ sonarqube-azuresql/scripts/secureSonarQube.ps1
@@ -1,5 +1,13 @@
+<#
+    .SYNOPSIS
+        Secure SonarQube installation
+#>
+
+#Generate certificate
 $certificate = New-SelfSignedCertificate -DnsName $nodeName -CertStoreLocation "cert:\LocalMachine\My"	
 #Install ARR
 Invoke-Expression ((new-object net.webclient).DownloadString("https://chocolatey.org/install.ps1"))
 cinst urlrewrite -y --force
 cinst iis-arr -y --force
+
+
