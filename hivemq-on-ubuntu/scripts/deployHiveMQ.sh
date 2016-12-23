a9b9009f52dbe5f48c58b4771b2ff5d9f865f713
Callum Coffin <CalCof@users.noreply.github.com>
2016-12-23 22:27:14 +0000

    Update deployHiveMQ.sh

--- hivemq-on-ubuntu/scripts/deployHiveMQ.sh
+++ hivemq-on-ubuntu/scripts/deployHiveMQ.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+DOWNLOADURL=$1
+
+# Install Java JDK
+echo "Installing Java JDK"
+
+apt-get install unzip --assume-yes
+apt-get install openjdk-7-jdk --assume-yes
+
+wget --content-disposition $DOWNLOADURL -P /tmp/
+unzip /tmp/hivemq-*.zip -d /opt
+rm /tmp/hivemq-*.zip.zip
+
+mv /opt/hivemq-* /opt/hivemq
