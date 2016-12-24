0f2c81b96ba8efef08df762b2f0f03d7c3e94e69
Callum Coffin <CalCof@users.noreply.github.com>
2016-12-24 11:45:03 +0000

    Update deployHiveMQ.sh

--- hivemq-on-ubuntu/scripts/deployHiveMQ.sh
+++ hivemq-on-ubuntu/scripts/deployHiveMQ.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 DOWNLOAD_URL=$1
+TOTAL_INSTACES=$2
+INSTACE_NUMBER=$3
+
 
 # Install Java JDK
 echo "Installing Java JDK"
@@ -31,8 +34,60 @@ chmod +x /etc/init.d/hivemq
 
 # Edit the HiveMQ configuration
 
-cd /opt/hivemq/conf
+NODE_IP=4+$INSTACE_NUMBER
 
+cat > /opt/hivemq/conf/config.xml << EOF
+
+<?xml version="1.0"?>
+<hivemq xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:noNamespaceSchemaLocation="hivemq-config.xsd">
+    <listeners>
+        <tcp-listener>
+            <port>1883</port>
+            <bind-address>10.0.0.$NODE_IP</bind-address>
+        </tcp-listener>
+    </listeners>
+	  <mqtt>
+        <max-client-id-length>65535</max-client-id-length>
+        <retry-interval>10</retry-interval>
+        <max-queued-messages>1000</max-queued-messages>
+    </mqtt>
+    <cluster>
+        <enabled>true</enabled>
+        <transport>
+            <tcp>
+                <bind-address>10.0.0.$NODE_IP</bind-address>
+                <bind-port>7800</bind-port>
+            </tcp>
+        </transport>
+        <discovery>
+            <static>
+                <node>
+                    <host>10.0.0.5</host>
+                    <port>7800</port>
+                </node>
+            </static>
+        </discovery>
+		    <failure-detection>
+            <heartbeat>
+                <enabled>true</enabled>
+                <interval>5000</interval>
+                <timeout>15000</timeout>
+            </heartbeat>
+        </failure-detection>
+    </cluster>
+	  <throttling>
+        <max-connections>-1</max-connections>
+        <max-message-size>268435456</max-message-size>
+        <outgoing-limit>0</outgoing-limit>
+        <incoming-limit>0</incoming-limit>
+    </throttling>
+    <general>
+        <update-check-enabled>true</update-check-enabled>
+    </general>
+</hivemq>
+
+EOF
 
 # Start HiveMQ on boot
 
