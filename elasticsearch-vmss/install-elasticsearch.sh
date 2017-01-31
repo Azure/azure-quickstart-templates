444263b56ee6169a21ced478994b1f719c88bc69
Hans Krijger <hkrijger@microsoft.com>
2017-01-30 17:41:20 -0800

    install x-pack; switch lb port to 5601

--- elasticsearch-vmss/install-elasticsearch.sh
+++ elasticsearch-vmss/install-elasticsearch.sh
@@ -133,6 +133,11 @@ install_es()
     echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-5.x.list    
     apt-get update -y 
     apt-get install -y elasticsearch
+    apt-get install -y kibana
+    cd /usr/share/elasticsearch/
+    bin/elasticsearch-plugin install x-pack --batch
+    cd /usr/share/kibana/
+    bin/kibana-plugin install x-pack
 }
 
 configure_es()
@@ -172,7 +177,9 @@ start_service()
 	log "Starting Elasticsearch on ${HOSTNAME}"
     systemctl daemon-reload
     systemctl enable elasticsearch.service
+    systemctl enable kibana.service
     systemctl start elasticsearch.service
+    systemctl start kibana.service
     sleep 60
     
     if [ `systemctl is-failed elasticsearch.service` == 'failed' ];
