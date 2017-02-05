95b675217a2be6a8844e2437c9466ee6ebb5295a
Cody Burns <cody.w.burns@gmail.com>
2017-02-05 09:46:07 -0600

    correct git push

--- ethereumClassic-go-ubuntu/configure-etc-geth.sh
+++ ethereumClassic-go-ubuntu/configure-etc-geth.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 
 # print commands and arguments as they are executed
@@ -34,40 +33,3 @@ time apt-get install -y ethereum-classic-unstable
 
 date
 echo "completed geth Classic Chain install $$"
-=======
-#!/bin/bash
-
-# print commands and arguments as they are executed
-set -x
-
-echo "initializing geth Classic Chain installation"
-date
-ps axjf
-
-#############
-# Parameters
-#############
-
-AZUREUSER=$1
-HOMEDIR="/home/$AZUREUSER"
-VMNAME=`hostname`
-echo "User: $AZUREUSER"
-echo "User home dir: $HOMEDIR"
-echo "vmname: $VMNAME"
-
-#####################
-# setup the Azure CLI
-#####################
-time sudo npm install azure-cli -g
-time sudo update-alternatives --install /usr/bin/node nodejs /usr/bin/nodejs 100
-
-####################
-# Setup Geth classic - using launchpad ppa
-####################
-time sudo add-apt-repository ppa:ethereum-classic/etc-geth
-time sudo apt-get update
-time apt-get install -y ethereum-classic-unstable
-
-date
-echo "completed geth Classic Chain install $$"
->>>>>>> origin/master
