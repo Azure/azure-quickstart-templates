54312a345295fb427dab7380d459f43b9a631376
Damien Caro <dcaro@microsoft.com>
2017-02-23 23:16:53 -0800

    Implementing requested changes  and moving to one template.

--- spinnaker-jenkins-to-vmss/scripts/set_azure_spinnaker.sh
+++ spinnaker-jenkins-to-vmss/scripts/set_azure_spinnaker.sh
@@ -42,48 +42,33 @@ WORKDIR=$(pwd)
 # Usually the workdir is /var/lib/waagent/custom-script/download/0
 JENKINS_URL='http:\/\/'$JENKINS_FQDN
 DEBIAN_REPO='http:\/\/ppa.launchpad.net\/openjdk-r\/ppa\/ubuntu trusty main;'$JENKINS_URL
-DEBUG_FILE=$WORKDIR"/debugfile"
 SED_FILE=$WORKDIR"/sedCommand.sed"
 
-# Record the Variables in text file for debugging purposes  
-sudo printf "TENANTID=%s\n" $TENANTID > $DEBUG_FILE
-sudo printf "SPNCLIENTSECRET=%s\n" $PASSWORD >> $DEBUG_FILE
-sudo printf "SPNCLIENTID=%s\n" $CLIENTID >> $DEBUG_FILE
-sudo printf "SUBSCRIPTIONID=%s\n" $SUBSCRIPTIONID >> $DEBUG_FILE
-sudo printf "PACKERSTORAGEACCOUNT=%s\n" $PACKERSTORAGEACCOUNT >> $DEBUG_FILE
-sudo printf "RESOURCEGROUP=%s\n" $RESOURCEGROUP >> $DEBUG_FILE
-sudo printf "RESOURCEGROUPLOCATION=%s\n" $RESOURCEGROUPLOCATION >> $DEBUG_FILE
-sudo printf "KEYVAULT=%s\n" $KEYVAULT >> $DEBUG_FILE
-sudo printf "JENKINS_URL=%s\n" $JENKINS_URL >> $DEBUG_FILE
-sudo printf "FRONT50_KEY=%s\n" $FRONT50_KEY >> $DEBUG_FILE
-
-sudo printf "working directory is %s\n" $WORKDIR >> $DEBUG_FILE
-
-sudo printf "Upgrading the environment\n" >> $DEBUG_FILE
+sudo printf "Upgrading the environment\n"
 # Update and upgrade packages
 sudo apt-mark hold walinuxagent grub-legacy-ec2
-sudo printf "Holding walinuxagent\n" >> $DEBUG_FILE
+sudo printf "Holding walinuxagent\n" 
 sudo apt-get update -y
-sudo printf "apt-get update completed\n" >> $DEBUG_FILE
+sudo printf "apt-get update completed\n" 
 sudo rm /var/lib/dpkg/updates/*
-sudo printf "directory /var/lib/dpkg/updates removed\n" >> $DEBUG_FILE
+sudo printf "directory /var/lib/dpkg/updates removed\n"
 sudo apt-get upgrade -y
-sudo printf "apt-get upgrade completed\n" >> $DEBUG_FILE
+sudo printf "apt-get upgrade completed\n" 
 
 # Install Spinnaker on the VM with no cassandra
-sudo printf "Starting to install Spinnaker\n" >> $DEBUG_FILE
+sudo printf "Starting to install Spinnaker\n" 
 curl --silent https://raw.githubusercontent.com/spinnaker/spinnaker/master/InstallSpinnaker.sh | sudo bash -s -- --cloud_provider azure --azure_region $RESOURCEGROUPLOCATION --noinstall_cassandra
 
-sudo printf "Spinnaker has been installed\n" >> $DEBUG_FILE
+sudo printf "Spinnaker has been installed\n" 
 
 # configure to not use cassandra
 sudo /opt/spinnaker/install/change_cassandra.sh --echo=inMemory --front50=azs
-sudo printf "Configured to not use cassandra" >> $DEBUG_FILE
+sudo printf "Configured to not use cassandra" 
 
 # Configuring the /opt/spinnaker/config/default-spinnaker-local.yml
 # Let's create the sed command file and run the sed command
 
-sudo printf "Setting up sedCommand \n" >> $DEBUG_FILE
+sudo printf "Setting up sedCommand \n" 
 
 sudo printf "s/enabled: \${SPINNAKER_AZURE_ENABLED:false}/enabled: \${SPINNAKER_AZURE_ENABLED:true}/g\n" > $SED_FILE
 sudo printf "s/defaultRegion: \${SPINNAKER_AZURE_DEFAULT_REGION:westus}/defaultRegion: \${SPINNAKER_AZURE_DEFAULT_REGION:$RESOURCEGROUPLOCATION}/g\n" >> $SED_FILE
@@ -112,25 +97,24 @@ sudo printf "/front50:/ {\n    N\n     /cassandra:/ {\n         N\n         s/en
 # Configure Azure storage
 sudo printf "/azs:/ {\n   N\n   s/enabled: false/enabled: true/\n   N\n   s/storageAccountName:/storageAccountName: $FRONT50_STORAGE/\n   N\n   s|storageAccountKey:|storageAccountKey: $FRONT50_KEY|\n   }\n" >> $SED_FILE
 
-sudo printf "sedCommand.sed file created\n" >> $DEBUG_FILE
+sudo printf "sedCommand.sed file created\n" 
 
 # Set the variables in the spinnaker-local.yml file
 sudo sed -i -f $SED_FILE /opt/spinnaker/config/spinnaker-local.yml 
-sudo printf "spinnaker-local.yml file has been updated\n" >> $DEBUG_FILE
+sudo printf "spinnaker-local.yml file has been updated\n" 
 
 # Configure rosco.yml file  
 sudo sed -i "/# debianRepository:/s/.*/debianRepository: $DEBIAN_REPO:9999 trusty main/" /opt/rosco/config/rosco.yml
 sudo sed -i '/defaultCloudProviderType/s/.*/defaultCloudProviderType: azure/' /opt/rosco/config/rosco.yml
-sudo printf "rosco.yml file has been updated\n" >> $DEBUG_FILE
+sudo printf "rosco.yml file has been updated\n" 
 
 # Adding apt-key key
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB9B1D8886F44E2A
-sudo printf "apt-key done\n" >> $DEBUG_FILE
+sudo printf "apt-key done\n" 
 
 # Removing debug file
-# sudo rm -f $DEBUG_FILE
-# sudo rm -f $SED_FILE
+sudo rm -f $SED_FILE
 
 # rebooting the VM to avoid issues with front50
-sudo shutdown -r now "Rebooting the system after Spinnaker installation" 
+sudo restart spinnaker 
 
