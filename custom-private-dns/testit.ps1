d7709dc4d2afea152bd01e91f7d2444ec93c3828
GarethBradshawMSFT <garbrad@microsoft.com>
2017-03-14 17:27:00 +0000

    fixing path issues with Windows vmextension

--- custom-private-dns/testit.ps1
+++ custom-private-dns/testit.ps1
@@ -2,7 +2,7 @@
 
 "Script Root Is $PSScriptRoot"
 
-#New-AzureRmResourceGroup -Name "quicktest" -Location "West Europe"
+New-AzureRmResourceGroup -Name "twoparttest" -Location "West Europe" -Force
 
-#New-AzureRmResourceGroupDeployment -Name "SetupDnsClient" -ResourceGroupName "clienttest" -TemplateFile "$PSScriptRoot/../azuredeploy-server.json" -newStorageAccountName "grbtemplatetest" -serverPublicDnsName "grbtemplatetest" -adminUsername "garbrad" -adminPassword (ConvertTo-SecureString "Gareth123123!" -AsPlainText -Force) -dnsZoneName "gareth.local" -assetLocation "https://dnsfwshare.blob.core.windows.net:443/share/custom-private-dns/"
-New-AzureRmResourceGroupDeployment -Name "SetupDnsClient" -ResourceGroupName "clienttest" -TemplateFile "$PSScriptRoot/../azuredeploy-clients.json" -newStorageAccountName "grbtemplatetest" -serverPublicDnsName "grbtemplatetest" -adminUsername "garbrad" -adminPassword (ConvertTo-SecureString "Gareth123123!" -AsPlainText -Force) -dnsZoneName "gareth.local" -assetLocation "https://dnsfwshare.blob.core.windows.net:443/share/custom-private-dns/"
+New-AzureRmResourceGroupDeployment -Name "servertest" -ResourceGroupName "twoparttest" -TemplateFile "$PSScriptRoot/azuredeploy-server.json" -newStorageAccountName "grbtemplatetest2" -serverPublicDnsName "grbtemplatetest" -adminUsername "garbrad" -adminPassword (ConvertTo-SecureString "Gareth123123!" -AsPlainText -Force) -dnsZoneName "gareth.local" -assetLocation "https://dnsfwshare.blob.core.windows.net:443/share"
+#New-AzureRmResourceGroupDeployment -Name "clienttest" -ResourceGroupName "twoparttest" -TemplateFile "$PSScriptRoot/azuredeploy-clients.json" -newStorageAccountName "grbtemplatetest2" -serverPublicDnsName "grbtemplatetest" -adminUsername "garbrad" -adminPassword (ConvertTo-SecureString "Gareth123123!" -AsPlainText -Force) -dnsZoneName "gareth.local" -assetLocation "https://dnsfwshare.blob.core.windows.net:443/share"
