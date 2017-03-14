21a06dade88996bf14d935111646897fa4a1961f
GarethBradshawMSFT <garbrad@microsoft.com>
2017-03-14 12:13:09 +0000

    multiple updates

---
+++ custom-private-dns/tests/clienttest.ps1
@@ -0,0 +1,5 @@
+﻿#Login-AzureRmAccount 
+
+"Script Root Is $PSScriptRoot"
+New-AzureRmResourceGroupDeployment -Name "SetupDnsClient" -ResourceGroupName "quicktest" -TemplateFile "$PSScriptRoot/../linux-client/setuplinuxclient.json" -dnsZone "gareth.local" -vmList "linclient"
+New-AzureRmResourceGroupDeployment -Name "SetupDnsClient" -ResourceGroupName "quicktest" -TemplateFile "$PSScriptRoot/../windows-client/setupwinclient.json" -dnsZone "gareth.local" -vmList "winclient"
