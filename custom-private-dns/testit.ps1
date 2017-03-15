87eb4d48466d25baa545c3d530f11c85d4ec3f88
GarethBradshawMSFT <garbrad@microsoft.com>
2017-03-15 12:31:43 +0000

    merging client and server scripts

--- custom-private-dns/testit.ps1
+++ custom-private-dns/testit.ps1
@@ -2,7 +2,7 @@
 
 "Script Root Is $PSScriptRoot"
 
-New-AzureRmResourceGroup -Name "twoparttest3" -Location "West Europe" -Force
+$rgname = "customdnstest1"
+New-AzureRmResourceGroup -Name $rgname -Location "West Europe" -Force
+New-AzureRmResourceGroupDeployment -Name $rgname -ResourceGroupName $rgname -TemplateFile "$PSScriptRoot/azuredeploy.json"  -newStorageAccountName "grb$rgname" -serverPublicDnsName "grb$rgname" -adminUsername "garbrad" -adminPassword (ConvertTo-SecureString "Gareth123123!" -AsPlainText -Force) -dnsZoneName "default.local" -assetLocation "https://dnsfwshare.blob.core.windows.net:443/share"
 
-New-AzureRmResourceGroupDeployment -Name "servertest" -ResourceGroupName "twoparttest3" -TemplateFile "$PSScriptRoot/azuredeploy-server.json" -newStorageAccountName "grbtemplatetest3" -serverPublicDnsName "grbtemplatetest3" -adminUsername "garbrad" -adminPassword (ConvertTo-SecureString "Gareth123123!" -AsPlainText -Force) -dnsZoneName "gareth.local" -assetLocation "https://dnsfwshare.blob.core.windows.net:443/share"
-#New-AzureRmResourceGroupDeployment -Name "clienttest" -ResourceGroupName "twoparttest3" -TemplateFile "$PSScriptRoot/azuredeploy-clients.json" -newStorageAccountName "grbtemplatetest3" -serverPublicDnsName "grbtemplatetest3" -adminUsername "garbrad" -adminPassword (ConvertTo-SecureString "Gareth123123!" -AsPlainText -Force) -dnsZoneName "gareth.local" -assetLocation "https://dnsfwshare.blob.core.windows.net:443/share"
