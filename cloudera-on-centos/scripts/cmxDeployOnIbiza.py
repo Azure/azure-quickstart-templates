611f91c20824ef2d5e159792f868ad0b32228442
Jason Wang <jasonw@cloudera.com>
2017-03-23 16:29:54 -0700

    update the cloudera-on-centos offering from 1.2.3 to 1.3.0

--- cloudera-on-centos/scripts/cmxDeployOnIbiza.py
+++ cloudera-on-centos/scripts/cmxDeployOnIbiza.py
@@ -33,7 +33,8 @@ def getParameterValue(vmsize, parameter):
         "Standard_DS13:impalad_memory_limit": "21500000000"
 
     }
-    return switcher.get(vmsize+":"+parameter, "0")
+    # vmsize[:13] is used to truncate the VM size since Standard_DS?? and Standard_DS??_v2 need same values.
+    return switcher.get(vmsize[:13]+":"+parameter, "0")
 
 def getDataDiskCount():
     bashCommand="lsblk | grep /data | grep -v /data/ | wc -l"
