d071be5001f6e5da488eec9b1453893253e6d60c
Ben Hummerstone <benhu@microsoft.com>
2017-06-29 21:55:49 +0100

    Added install script

---
+++ centos-singularity/install-singularity.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+VERSION=2.3.1
+yum -y groupinstall "Development Tools"
+wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
+tar xvf singularity-$VERSION.tar.gz
+cd singularity-$VERSION
+./configure --prefix=/usr/local
+make
+make install
