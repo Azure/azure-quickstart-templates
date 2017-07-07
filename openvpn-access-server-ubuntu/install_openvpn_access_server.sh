4acc66ba640325742dbbfe6d17cfee3c8148cc47
Felipe Pessoto <felipepessoto@hotmail.com>
2017-07-07 10:17:45 -0300

    Network config

--- openvpn-access-server-ubuntu/install_openvpn_access_server.sh
+++ openvpn-access-server-ubuntu/install_openvpn_access_server.sh
@@ -12,3 +12,11 @@ sudo dpkg -i openvpn-as-2.1.9-Ubuntu16.amd_64.deb
 
 #update the password for user openvpn
 sudo echo "openvpn:$userPassword"|sudo chpasswd
+
+#configure server network settings
+PUBLICIP=$(curl -s ifconfig.me)
+sudo apt-get install sqlite3
+sudo sqlite3 "/usr/local/openvpn_as/etc/db/config.db" "update config set value='$PUBLICIP' where name='host.name';"
+
+#restart OpenVPN AS service
+sudo systemctl restart openvpnas
