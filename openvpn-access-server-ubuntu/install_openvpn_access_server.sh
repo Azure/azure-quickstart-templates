c54122aaddd7cb06dc41c0a7d30de2f9e55098c5
Felipe Pessoto <felipepessoto@hotmail.com>
2017-07-10 22:42:00 -0300

    Remove comments

--- openvpn-access-server-ubuntu/install_openvpn_access_server.sh
+++ openvpn-access-server-ubuntu/install_openvpn_access_server.sh
@@ -4,11 +4,9 @@ userPassword=$1
 #download the packages
 cd /tmp
 wget -c http://swupdate.openvpn.org/as/openvpn-as-2.1.9-Ubuntu16.amd_64.deb
-#sudo apt-get update
 
 #install the software
 sudo dpkg -i openvpn-as-2.1.9-Ubuntu16.amd_64.deb
-#sudo apt-get -y install openvpn-as
 
 #update the password for user openvpn
 sudo echo "openvpn:$userPassword"|sudo chpasswd
