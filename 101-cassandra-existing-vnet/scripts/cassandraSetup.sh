bd9d9c8419199fa3b1b6a4957111d4dab8c3cda8
yaron2 <schneider.yaron@live.com>
2017-07-22 22:43:05 +0300

    script logs

--- 101-cassandra-existing-vnet/scripts/cassandraSetup.sh
+++ 101-cassandra-existing-vnet/scripts/cassandraSetup.sh
@@ -18,35 +18,47 @@ else
 fi
 
 if [ -n "$2" ]; then
+    echo "1"
     CASSANDRA_VERSION=$2
+    echo "2"
 fi
 
+echo "3"
+
 function install_cassandra() {
+    echo "4"
     if [ $IS_SEED_NODE = 0 ]; then
+        echo "5"
         docker run -d -e CASSANDRA_BROADCAST_ADDRESS="$LOCAL_PRIVATE_IP" -p 7000:7000 -p 9042:9042 cassandra:"$CASSANDRA_VERSION"
+        "echo 6"
     else
+        echo "7"
         docker run -d -e CASSANDRA_BROADCAST_ADDRESS="$LOCAL_PRIVATE_IP" -e CASSANDRA_SEEDS="$HOST_IP" -p 7000:7000 -p 9042:9042 cassandra:"$CASSANDRA_VERSION"
+        echo "8"
     fi
 }
 
 function install_dependencies() {
+    echo "8"
     apt-get update
-
+    "echo 9"
     apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
-    
+    echo "10"
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
+    echo "11"
     add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
-
+    echo "12"
     apt-get update
+    echo "14"
     apt-get install -y docker-ce
+    echo "15"
 }
 
 install_dependencies
