038fd8ecb8247568986f7f22fdbb187bba81035d
yaron2 <schneider.yaron@live.com>
2017-07-22 17:44:02 +0300

    updated

--- 101-cassandra-existing-vnet/scripts/cassandraSetup.sh
+++ 101-cassandra-existing-vnet/scripts/cassandraSetup.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+LOCAL_PRIVATE_IP=$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)
+IS_SEED_NODE=1
+HOST_IP=""
+CASSANDRA_VERSION="latest"
+
+if [ $1 = $LOCAL_PRIVATE_IP ]; then
+    IS_SEED_NODE=0
+    echo "host node detected"
+else
+    HOST_IP=$1
+    echo "seed node detected"
+fi
+
+if [ -n "$2" ]; then
+    CASSANDRA_VERSION=$2
+fi
+
 function install_cassandra() {
-    localPrivateIP=$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)
-    docker run -d -e CASSANDRA_BROADCAST_ADDRESS="$localPrivateIP" -p 7000:7000 -p 9042:9042 cassandra:latest
+    if [ $IS_SEED_NODE = 0 ]; then
+        docker run -d -e CASSANDRA_BROADCAST_ADDRESS="$LOCAL_PRIVATE_IP" -p 7000:7000 -p 9042:9042 cassandra:"$CASSANDRA_VERSION"
+    else
+        docker run -d -e CASSANDRA_BROADCAST_ADDRESS="$LOCAL_PRIVATE_IP" -e CASSANDRA_SEEDS="$HOST_IP" -p 7000:7000 -p 9042:9042 cassandra:"$CASSANDRA_VERSION"
+    fi
 }
 
 function install_dependencies() {
