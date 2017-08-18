ca246c9602219412d3d0cf07500f6a18c39800e2
Amit Malik <contact2amitmalik@gmail.com>
2017-08-18 11:57:51 -0700

    Update jumpvmScript.ps1

--- netapp-ontap-sql/scripts/jumpvmScript.ps1
+++ netapp-ontap-sql/scripts/jumpvmScript.ps1
@@ -1,9 +1,1 @@
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
-
-$DesktopPath = [Environment]::GetFolderPath("Desktop") 
-New-Item $DesktopPath\sql-vm.rdp  -ItemType file
-$a="full address:s:10.0.2.4:3389
-prompt for credentials:i:1
-administrative session:i:1"
-
-Add-Content $DesktopPath\sql-vm.rdp  $a
