f03b9f4be654214b44a00de81d6d0326e7a74f2e
jainish shah <jainishshah@yahoo.com>
2017-09-14 14:39:49 -0700

    added logging

--- artifactory/install_lamp.sh
+++ artifactory/install_lamp.sh
@@ -4,20 +4,19 @@ dbpass=$1
 export DEBIAN_FRONTEND=noninteractive
 
 # install the LAMP stack
-apt-get update
-apt-get -y install mysql-server wget curl
+apt-get -y install mysql-server wget curl>> /tmp/yum-install.log 2>&1
 
 # install Java 8
 add-apt-repository -y ppa:webupd8team/java
 apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
-apt-get install -y oracle-java8-installer
+apt-get install -y oracle-java8-installer>> /tmp/yum-java8.log 2>&1
 
 # install the MySQL stack
 echo "deb https://jfrog.bintray.com/artifactory-pro-debs trusty main" | tee -a /etc/apt/sources.list
 curl https://bintray.com/user/downloadSubjectPublicKey?username=jfrog | apt-key add -
 apt-get update
-apt-get -y install jfrog-artifactory-pro
+apt-get -y install jfrog-artifactory-pro>> /tmp/yum-artifactory.log 2>&1
 
 # start Artifactory
 service artifactory start
