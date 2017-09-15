1b16e7de7dc13e2e1d8a8abb23b92af92060369d
jainish shah <jainishshah@yahoo.com>
2017-09-15 12:55:34 -0700

    installing nginx

--- artifactory/install_artifactory.sh
+++ artifactory/install_artifactory.sh
@@ -4,7 +4,7 @@ dbpass=$1
 export DEBIAN_FRONTEND=noninteractive
 
 # install the LAMP stack
-apt-get -y install mysql-server wget curl>> /tmp/yum-install.log 2>&1
+apt-get -y install wget curl>> /tmp/yum-install.log 2>&1
 
 # install Java 8
 add-apt-repository -y ppa:webupd8team/java
@@ -16,7 +16,9 @@ apt-get install -y oracle-java8-installer>> /tmp/yum-java8.log 2>&1
 echo "deb https://jfrog.bintray.com/artifactory-pro-debs trusty main" | tee -a /etc/apt/sources.list
 curl https://bintray.com/user/downloadSubjectPublicKey?username=jfrog | apt-key add -
 apt-get update
+apt-get -y install nginx>> /tmp/yum-nginx.log 2>&1
 apt-get -y install jfrog-artifactory-pro>> /tmp/yum-artifactory.log 2>&1
 
 # start Artifactory
 service artifactory start
+service nginx start
