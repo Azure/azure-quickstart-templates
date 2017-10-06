bc7776e403123cd1470c5e6b9183db3799021d65
Alejandro Taboada <ataboada@gmail.com>
2017-10-06 09:03:54 -0300

    upgrade Safewalk version to 2.4.18

--- safewalk2-platform/scripts/safewalk_upgrade.sh
+++ safewalk2-platform/scripts/safewalk_upgrade.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm /root/.ssh/known_hosts 
 
-SWP_NAME=safewalk-accumulated-patch-2.4.17
-SWP_DOWNLOAD_URL=https://download.altipeaksecurity.com/index.php/s/jMEdod0E2l1i2os/download
+SWP_NAME=safewalk-accumulated-patch-2.4.18
+SWP_DOWNLOAD_URL=https://download.altipeaksecurity.com/index.php/s/7qedA0vkkqFzVSe/download
 
 pushd /home/safewalk/safewalk_server/sources
 bin/safewalk_upgrade.sh $SWP_DOWNLOAD_URL $SWP_NAME
