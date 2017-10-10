ffaeb7b84fcb8fec4a4e880f1d7dacd2e6bc803a
Alejandro Taboada <ataboada@gmail.com>
2017-10-10 10:52:16 -0300

    add security updates for gateway

--- safewalk2-platform/scripts/configure_gateway_custom_script.sh
+++ safewalk2-platform/scripts/configure_gateway_custom_script.sh
@@ -7,9 +7,15 @@ CLUSTER_ENABLED=$4
 
 
 my_dir=`dirname $0`
+
 sh $my_dir/set_root_password.sh $ROOT_PASSWORD
+
 bash $my_dir/setup_timezone.sh
+
+turnkey-install-security-updates
+
 bash $my_dir/gateway_upgrade.sh
+
 sh $my_dir/gateway_whitelist_safewalk_server.sh $SAFEWALK_IP_1
 if [ "$CLUSTER_ENABLED" = "True" ]; then
     sh $my_dir/gateway_whitelist_safewalk_server.sh $SAFEWALK_IP_2
