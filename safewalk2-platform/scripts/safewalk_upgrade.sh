ee17e94dd41a0f3faa5322c1e6cd8de82c4a49c6
Alejandro Taboada <ataboada@gmail.com>
2017-10-17 17:06:02 -0300

    fix update url

--- safewalk2-platform/scripts/safewalk_upgrade.sh
+++ safewalk2-platform/scripts/safewalk_upgrade.sh
@@ -2,7 +2,7 @@
 rm /root/.ssh/known_hosts 
 
 SWP_NAME=safewalk-accumulated-patch-2.4.18
-SWP_DOWNLOAD_URL=https://download.altipeaksecurity.com/index.php/s/7qedA0vkkqFzVSe/download
+SWP_DOWNLOAD_URL=https://download.altipeaksecurity.com/index.php/s/2GGKBMbv0RnUyCH/download
 
 pushd /home/safewalk/safewalk_server/sources
 bin/safewalk_upgrade.sh $SWP_DOWNLOAD_URL $SWP_NAME
