2db1516821452a46a4f846c70b4b4eba4d35ff72
Alejandro Taboada <ataboada@gmail.com>
2017-10-18 13:22:38 -0300

    fix safewalk gateway integration

--- safewalk2-platform/scripts/configure_safewalk_custom_script_master.sh
+++ safewalk2-platform/scripts/configure_safewalk_custom_script_master.sh
@@ -4,10 +4,10 @@ ROOT_PASSWORD=$1
 GATEWAY_IP=$2
 GATEWAY_PUBLIC_IP=$3
 GATEWAY_ROOT_PASSWORD=$4
-SAFEWALK_IP_1=$5
-SAFEWALK_SUBNET_IP=$6
-CLUSTER_ENABLED=$7
-ADMIN_PASSWORD=$8
+FIRST_IP=$5
+COUNT_IP=$6
+SAFEWALK_SUBNET_IP=$7
+ADMIN_PASSWORD=$9
 
 
 #sh configure_safewalk_custom_script.sh root 192.168.10.244 192.168.10.244 Safewalk1 192.168.10.201
@@ -35,15 +35,26 @@ bash $safewalk_dir/bin/safewalk_set_admin_password.sh $ADMIN_PASSWORD
 
 
 
-if [ "$CLUSTER_ENABLED" = "True" ]; then
-    SAFEWALK_HOSTS=$SAFEWALK_IP_1,$SAFEWALK_IP_2
-else
-    SAFEWALK_HOSTS=$SAFEWALK_IP_1
-fi
+
+
+
+IFS='.' read -ra ADDR <<< "$SAFEWALK_SUBNET_IP"
+PREFIX=${ADDR[0]}.${ADDR[1]}.${ADDR[2]}
+SAFEWALK_IP_1=$PREFIX.$FIRST_IP
+
+SAFEWALK_HOSTS=
+for (( i = $FIRST_IP; i < $(($FIRST_IP + $COUNT_IP)); i++ ))
+do
+    if [ $i = $FIRST_IP ]; then
+        SAFEWALK_HOSTS="$PREFIX.$i"
+    else
+        SAFEWALK_HOSTS="$SAFEWALK_HOSTS,$PREFIX.$i"
+    fi
+done
 
 bash $my_dir/safewalk_create_gateway.sh --gateway-name "My Gateway" --gateway-password $GATEWAY_ROOT_PASSWORD --gateway-public-host $GATEWAY_PUBLIC_IP --gateway-ssh-host $GATEWAY_IP --safewalk-host $SAFEWALK_HOSTS
 
-if [ "$CLUSTER_ENABLED" = "True" ]; then
+if [ $COUNT_IP > 1 ]; then
     bash $my_dir/safewalk_bdr_create.sh $SAFEWALK_IP_1 $SAFEWALK_SUBNET_IP
 else
     echo "Do nothing"
