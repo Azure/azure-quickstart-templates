9bdd5149452bc031fca180d2959bb58fcc5d2544
ashwinse <asebastian@sysgain.com>
2017-10-25 00:33:37 +0530

    update

--- devopstools-jenkins-chefhabitat-terraform/scripts/install_jenkins.sh
+++ devopstools-jenkins-chefhabitat-terraform/scripts/install_jenkins.sh
@@ -37,8 +37,8 @@ function run_util_script() {
 
 #defaults
 #artifacts_location="https://raw.githubusercontent.com/Azure/azure-devops-utils/master/"
-artifacts_location="https://raw.githubusercontent.com/sysgain/azure-quickstart-templates/msoss-p1/devopstools-jekins-chefhabitat-terraform"
-jenkins_version_location="https://raw.githubusercontent.com/sysgain/azure-quickstart-templates/msoss-p1/devopstools-jekins-chefhabitat-terraform/scripts/jenkins-verified-ver"
+artifacts_location="https://raw.githubusercontent.com/sysgain/azure-quickstart-templates/msoss-p1/devopstools-jenkins-chefhabitat-terraform"
+jenkins_version_location="https://raw.githubusercontent.com/sysgain/azure-quickstart-templates/msoss-p1/devopstools-jenkins-chefhabitat-terraform/scripts/jenkins-verified-ver"
 azure_web_page_location="/usr/share/nginx/azure"
 jenkins_release_type="LTS"
 
