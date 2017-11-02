069ab0b14d22ae4c0d89eceab8330fd4929dd9b9
Vidya <VidyaPatil@users.noreply.github.com>
2017-11-02 17:38:03 +0530

    Silent install Oracle JDK 8 on Ubuntu

--- shibboleth-singlevm-ubuntu/install_shibboleth_idp.sh
+++ shibboleth-singlevm-ubuntu/install_shibboleth_idp.sh
@@ -27,6 +27,9 @@ echo "==============>Installing JDK 8"
 apt-get -y install python-software-properties
 add-apt-repository -y ppa:webupd8team/java
 apt-get -y update
+
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+
 apt-get -y install oracle-java8-installer
 
 
