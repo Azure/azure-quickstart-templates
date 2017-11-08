1e875d7f1261f59f6e59baff4f51c6bdacd5b96b
Michael Greene <michael.greene@microsoft.com>
2017-11-08 09:52:08 -0600

    use common credential and domain name from param

--- 101-automation-configuration/dscConfigurations/DomainControllerConfig.ps1
+++ 101-automation-configuration/dscConfigurations/DomainControllerConfig.ps1
@@ -55,7 +55,7 @@ configuration DomainControllerConfig
 {
 
     $domainCredential = Get-AutomationPSCredential domainCredential
-    $safeModeCredential = Get-AutomationPSCredential safeModeCredential
+    $safeModeCredential = Get-AutomationPSCredential domainCredential
 
     Import-DscResource -ModuleName @{ModuleName='xActiveDirectory';ModuleVersion='2.16.0.0';GUID='9FECD4F6-8F02-4707-99B3-539E940E9FF5'},@{ModuleName='xStorage';ModuleVersion='3.2.0.0';GUID='00d73ca1-58b5-46b7-ac1a-5bfcf5814faf'}
 
