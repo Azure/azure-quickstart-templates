e333cb772d60cbfa452b9d1bb333aa1158407832
Vidya <VidyaPatil@users.noreply.github.com>
2017-11-08 11:22:45 +0530

    Update install_shibboleth_idp.sh

--- shibboleth-singlevm-ubuntu/install_shibboleth_idp.sh
+++ shibboleth-singlevm-ubuntu/install_shibboleth_idp.sh
@@ -26,8 +26,10 @@ source /etc/environment
 apt-get -y install tomcat8
 echo "==============>Configuring SSL for Tomcat8"
 SSLKEYPASSWORD=$(openssl rand -base64 12)
+service tomcat8 restart
 
 mkdir /usr/share/tomcat8/keystore
 keytool -genkey -alias tomcat -keyalg RSA -keystore /usr/share/tomcat8/keystore/server.keystore -keysize 2048 -storepass $SSLKEYPASSWORD -keypass $SSLKEYPASSWORD -dname "cn=testname, ou=shibbolethOU, o=shibbolethO, c=US"
+sed -i '/redirectPort="8443"/a  <Connector port="8443"  protocol="org.apache.coyote.http11.Http11NioProtocol" SSLEnabled="true" maxThreads="150" scheme="https" secure="true"  clientAuth="false" sslProtocol="TLS" address="0.0.0.0" keystoreFile="/\usr/\share\/tomcat8/\keystore/\server.keystore" keystorePass="'$SSLKEYPASSWORD'"/>' /var/lib/tomcat8/conf/server.xml
 
 service tomcat8 restart
