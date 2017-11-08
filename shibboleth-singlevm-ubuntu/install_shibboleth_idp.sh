acb9b07455db51daeecfee28f0e7a8c534ade10c
Vidya <VidyaPatil@users.noreply.github.com>
2017-11-08 11:10:33 +0530

    Update install_shibboleth_idp.sh

--- shibboleth-singlevm-ubuntu/install_shibboleth_idp.sh
+++ shibboleth-singlevm-ubuntu/install_shibboleth_idp.sh
@@ -13,4 +13,21 @@ echo "location"
 echo $location
 echo "Sitename"
 echo $SITENAME
+echo "==============>Install JDK 8"
+add-apt-repository -y ppa:webupd8team/java
+apt-get -y update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+apt-get -y install oracle-java8-installer
 
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+echo JAVA_HOME='"'$JAVA_HOME'"' >> /etc/environment
+source /etc/environment
+
+apt-get -y install tomcat8
+echo "==============>Configuring SSL for Tomcat8"
+SSLKEYPASSWORD=$(openssl rand -base64 12)
+
+mkdir /usr/share/tomcat8/keystore
+keytool -genkey -alias tomcat -keyalg RSA -keystore /usr/share/tomcat8/keystore/server.keystore -keysize 2048 -storepass $SSLKEYPASSWORD -keypass $SSLKEYPASSWORD -dname "cn=testname, ou=shibbolethOU, o=shibbolethO, c=US"
+
+service tomcat8 restart
