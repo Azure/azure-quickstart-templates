b1aa341b5b886a7bdbef80aa08e88d40a435ad68
clint <clint.carr@outlook.com>
2017-11-16 07:45:07 +1100

    Addition of November to download choices, changes to API version. Moved qs-install.ps1 to relative path.

--- qlik-sense-enterprise/scripts/qs-install.ps1
+++ qlik-sense-enterprise/scripts/qs-install.ps1
@@ -17,6 +17,10 @@ $serviceAccountWithDomain = -join ($($env:ComputerName), '\',$($Args[2]))
 $json = @{
     qliksense = @(
         @{
+            name= "Qlik Sense November 2017"
+            url= "https://da3hntz84uekx.cloudfront.net/QlikSense/11.24/0/_MSI/Qlik_Sense_setup.exe"
+        },
+        @{
             name= "Qlik Sense September 2017 Patch 1"
             url= "https://da3hntz84uekx.cloudfront.net/QlikSense/11.14/1/_MSI/Qlik_Sense_update.exe"
             url2= "https://da3hntz84uekx.cloudfront.net/QlikSense/11.14/0/_MSI/Qlik_Sense_setup.exe"
