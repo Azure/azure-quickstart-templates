06b3d59e43a4618b97115e109e822afb3b17b179
unknown <nvtuluva@sysgain.com>
2017-11-27 17:50:25 +0530

    update

--- devopstools-jenkins-chefhabitat-kubernetes/scripts/elkstack_deploy.sh
+++ devopstools-jenkins-chefhabitat-kubernetes/scripts/elkstack_deploy.sh
@@ -84,7 +84,7 @@ cd beats-dashboards-* >> $LOG
 
 #Load Filebeat Index Template in Elasticsearch
 echo "---Load Filebeat Index Template in Elasticsearch---" >> $LOG
-curl -O https://gist.githubusercontent.com/thisismitch/3429023e8438cc25b86c/raw/d8c479e2a1adcea8b1fe86570e42abab0f10f364/filebeat-index-template.json >> $LOG
+curl -O https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/devopstools-jenkins-chefhabitat-kubernetes/scripts/filebeat-index-template.json >> $LOG
 curl -XPUT 'http://localhost:9200/_template/filebeat?pretty' -d@filebeat-index-template.json >> $LOG
 cd /etc/pki/tls/certs/
 #az login --service-principal -u $2 --password $3 --tenant $4 >> $LOG
