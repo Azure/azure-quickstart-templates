580f50e757c9cb05fd5a6ff97466e75ef621a2b1
Vidya <VidyaPatil@users.noreply.github.com>
2018-01-19 13:18:52 +0530

    Resolved JAVAHOME undefined

--- shibboleth-singlevm-ubuntu/install_shibboleth_idp.sh
+++ shibboleth-singlevm-ubuntu/install_shibboleth_idp.sh
@@ -5,38 +5,25 @@ SITENAME=$1.$2.cloudapp.azure.com
 INSTALLDIR=/opt/shibboleth-idp
 
 apt-get -y update
-
-echo "==============>Printing values of all variables"
-echo "domain"
-echo $domain
-echo "location"
-echo $location
-echo "Sitename"
-echo $SITENAME
-echo "==============>Install JDK 8"
-add-apt-repository -y ppa:webupd8team/java
-apt-get -y update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-apt-get -y install oracle-java8-installer
-
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-echo JAVA_HOME='"'$JAVA_HOME'"' >> /etc/environment
-source /etc/environment
-
+apt-get -y upgrade
+apt-get -y install unzip
 apt-get -y install tomcat8
-echo "==============>Configuring SSL for Tomcat8"
+
 SSLKEYPASSWORD=$(openssl rand -base64 12)
-service tomcat8 restart
 
 mkdir /usr/share/tomcat8/keystore
 keytool -genkey -alias tomcat -keyalg RSA -keystore /usr/share/tomcat8/keystore/server.keystore -keysize 2048 -storepass $SSLKEYPASSWORD -keypass $SSLKEYPASSWORD -dname "cn=testname, ou=shibbolethOU, o=shibbolethO, c=US"
 sed -i '/redirectPort="8443"/a  <Connector port="8443"  protocol="org.apache.coyote.http11.Http11NioProtocol" SSLEnabled="true" maxThreads="150" scheme="https" secure="true"  clientAuth="false" sslProtocol="TLS" address="0.0.0.0" keystoreFile="/\usr/\share\/tomcat8/\keystore/\server.keystore" keystorePass="'$SSLKEYPASSWORD'"/>' /var/lib/tomcat8/conf/server.xml
 
-service tomcat8 restart
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+echo JAVA_HOME='"'$JAVA_HOME'"' >> /etc/environment
+source /etc/environment
 
-echo export JAVA_HOME=/usr/lib/jvm/java-8-oracle/ >> /etc/profile
+echo export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64 >> /etc/profile
 echo export CATALINA_HOME=/var/lib/tomcat8 >> /etc/profile
 source /etc/profile
+
+
 sed -i 's,</tomcat-users>,  <role rolename="manager-gui"/>\n  <user username="admin" password="secret" roles="manager-gui"/>  \n</tomcat-users>,g'   /var/lib/tomcat8/conf/tomcat-users.xml
 
 #Change 128m to 512m
@@ -53,13 +40,11 @@ cd /usr/share
 
 # install Shibboleth
 wget http://shibboleth.net/downloads/identity-provider/3.2.1/shibboleth-identity-provider-3.2.1.zip -O shibboleth.zip
-jar -xf shibboleth.zip
+unzip shibboleth.zip
 
 cd shibboleth-identity-provider-3.2.1
 chmod -R +x bin
 
-
-# generate a password for client-side encryption
 echo "idp.sealer.password = $(openssl rand -base64 12)" >credentials.properties
 chmod 0600 credentials.properties
 
@@ -81,8 +66,6 @@ idp.consent.compareValues= true
 idp.consent.maxStoredRecords= -1
 idp.ui.fallbackLanguages= en,de,fr
 EOF
-
-
 
 echo "==============> Running the installer"
 
