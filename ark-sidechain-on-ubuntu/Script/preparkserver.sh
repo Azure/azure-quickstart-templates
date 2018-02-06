f9e59b86424bd2f1f15341c4397ebbed482a41d2
davepinkawa <davepinkawa@gmail.com>
2018-02-06 13:44:17 -0600

    npm change

--- ark-sidechain-on-ubuntu/Script/preparkserver.sh
+++ ark-sidechain-on-ubuntu/Script/preparkserver.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 sudo apt-get -y update && sudo apt-get -y install libpq-dev nodejs npm build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev jq
-sudo npm install grunt-cli@1.2.0 node-sass@4.7.2 forever@0.15.3 angular-cli@1.0.0-beta.28.3
+
+# to be remedied with a --install-deps flag by developers
+npm install grunt-cli@1.2.0
+npm install node-sass@4.7.2
+npm install forever@0.15.3
+npm install angular-cli@1.0.0-beta.28.3
+
 sudo apt-get -y install postgresql postgresql-contrib
