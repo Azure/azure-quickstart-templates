aecbd94ea622a7d68a8804d142bab0ba102e9acc
davepinkawa <davepinkawa@gmail.com>
2018-02-06 13:06:19 -0600

    value changes

--- ark-sidechain-on-ubuntu/Script/preparkserver.sh
+++ ark-sidechain-on-ubuntu/Script/preparkserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get -y update && sudo apt-get -y install npm jq libpq-dev build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev
+sudo apt-get -y update && sudo apt-get -y install libpq-dev nodejs npm build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev jq
 sudo apt-get -y install postgresql postgresql-contrib
