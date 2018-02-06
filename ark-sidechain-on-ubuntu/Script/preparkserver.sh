35ab1b34321863eccba913a577af5786d457738d
davepinkawa <davepinkawa@gmail.com>
2018-02-06 14:04:01 -0600

    removed npm in favor of changing ark-deployer soon

--- ark-sidechain-on-ubuntu/Script/preparkserver.sh
+++ ark-sidechain-on-ubuntu/Script/preparkserver.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 sudo apt-get -y update && sudo apt-get -y install libpq-dev nodejs npm build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev jq
-
-# to be remedied with a --install-deps flag by developers
-sudo npm install grunt-cli
-sudo npm install node-sass
-sudo npm install forever
-sudo npm install angular-cli
-
 sudo apt-get -y install postgresql postgresql-contrib
