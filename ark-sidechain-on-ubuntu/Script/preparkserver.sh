a08ef07c39f23eaddad39aa3a7b45de56c19109c
davepinkawa <davepinkawa@gmail.com>
2018-02-06 13:28:52 -0600

    Testing for npm installation in provisioning script

--- ark-sidechain-on-ubuntu/Script/preparkserver.sh
+++ ark-sidechain-on-ubuntu/Script/preparkserver.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo apt-get -y update && sudo apt-get -y install libpq-dev nodejs npm build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev jq
+sudo npm install grunt-cli@1.2.0 node-sass@4.7.2 forever@0.15.3 angular-cli@1.0.0-beta.28.3
 sudo apt-get -y install postgresql postgresql-contrib
