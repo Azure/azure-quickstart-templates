28998d0a4c672a3f303a33b9e21b627ac850c4ae
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-06 14:32:04 -0600

    nodejs not allowed global for npm compatibility

--- ark-sidechain-on-ubuntu/Script/preparkserver.sh
+++ ark-sidechain-on-ubuntu/Script/preparkserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt-get -y update && apt-get -y install libpq-dev nodejs npm build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev jq
+apt-get -y update && apt-get -y install libpq-dev build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev jq
 apt-get -y install postgresql postgresql-contrib
