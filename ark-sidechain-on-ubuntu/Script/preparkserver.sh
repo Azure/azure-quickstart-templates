75f6e4beefde741befa6fafc80d031383f3b298d
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-06 14:16:32 -0600

    sudo not required

--- ark-sidechain-on-ubuntu/Script/preparkserver.sh
+++ ark-sidechain-on-ubuntu/Script/preparkserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get -y update && sudo apt-get -y install libpq-dev nodejs npm build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev jq
-sudo apt-get -y install postgresql postgresql-contrib
+apt-get -y update && apt-get -y install libpq-dev nodejs npm build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev jq
+apt-get -y install postgresql postgresql-contrib
