770dac087b937a279c36a8aa7951728fb6a37e12
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-13 15:34:56 -0600

    new autoinstall deps flag

--- ark-sidechain-on-ubuntu/Script/arkchaindeploy.sh
+++ ark-sidechain-on-ubuntu/Script/arkchaindeploy.sh
@@ -13,8 +13,8 @@ REWARDPERBLOCK=200000000
 TOTALPREMINE=2100000000000000
 IPADDR=10.0.0.4
 
-~/ark-deployer/sidechain.sh install-node --name $SIDECHAINNAME --database ark_$SIDECHAINNAME --ip $IPADDR --token $CHAINTOKEN --symbol $CHAINSYMBOL --forgers $CHAINFORGERS --max-votes $MAXVOTESPERWALLET --blocktime $CHAINBLOCKTIME --transactions-per-block $CHAINTRANSPERBLOCK --reward-height-start $REWARDSTART --reward-per-block $REWARDPERBLOCK --total-premine $TOTALPREMINE
+~/ark-deployer/sidechain.sh install-node --name $SIDECHAINNAME --database ark_$SIDECHAINNAME --ip $IPADDR --token $CHAINTOKEN --symbol $CHAINSYMBOL --forgers $CHAINFORGERS --max-votes $MAXVOTESPERWALLET --blocktime $CHAINBLOCKTIME --transactions-per-block $CHAINTRANSPERBLOCK --reward-height-start $REWARDSTART --reward-per-block $REWARDPERBLOCK --total-premine $TOTALPREMINE --autoinstall-deps
 ~/ark-deployer/sidechain.sh start-node --name $SIDECHAINNAME
 
-~/ark-deployer/sidechain.sh install-explorer --name $SIDECHAINNAME --token $CHAINTOKEN --ip $IPADDR
+~/ark-deployer/sidechain.sh install-explorer --name $SIDECHAINNAME --token $CHAINTOKEN --ip $IPADDR --autoinstall-deps
 ~/ark-deployer/sidechain.sh start-explorer
