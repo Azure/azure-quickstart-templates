5d6135c703359c15d18c134d9dfa910614f8e56d
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-13 16:58:52 -0600

    remove npm

--- ark-sidechain-on-ubuntu/Script/arkinternalip1.sh
+++ ark-sidechain-on-ubuntu/Script/arkinternalip1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install npm -y
+
 git clone https://github.com/ArkEcosystem/ark-deployer.git
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash && source ~/.profile && nvm install 8.9.1
 
