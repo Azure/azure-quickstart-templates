2890ee49a509968f8dee61f8e04a77a6483937df
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-13 15:54:37 -0600

    attempts

--- ark-sidechain-on-ubuntu/Script/arksidechaindeploy.sh
+++ ark-sidechain-on-ubuntu/Script/arksidechaindeploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/ArkEcosystem/ark-deployer.git && cd ark-deployer
+git clone https://github.com/ArkEcosystem/ark-deployer.git &&
+cd ark-deployer
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 source ~/.profile
 nvm install 8.9.1
