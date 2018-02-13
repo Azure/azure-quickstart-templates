0a67dbc1d3041507bb8c781159ac85d6e20e0415
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-13 15:58:20 -0600

    cd command unnecessary

--- ark-sidechain-on-ubuntu/Script/arksidechaindeploy.sh
+++ ark-sidechain-on-ubuntu/Script/arksidechaindeploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/ArkEcosystem/ark-deployer.git &&
-cd ark-deployer
+git clone https://github.com/ArkEcosystem/ark-deployer.git
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 source ~/.profile
 nvm install 8.9.1
