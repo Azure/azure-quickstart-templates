796bf70f3820053969aad2a55b9c190541d0e0dc
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-13 15:46:43 -0600

    Added changes to script to include ark-deployer

--- ark-sidechain-on-ubuntu/Script/arkchaindeploy.sh
+++ ark-sidechain-on-ubuntu/Script/arkchaindeploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+git clone https://github.com/ArkEcosystem/ark-deployer.git && cd ark-deployer
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+source ~/.profile
+nvm install 8.9.1
+
 # Variables - Current settings are defaults
 SIDECHAINNAME=MyTestChain
 CHAINTOKEN=MyTestToken
