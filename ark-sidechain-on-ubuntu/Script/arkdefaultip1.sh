bf72658682bc5c789b5fe33d233748afb22c4f9a
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-13 20:49:52 -0600

    Update arkdefaultip1.sh

--- ark-sidechain-on-ubuntu/Script/arkdefaultip1.sh
+++ ark-sidechain-on-ubuntu/Script/arkdefaultip1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/ArkEcosystem/ark-deployer.git && apt-get -y install npm
+apt-get -y install npm && git clone https://github.com/ArkEcosystem/ark-deployer.git 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash && source ~/.profile && nvm install 8.9.1
 
 #Variables for installations - default values input below
