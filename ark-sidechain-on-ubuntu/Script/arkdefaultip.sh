b15b2bd72ff0ac9f771c146611eef770c190be0e
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-13 20:46:48 -0600

    rearrange

--- ark-sidechain-on-ubuntu/Script/arkdefaultip.sh
+++ ark-sidechain-on-ubuntu/Script/arkdefaultip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/ArkEcosystem/ark-deployer.git
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash && source ~/.profile && nvm install 8.9.1 && apt-get -y install npm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash && source ~/.profile && apt-get -y install npm && nvm install 8.9.1
 
 #Variables for installations - default values input below
 SIDECHAINNAME=MyTestChain
