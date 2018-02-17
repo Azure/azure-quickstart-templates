dc75a6b331747a76f04a8fb2debd41d5a342981c
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-16 19:36:01 -0600

    Update arkdefaultinstall.sh

--- ark-sidechain-on-ubuntu/Script/arkdefaultinstall.sh
+++ ark-sidechain-on-ubuntu/Script/arkdefaultinstall.sh
@@ -8,8 +8,8 @@ nvm install 8.9.1
 sudo apt-get install -y jq
 
 #Variables for installations - default values input below
-IPNODE=
-IPEXPLORE=
+IPNODE=13.84.184.172
+IPEXPLORE=10.0.0.4
 SIDECHAINNAME=MyTest
 DATABASENAME=ark_mytest
 CHAINTOKEN=MYTEST
@@ -22,8 +22,9 @@ REWARDSTART=75600
 REWARDPERBLOCK=200000000
 TOTALPREMINE=2100000000000000
 
-~/ark-deployer/sidechain.sh install-node --name $SIDECHAINNAME --database $DATABASENAME --token $CHAINTOKEN --symbol $CHAINSYMBOL --forgers $CHAINFORGERS --max-votes $MAXVOTESPERWALLET --blocktime $CHAINBLOCKTIME --transactions-per-block $CHAINTRANSPERBLOCK --reward-height-start $REWARDSTART --reward-per-block $REWARDPERBLOCK --total-premine $TOTALPREMINE --autoinstall-deps
+~/ark-deployer/sidechain.sh install-node --name $SIDECHAINNAME --database $DATABASENAME --token $CHAINTOKEN --symbol $CHAINSYMBOL --ip $IPNODE --forgers $CHAINFORGERS --max-votes $MAXVOTESPERWALLET --blocktime $CHAINBLOCKTIME --transactions-per-block $CHAINTRANSPERBLOCK --reward-height-start $REWARDSTART --reward-per-block $REWARDPERBLOCK --total-premine $TOTALPREMINE --autoinstall-deps
 ~/ark-deployer/sidechain.sh start-node --name $SIDECHAINNAME
 
-~/ark-deployer/sidechain.sh install-explorer --name $SIDECHAINNAME --token $CHAINTOKEN --autoinstall-deps
+~/ark-deployer/sidechain.sh install-explorer --name $SIDECHAINNAME --token $CHAINTOKEN --ip $IPNODE --autoinstall-deps
+
 ~/ark-deployer/sidechain.sh start-explorer &>/dev/null &
