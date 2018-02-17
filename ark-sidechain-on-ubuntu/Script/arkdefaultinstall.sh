dbf18807edb2381f5d1f3f7de931b1e24e05e8fd
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-16 21:08:48 -0600

    Update arkdefaultinstall.sh

--- ark-sidechain-on-ubuntu/Script/arkdefaultinstall.sh
+++ ark-sidechain-on-ubuntu/Script/arkdefaultinstall.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
+echo "running update"
 sudo apt-get -y update
+echo "downloading ark-deployer"
 git clone https://github.com/ArkEcosystem/ark-deployer.git 
+echo "downloading nvm"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-source ~/.profile && nvm install 8.9.1
+echo "sourcing nvm and running install"
+source ~/.profile
+~/.nvm/nvm.sh install 8.9.1
+source ~/.profile
 sudo apt-get install -y jq
 
 #Variables for installations
