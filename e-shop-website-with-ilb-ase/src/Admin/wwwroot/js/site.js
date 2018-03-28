35e33d93ef560cecad65e1e17805bc17d9fc7068
Tyler Lu <tyler.lu@canviz.com>
2018-03-28 18:00:16 +0800

    Use source code from other repo

--- e-shop-website-with-ilb-ase/src/Admin/wwwroot/js/site.js
+++
@@ -1,6 +0,0 @@
-﻿/*   
- *   * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.  
- *   * See LICENSE in the project root for license information.  
- */
-
-// Write your JavaScript code.
