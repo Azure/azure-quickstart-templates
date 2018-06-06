367dcb9d2f754dc6b6cda943ffcde0216ae8707e
Bin Xia <binxi@microsoft.com>
2018-06-06 09:33:08 +0000

    Fixed the version of Azure CLI

--- bosh-setup/scripts/setup_env.sh
+++ bosh-setup/scripts/setup_env.sh
@@ -41,7 +41,7 @@ AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | tee /etc/apt/sources.list.d/azure-cli.list
 curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 retryop "apt-get install apt-transport-https"
-retryop "apt-get update && apt-get install azure-cli"
+retryop "apt-get update && apt-get install azure-cli=2.0.33-1~$AZ_REPO"
 
 echo "Creating the containers (bosh and stemcell) and the table (stemcells) in the default storage account"
 default_storage_account=$(get_setting DEFAULT_STORAGE_ACCOUNT_NAME)
