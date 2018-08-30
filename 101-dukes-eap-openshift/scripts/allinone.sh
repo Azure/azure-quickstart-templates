330f13edaef65fa70816f3f0d47e9c0cab07c9ab
MyriamFentanes <myriam.fentanes@gmail.com>
2018-08-30 12:36:07 -0400

    adding activation key support

--- 101-dukes-eap-openshift/scripts/allinone.sh
+++ 101-dukes-eap-openshift/scripts/allinone.sh
@@ -41,7 +41,7 @@ domain=$(grep search /etc/resolv.conf | awk '{print $2}')
 ps -ef | grep allinone.sh > cmdline.out
 
 swapoff -a
-subscription-manager register --username=${RHSM_USER} --password=${RHSM_PASSWORD}
+subscription-manager register --username=${RHSM_USER} --password=${RHSM_PASSWORD} || subscription-manager register --org=${RHSM_USER} -- activationkey=${RHSM_PASSWORD}
 subscription-manager attach --pool=${RHSM_POOL}
 subscription-manager repos --disable="*" --enable="rhel-7-server-rpms" --enable="rhel-7-server-extras-rpms" --enable="rhel-7-server-ose-3.9-rpms" --enable="rhel-7-fast-datapath-rpms" --enable="rhel-7-server-ansible-2.4-rpms"
 yum install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct httpd-tools pyOpenSSL ansible atomic-openshift-utils
