7e7e21b5ef7719cdd0885a399b4afd4b5f07da29
Srđan Božović <srdjan.bozovic@live.com>
2018-09-09 14:14:17 +0200

    add: verification of PS version and Managed Instance name

--- 201-sqlmi-new-vnet-w-point-to-site-vpn/scripts/deploy.ps1
+++ 201-sqlmi-new-vnet-w-point-to-site-vpn/scripts/deploy.ps1
@@ -10,16 +10,52 @@ $parameters.Remove('subscriptionId')
 $parameters.Remove('resourceGroupName')
 $parameters.Remove('certificateNamePrefix')
 
-function Ensure-Login () 
+$managedInstanceName = $parameters['managedInstanceName']
+
+function EnsureLogin() 
 {
     $context = Get-AzureRmContext
-    If($context.Subscription -eq $null)
+    If($null -eq $context.Subscription)
     {
         Login-AzureRmAccount | Out-null
     }
 }
 
-Ensure-Login
+function VerifyPSVersion
+{
+    Write-Host "Verifying PowerShell version, must be 5.0 or higher."
+    if($PSVersionTable.PSVersion.Major -ge 5)
+    {
+        Write-Host "PowerShell version verified." -ForegroundColor Green
+    }
+    else
+    {
+        Write-Host "You need to install PowerShell version 5.0 or heigher." -ForegroundColor Red
+        Break;
+    }
+}
+
+function VerifyManagedInstanceName
+{
+    param($managedInstanceName)
+    Write-Host "Verifying Managed Instance name, must be globally unique."
+    if([string]::IsNullOrEmpty($managedInstanceName))
+    {
+        Write-Host "Managed Instance name is required parameter." -ForegroundColor Red
+        break;
+    }
+    if($null -ne (Resolve-DnsName ($managedInstanceName+'.provisioning.database.windows.net') -ErrorAction SilentlyContinue))
+    {
+        Write-Host "Managed Instance name already in use." -ForegroundColor Red
+        break;
+    }
+    Write-Host "Managed Instance name verified." -ForegroundColor Green
+}
+
+VerifyPSVersion
+VerifyManagedInstanceName $managedInstanceName
+
+EnsureLogin
 
 $context = Get-AzureRmContext
 If($context.Subscription.Id -ne $subscriptionId)
