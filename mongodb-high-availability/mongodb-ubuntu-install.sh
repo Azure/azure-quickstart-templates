5852f35a4a0698eb9c03db7b92170ecb1531399e
Philip Niehus <philip@holobuilder.com>
2018-09-12 13:54:08 +0200

    Fix mongodb installer-script
    Changes:
    * Added missing -v option to parsing loop

--- mongodb-high-availability/mongodb-ubuntu-install.sh
+++ mongodb-high-availability/mongodb-ubuntu-install.sh
@@ -82,7 +82,7 @@ then
 fi
 
 # Parse script parameters
-while getopts :i:b:r:k:u:p:x:n:alh optname; do
+while getopts :i:b:v:r:k:u:p:x:n:alh optname; do
 
 	# Log input parameters (except the admin password) to facilitate troubleshooting
 	if [ ! "$optname" == "p" ] && [ ! "$optname" == "k" ]; then
