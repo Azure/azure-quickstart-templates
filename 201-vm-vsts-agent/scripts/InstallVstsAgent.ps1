22a290ff10330171115d02d7b709278e2d9b2996
Mark Allison <Mark.Allison@sabin.io>
2018-10-02 17:04:17 +0100

    updated URL to dev.azure.com

--- 201-vm-vsts-agent/scripts/InstallVstsAgent.ps1
+++ 201-vm-vsts-agent/scripts/InstallVstsAgent.ps1
@@ -37,7 +37,7 @@ $agentTempFolderName = Join-Path $env:temp ([System.IO.Path]::GetRandomFileName(
 New-Item -ItemType Directory -Force -Path $agentTempFolderName
 Write-Verbose "Temporary Agent download folder: $agentTempFolderName" -verbose
 
-$serverUrl = "https://$VSTSAccount.visualstudio.com"
+$serverUrl = "https://dev.azure.com/$VSTSAccount"
 Write-Verbose "Server URL: $serverUrl" -verbose
 
 $retryCount = 3
