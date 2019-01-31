7c3a4272538a18e7f4d308f17d7f69cfc621cb71
jfrog <jainishs@jfrog.com>
2019-01-30 17:07:32 -0800

    updating script

--- artifactory-enterprise/scripts/install_artifactory.sh
+++ artifactory-enterprise/scripts/install_artifactory.sh
@@ -189,10 +189,12 @@ cat <<EOF >/var/opt/jfrog/artifactory/etc/binarystore.xml
 </config>
 EOF
 
-sed -i -e "s/art1/art-$(date +%s$RANDOM)/" /var/opt/jfrog/artifactory/etc/ha-node.properties
-sed -i -e "s/127.0.0.1/$(hostname -i)/" /var/opt/jfrog/artifactory/etc/ha-node.properties
-sed -i -e "s/127.0.0.2/$(hostname -i)/" /var/opt/jfrog/artifactory/etc/ha-node.properties
-sed -i -e "s/172.25.0.3/$(hostname -i)/" /var/opt/jfrog/artifactory/etc/ha-node.properties
+HOSTNAME=$(hostname -i)
+sed -e "s/art1/art-$(date +%s$RANDOM)/" /var/opt/jfrog/artifactory/etc/ha-node.properties > /var/opt/jfrog/artifactory/etc/ha-node.properties.tmp
+sed -e "s/127.0.0.1/${HOSTNAME}/" /var/opt/jfrog/artifactory/etc/ha-node.properties.tmp > /var/opt/jfrog/artifactory/etc/ha-node.properties.tmp1
+sed -e "s/127.0.0.2/${HOSTNAME}/" /var/opt/jfrog/artifactory/etc/ha-node.properties.tmp1 > /var/opt/jfrog/artifactory/etc/ha-node.properties.tmp2
+sed -e "s/172.25.0.3/${HOSTNAME}/" /var/opt/jfrog/artifactory/etc/ha-node.properties.tmp2 > /var/opt/jfrog/artifactory/etc/ha-node.properties
+rm /var/opt/jfrog/artifactory/etc/ha-node.properties.tmp*
 
 cat /var/lib/cloud/instance/user-data.txt | grep "^CERTIFICATE=" | sed "s/CERTIFICATE=//" > /tmp/temp.pem
 cat /tmp/temp.pem | sed 's/CERTIFICATE----- /&\n/g' | sed 's/ -----END/\n-----END/g' | awk '{if($0 ~ /----/) {print;} else { gsub(/ /,"\n");print;}}' > /etc/pki/tls/certs/cert.pem
