b31a07a5a3a449293d920c316eb2a8b816ac582c
Daniel Oh <doh@danieloh.redhat.com>
2019-04-05 14:32:52 +0900

    Fixed azuredeploy.json

--- jboss-eap-clustered-rhel7/scripts/eap-setup-redhat.sh
+++ jboss-eap-clustered-rhel7/scripts/eap-setup-redhat.sh
@@ -3,10 +3,7 @@
 echo "Red Hat JBoss EAP 7 Clustered Intallation Start: " | /bin/date +%H:%M:%S  >> /home/$1/install.log
 
 export JBOSS_HOME="/opt/rh/eap7/root/usr/share/wildfly"
-export NODENAME1="node1"
-export NODENAME2="node2"
 export SVR_CONFIG="standalone-ha.xml"
-export PORT_OFFSET=100
 
 export EAP_USER=$2
 export EAP_PASSWORD=$3
@@ -40,31 +37,22 @@ echo "JBoss EAP RPM installating..." >> /home/$1/install.log
 yum-config-manager --disable rhel-7-server-htb-rpms 
 yum groupinstall -y jboss-eap7 
 
-echo "Create 2 EAP nodes on AZure..." >> /home/$1/install.log 
-/bin/cp  -rL  $JBOSS_HOME/standalone $JBOSS_HOME/$NODENAME1
-/bin/cp  -rL  $JBOSS_HOME/standalone $JBOSS_HOME/$NODENAME2
-
 echo "Eap session replication app deploy..." >> /home/$1/install.log 
 yum install -y git
 cd /home/$1
 git clone https://github.com/danieloh30/eap-session-replication.git
-/bin/cp -rf /home/$1/eap-session-replication/eap-configuration/standalone-ha.xml $JBOSS_HOME/$NODENAME1/configuration/
-/bin/cp -rf /home/$1/eap-session-replication/eap-configuration/standalone-ha.xml $JBOSS_HOME/$NODENAME2/configuration/
-/bin/cp -rf /home/$1/eap-session-replication/target/eap-session-replication.war $JBOSS_HOME/$NODENAME1/deployments/eap-session-replication.war
-/bin/cp -rf /home/$1/eap-session-replication/target/eap-session-replication.war $JBOSS_HOME/$NODENAME2/deployments/eap-session-replication.war
-touch $JBOSS_HOME/$NODENAME1/deployments/eap-session-replication.war.dodeploy
-touch $JBOSS_HOME/$NODENAME2/deployments/eap-session-replication.war.dodeploy
+/bin/cp -rf /home/$1/eap-session-replication/eap-configuration/standalone-ha.xml $JBOSS_HOME/standalone/configuration/
+/bin/cp -rf /home/$1/eap-session-replication/target/eap-session-replication.war $JBOSS_HOME/standalone/deployments/eap-session-replication.war
+touch $JBOSS_HOME/standalone/deployments/eap-session-replication.war.dodeploy
 
 echo "Configuring EAP managment user..." >> /home/$1/install.log 
 $JBOSS_HOME/bin/add-user.sh -u $EAP_USER -p $EAP_PASSWORD -g 'guest,mgmtgroup' 
 
 echo "Start EAP 7 instances..." >> /home/$1/install.log 
-$JBOSS_HOME/bin/standalone.sh -Djboss.node.name=$NODENAME1 -Djboss.server.base.dir=$JBOSS_HOME/$NODENAME1 -c $SVR_CONFIG -b $IP_ADDR -bmanagement $IP_ADDR -bprivate $IP_ADDR -Djboss.jgroups.azure_ping.storage_account_name=$STORAGE_ACCOUNT_NAME -Djboss.jgroups.azure_ping.storage_access_key=$STORAGE_ACCESS_KEY -Djboss.jgroups.azure_ping.container=$CONTAINER_NAME > /dev/null 2>&1 &
-#$JBOSS_HOME/bin/standalone.sh -Djboss.node.name=$NODENAME2 -Djboss.server.base.dir=$JBOSS_HOME/$NODENAME2 -c $SVR_CONFIG -b $IP_ADDR -bmanagement $IP_ADDR -bprivate $IP_ADDR -Djboss.jgroups.azure_ping.storage_account_name=$STORAGE_ACCOUNT_NAME -Djboss.jgroups.azure_ping.storage_access_key=$STORAGE_ACCESS_KEY -Djboss.jgroups.azure_ping.container=$CONTAINER_NAME -Djboss.socket.binding.port-offset=$PORT_OFFSET > /dev/null 2>&1 &
+$JBOSS_HOME/bin/standalone.sh -c $SVR_CONFIG -b $IP_ADDR -bmanagement $IP_ADDR -bprivate $IP_ADDR -Djboss.jgroups.azure_ping.storage_account_name=$STORAGE_ACCOUNT_NAME -Djboss.jgroups.azure_ping.storage_access_key=$STORAGE_ACCESS_KEY -Djboss.jgroups.azure_ping.container=$CONTAINER_NAME > /dev/null 2>&1 &
 
 echo "Configure firewall for ports 8080, 9990..." >> /home/$1/install.log 
 firewall-cmd --zone=public --add-port=8080/tcp --permanent 
-firewall-cmd --zone=public --add-port=8180/tcp --permanent 
 firewall-cmd --zone=public --add-port=9990/tcp --permanent 
 firewall-cmd --reload 
 
