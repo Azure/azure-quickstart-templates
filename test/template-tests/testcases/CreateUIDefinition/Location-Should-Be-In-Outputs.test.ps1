ae56c8dcf8b0a286fed8fd6819b65fb2cff12ebe
StartAutomating <james.brundage@start-automating.com>
2019-08-15 20:33:53 -0700

    Removing Throws, Improving Documentation, Adding Good/Bad Input

--- test/template-tests/testcases/CreateUIDefinition/Location-Should-Be-In-Outputs.test.ps1
+++ test/template-tests/testcases/CreateUIDefinition/Location-Should-Be-In-Outputs.test.ps1
@@ -1,29 +1,39 @@
-﻿param(
+﻿<#
+.Synopsis
+    Ensures that the .location field exists in .outputs.
+.Description
+    Ensures that the .location field exists in .outputs, and is [location()]
+.Example
+    Test-AzureRMTemplate -TemplatePath .\100-marketplace-sample -Test Location-Should-Be-In-Outputs
+.Example
+    .\Location-Should-Be-In-Outputs.test.ps1 ([PSCustomObject]@{BadInput=$true})
+#>
+param(
 [Parameter(Mandatory=$true,Position=0)]
 [PSObject]
 $CreateUIDefinitionObject
 )
+<#{
+    Bad: {},
+    Good: {
+        parameters: {
+            outputs: {
+                location:"[location()]"
+            }
+        }
+    }                
+}#>
 
-# First, check that CreateUIDefinition has an outputs
-if (-not $CreateUIDefinitionObject.parameters.outputs) {
-    # (if it didn't, throw an error)
-    throw "Outputs is missing from CreateUIDefinition"
-    $CreateUIDefinitionObject.parameters.outputs | Out-String
+
+if (-not $CreateUIDefinitionObject.parameters.outputs) { # If CreateUIDefinition has no outputs
+    Write-Error "Outputs is missing from CreateUIDefinition" -ErrorId CreateUIDefinition.Missing.Outputs # error
+    return # and return.
 }
-if (-not $CreateUIDefinitionObject.parameters.outputs.location) {
-    throw "Location is missing from outputs"
+if (-not $CreateUIDefinitionObject.parameters.outputs.location) { # If outputs does not have a location    
+    Write-Error "Location is missing from outputs" -ErrorId CreateUIDefinition.Missing.Outputs.Location # error
+    return # and return.
 }
 
-# Then, check that the location property exists in outputs
-if (-not $CreateUIDefinitionObject.parameters.outputs.location) { 
-    # (if it didn't, throw an error).
-    throw "Location is missing from outputs"
-}
-
-# Last, make sure that the location's trimmed value is [location()]
-if ("$($CreateUIDefinitionObject.outputs.location)".Trim() -ne '[location()]') {
-    # (if it wasn't, throw an error)
-    if ("$($CreateUIDefinitionObject.parameters.outputs.location)".Trim() -ne '[location()]') {
-        throw "CreateUIDefinition.outputs.location must be [location()]"
-    }
+if ("$($CreateUIDefinitionObject.parameters.outputs.location)".Trim() -ne '[location()]') { # Last, make sure that the location's trimmed value is [location()].
+    Write-Error "CreateUIDefinition.outputs.location must be [location()]" -ErrorId CreateUIDefinition.Incorrect.Outputs.Location -TargetObject $CreateUIDefinitionObject.parameters.outputs.location
 }
