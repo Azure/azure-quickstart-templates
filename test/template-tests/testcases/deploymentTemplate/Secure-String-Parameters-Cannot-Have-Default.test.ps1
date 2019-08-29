20d14d647626079a6e3180f7fa4f29a07281673c
Brian Moore (AZURE RESOURCE MANAGER) <bmoore@microsoft.com>
2019-08-29 14:16:33 -0500

    allowing for a newGuid in a defaultValue

--- test/template-tests/testcases/deploymentTemplate/Secure-String-Parameters-Cannot-Have-Default.test.ps1
+++ test/template-tests/testcases/deploymentTemplate/Secure-String-Parameters-Cannot-Have-Default.test.ps1
@@ -7,7 +7,7 @@ foreach ($parameterProp in $templateObject.parameters.psobject.properties) {
     $parameter = $parameterProp.Value
     $name = $parameterProp.Name
     if ($parameter.Type -eq 'securestring' -and 
-        $parameter.defaultValue) # Will return true when defaultvalue is not null or blank (blank values are OK). 
+        ($parameter.defaultValue -and $parameter.defaultValue -like '[*newGuid(*]')) # Will return true when defaultvalue is not null or blank (blank values are OK). 
     {
         Write-Error -Message "Parameter $name is a SecureString, and must not have a default value." `
             -ErrorId SecureString.Must.Not.Have.Default -TargetObject $parameter
