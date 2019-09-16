08822291fe56f74c5385a43593698c898796f3d0
Brian Moore (AZURE RESOURCE MANAGER) <bmoore@microsoft.com>
2019-09-16 09:55:00 -0500

    updates to write-results

--- test/ci-scripts/Write-TestResults.ps1
+++ test/ci-scripts/Write-TestResults.ps1
@@ -1,20 +1,21 @@
 ﻿<#
 
 This script is used to update the table where the test results for each sample are stored.
+Typical scenario is that results will be passed in for only one cloud Public or Fairfax - so the 
 
 #>
 
 param(
-    [Parameter(Mandatory = $true)]$SourceDirectory = ".",
-    [Parameter(Mandatory = $true)]$StorageAccountResourceGroupName = "ttk-gen-artifacts-storage",
-    [Parameter(Mandatory = $true)]$StorageAccountName = "azbotstorage",
-    [Parameter(Mandatory = $true)]$TableName = "QuickStartDeploymentStatus",
-    [Parameter(Mandatory = $true)]$RowKey = "100-blank-template",
-    [Parameter(Mandatory = $false)]$BestPracticeResult = $false,
-    [Parameter(Mandatory = $false)]$CredScanResult = $false,
-    [Parameter(Mandatory = $false)]$FairfaxDeployment = $false,
+    [Parameter(Mandatory = $true)]$SampleFolder,
+    [Parameter(Mandatory = $true)]$BuildSourcesDirectory,
+    [Parameter(Mandatory = $false)]$StorageAccountResourceGroupName = "ttk-gen-artifacts-storage",
+    [Parameter(Mandatory = $false)]$StorageAccountName = "azbotstorage",
+    [Parameter(Mandatory = $false)]$TableName = "QuickStartDeploymentStatus",
+    [Parameter(Mandatory = $false)]$BestPracticeResult = $null,
+    [Parameter(Mandatory = $false)]$CredScanResult = $null,
+    [Parameter(Mandatory = $false)]$FairfaxDeployment = $null,
     [Parameter(Mandatory = $false)]$FairfaxLastTestDate = (Get-Date -Format "yyyy-MM-dd").ToString(),
-    [Parameter(Mandatory = $false)]$PublicDeployment = $false,
+    [Parameter(Mandatory = $false)]$PublicDeployment = $null,
     [Parameter(Mandatory = $false)]$PublicLastTestDate = (Get-Date -Format "yyyy-MM-dd").ToString()
 )
 
@@ -23,38 +24,53 @@ $ctx = (Get-AzStorageAccount -Name $StorageAccountName -ResourceGroupName $Stora
 $cloudTable = (Get-AzStorageTable –Name $tableName –Context $ctx).CloudTable
 
 #Get the type of Sample from metadata.json, needed for the partition key lookup
-$PathToSample = ("$SourceDirectory\$RowKey\metadata.json").Replace("@", "\")
-$Metadata = Get-Content $PathToSample -Raw | ConvertFrom-Json
+$PathToMetadata = "$SampleFolder\metadata.json"
+Write-Host "PathToMetadata: $PathToMetadata"
+
+$RowKey = $SampleFolder.Replace("$BuildSourcesDirectory\", "").Replace("\", "@").Replace("/", "@")
+Write-Host "RowKey: $RowKey"
+
+$Metadata = Get-Content $PathToMetadata -Raw | ConvertFrom-Json
 $PartitionKey = $Metadata.Type
 
 #Get the row to update
 $r = Get-AzTableRow -table $cloudTable -PartitionKey $PartitionKey -RowKey $RowKey
 
-# if the record doesn't exist, probably a new sample and needs to be added
+# if the record doesn't exist, this is probably a new sample and needs to be added (or we just cleaned the table)
 if ($r -eq $null) {
+
+    $results = New-Object -TypeName hashtable
+    $results.Add("BestPracticeResult", $BestPracticeResult)
+    $results.Add("CredScanResult", $CredScanResult)
+    # set the values for Fairfax only if a result was passed
+    if ($FairfaxDeployment -ne $null) { 
+        $results.Add("FairfaxDeployment", $FairfaxDeployment) 
+        $results.Add("FairfaxLastTestDate", $FairfaxLastTestDate) 
+    }
+    # set the values for MAC only if a result was passed
+    if ($PublicDeployment -ne $null) {
+        $results.Add("PublicDeployment", $PublicDeployment) 
+        $results.Add("PublicLastTestDate", $PublicLastTestDate) 
+    }
 
     Add-AzTableRow -table $cloudTable `
         -partitionKey $PartitionKey `
         -rowKey $RowKey `
-        -property @{"BestPracticeResult" = $BestPracticeResult; `
-            "CredScanResult"             = $CredScanResult; `
-            "FairfaxDeployment"          = $FairfaxDeployment; `
-            "FairfaxLastTestDate"        = $FairfaxLastTestDate; `
-            "PublicDeployment"           = $PublicDeployment; `
-            "PublicLastTestDate"         = $PublicLastTestDate; 
-    }
+        -property $results
 }
-else { # Update the existing row
-
+else {
+    # Update the existing row
     $r.BestPracticeResult = $BestPracticeResult
     $r.CredScanResult = $CredScanResult
-    $r.FairfaxDeployment = $FairfaxDeployment
-    $r.FairfaxLastTestDate = $FairfaxLastTestDate
-    $r.PublicDeployment = $PublicDeployment
-    $r.PublicLastTestDate = $PublicLastTestDate
-
+    # set the values for FF only if a result was passed
+    if ($FairfaxDeployment -ne $null) { 
+        $r.FairfaxDeployment = $FairfaxDeployment
+        $r.FairfaxLastTestDate = $FairfaxLastTestDate 
+    }
+    # set the values for MAC only if a result was passed
+    if ($PublicDeployment -ne $null) {
+        $r.PublicDeployment = $PublicDeployment 
+        $r.PublicLastTestDate = $PublicLastTestDate 
+    }
     $r | Update-AzTableRow -table $cloudTable
 }
-
-#$t = Get-AzTableRow -table $cloudTable | Sort-Object -Property $TableSortKey 
-#$t | ft
