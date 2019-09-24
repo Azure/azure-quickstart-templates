efffb04ab7f80426abbf411bd1572ae320701cea
Brian Moore (AZURE RESOURCE MANAGER) <bmoore@microsoft.com>
2019-09-24 17:25:35 -0500

    the reason we have unit tests

--- test/template-tests/testcases/deploymentTemplate/IDs-Should-Be-Derived-From-ResourceIDs.test.ps1
+++ test/template-tests/testcases/deploymentTemplate/IDs-Should-Be-Derived-From-ResourceIDs.test.ps1
@@ -34,7 +34,7 @@ foreach ($id in $ids) { # Then loop over each object with an ID
     if ($expandedId -notmatch "\s{0,}\[\s{0,}(extensionResourceId|resourceId)\s{0,}\(\s{0,}"  -and ` # this is an "or" scenario since extensionResourceId should contain resourceId and would provide non-whitespace before the function name
         $expandedId -notmatch "\s{0,}\[\s{0,}subscriptionResourceId\s{0,}\(\s{0,}'" -and `
         $expandedId -notmatch "\s{0,}\[\s{0,}tenantResourceId\s{0,}\(\s{0,}'" -and `
-        $expandedId -notmatch "\s{0,}\[\s{0,}if\s{0,}\(\s{0,}'" -and `
+        $expandedId -notmatch "\s{0,}\[\s{0,}if\s{0,}\(\s{0,}" -and `
         $expandedId -notmatch "\s{0,}\[\s{0,}parameters\s{0,}\(\s{0,}'" -and `
         $expandedId -notmatch "\s{0,}\[\s{0,}variables\s{0,}\(\s{0,}'" ){
             Write-Error "Property: `"$($id.propertyName)`" must use one of the following expressions for an resourceId property
