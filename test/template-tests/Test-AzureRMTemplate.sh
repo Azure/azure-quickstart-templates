d85e6592a90dc2548750970b8e0f74da8518df8b
Ed Burns <edburns@microsoft.com>
2019-10-07 21:11:35 -0400

    #6599 change powershell to pwsh

--- test/template-tests/Test-AzureRMTemplate.sh
+++ test/template-tests/Test-AzureRMTemplate.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-powershell -noprofile -nologo -command "Import-Module '$(dirname $(readlink -f $0))/AzRMTester.psd1'; Test-AzureRMTemplate $@ ; if (\$error.Count) { exit 1}"
+pwsh -noprofile -nologo -command "Import-Module '$(dirname $(readlink -f $0))/AzRMTester.psd1'; Test-AzureRMTemplate $@ ; if (\$error.Count) { exit 1}"
