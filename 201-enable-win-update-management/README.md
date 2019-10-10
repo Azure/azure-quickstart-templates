6216fbdaf21c6a90f97f8d4504c85e0ceafc45a7
Kasun Rajapakse <kasunsjc@hotmail.com>
2019-10-10 13:41:29 +0530

    Add Update Management

---
+++ 201-enable-win-update-management/README.md
@@ -0,0 +1,21 @@
+# This template Create a VM with Update Management enabled.
+
+<IMG SRC="https://azurequickstartsservice.blob.core.windows.net/badges/201-enable-win-update-management/PublicLastTestDate.svg" />&nbsp;
+<IMG SRC="https://azurequickstartsservice.blob.core.windows.net/badges/201-enable-win-update-management/PublicDeployment.svg" />&nbsp;
+
+<IMG SRC="https://azurequickstartsservice.blob.core.windows.net/badges/201-enable-win-update-management/FairfaxLastTestDate.svg" />&nbsp;
+<IMG SRC="https://azurequickstartsservice.blob.core.windows.net/badges/201-enable-win-update-management/FairfaxDeployment.svg" />&nbsp;
+
+<IMG SRC="https://azurequickstartsservice.blob.core.windows.net/badges/201-enable-win-update-management/BestPracticeResult.svg" />&nbsp;
+<IMG SRC="https://azurequickstartsservice.blob.core.windows.net/badges/201-enable-win-update-management/CredScanResult.svg" />&nbsp;
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2Fazure-quickstart-templates%2Fmaster%2F201-enable-win-update-management%2Fazuredeploy.json" target="_blank">
+    <img src="https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/1-CONTRIBUTION-GUIDE/images/deploytoazure.png"/>
+</a>
+<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2Fazure-quickstart-templates%2Fmaster%2F201-enable-win-update-management%2Fazuredeploy.json" target="_blank">
+    <img src="https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/1-CONTRIBUTION-GUIDE/images/visualizebutton.png"/>
+</a>
+
+This template disables encryption on a running VM Scale Set of Windows VMs.
+
+For more information about Azure Disk Encryption, see https://docs.microsoft.com/en-us/azure/security/enable-win-update-management
---
+++ 201-enable-win-update-management/scripts/CreateSheduleUpdate.ps1
@@ -0,0 +1,29 @@
+#Create Resource Group
+$ResourceGroup = (New-AzResourceGroup -Name VM-UpdateMgt -Location "Southeast Asia").ResourceGroupName
+
+#Create ARM Deployment
+New-AzResourceGroupDeployment -Name VM-UpdateMgt -ResourceGroupName $ResourceGroup -Mode Incremental -TemplateFile ../azuredeploy.json -TemplateParameterFile ../azuredeploy.parameters.json -Verbose
+
+#Configure Update Management
+
+$duration = New-TimeSpan -Hours 2
+$StartTime = (Get-Date "02:00:00").AddDays(5)
+$ImediateUpdateStartTime = (Get-Date).AddHours(1)
+[System.DayOfWeek[]]$WeekendDay = [System.DayOfWeek]::Sunday
+$AutomationAccountName = (Get-AzAutomationAccount -ResourceGroupName $ResourceGroup).AutomationAccountName 
+$LogAnalyticsWorkspaceName = (Get-AzOperationalInsightsWorkspace -ResourceGroupName $ResourceGroup).Name
+
+
+#Create a Weekly Scedule 
+$Schedule = New-AzAutomationSchedule -AutomationAccountName $AutomationAccountName -Name "WeeklyCriticalSecurity" -StartTime $StartTime -WeekInterval 1 -DaysOfWeek $WeekendDay -ResourceGroupName $ResourceGroup -Verbose
+#Imediate Update Scedule
+$ImediateUpdateSchedule = New-AzAutomationSchedule -AutomationAccountName $AutomationAccountName -Name "FullUpdate" -StartTime $ImediateUpdateStartTime -OneTime -ResourceGroupName $ResourceGroup -Verbose
+
+#Get VM IDs
+$VMIDs = (Get-AzVM -ResourceGroupName $ResourceGroup).Id 
+
+#Software Update Weekly configuration
+New-AzAutomationSoftwareUpdateConfiguration -ResourceGroupName $ResourceGroup -Schedule $Schedule -Windows -AzureVMResourceId $VMIDs -Duration $duration -IncludedUpdateClassification Critical,Security,Definition -AutomationAccountName $AutomationAccountName -Verbose
+
+#Software Update Onetime configuration
+New-AzAutomationSoftwareUpdateConfiguration -ResourceGroupName $ResourceGroup -Schedule $ImediateUpdateSchedule -Windows -AzureVMResourceId $VMIDs -Duration $duration -IncludedUpdateClassification Critical,Security,ServicePack,FeaturePack,UpdateRollup,Updates,Definition -AutomationAccountName $AutomationAccountName -Verbose
