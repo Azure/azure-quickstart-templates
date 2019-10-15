a4ccde2785465a928920940ca3d2d8fb8c4df41b
Ryan Williams <rwilliams@sol-tec.com>
2019-10-15 14:42:07 +0100

    Update HVHostSetup.ps1

--- 301-nested-vms-in-virtual-network/HVHostSetup.ps1
+++ 301-nested-vms-in-virtual-network/HVHostSetup.ps1
@@ -1,6 +1,7 @@
 [cmdletbinding()]
 param (
-    [string]$GhostedSubnet = "172.16.2.0/24"
+    [string]$GhostedSubnetPrefix = "172.16.2.0/24",
+    [string]$VirtualNetworkPrefix = "172.16.0.0/22"
 )
 
 New-VMSwitch -Name "NestedSwitch" -SwitchType Internal
@@ -9,7 +10,8 @@ $NIC1IP = Get-NetIPAddress | Where-Object -Property AddressFamily -EQ IPv4 | Whe
 $NIC2IP = Get-NetIPAddress | Where-Object -Property AddressFamily -EQ IPv4 | Where-Object -Property InterfaceAlias -EQ "Ethernet 2"
 $NATSubnet = Get-Subnet -IP $NIC1IP.IPAddress -MaskBits $NIC1IP.PrefixLength
 $HyperVSubnet = Get-Subnet -IP $NIC2IP.IPAddress -MaskBits $NIC2IP.PrefixLength
-$NestedSubnet = Get-Subnet $GhostedSubnet
+$NestedSubnet = Get-Subnet $GhostedSubnetPrefix
+$VirtualNetwork = Get-Subnet $VirtualNetworkPrefix
 
 New-NetIPAddress -IPAddress $NestedSubnet.HostAddresses[0] -InterfaceAlias "vEthernet (NestedSwitch)"
 
@@ -17,3 +19,7 @@ Add-DhcpServerv4Scope -Name "Nested VMs" -StartRange $NestedSubnet.HostAddresses
 Set-DhcpServerv4OptionValue -DnsServer 168.63.129.16 -Router $NestedSubnet.HostAddresses[0]
 
 Install-RemoteAccess -VpnType RoutingOnly
+cmd.exe /c "netsh routing ip nat install"
+cmd.exe /c "netsh routing ip nat add interface '$($NIC1IP.InterfaceAlias)'"
+cmd.exe /c "netsh routing ip add persistentroute dest=$($NatSubnet.NetworkAddress) mask=$($NATSubnet.SubnetMask) name=$($NIC1IP.InterfaceAlias) nhop=$($NATSubnet.HostAddresses[0])"
+cmd.exe /c "netsh routing ip add persistentroute dest=$($VirtualNetwork.NetworkAddress) mask=$($VirtualNetwork.SubnetMask) name=$($NIC2IP.InterfaceAlias) nhop=$($HyperVSubnet.HostAddresses[0])"
