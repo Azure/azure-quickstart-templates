5807519b03671465180028a688a7b7dce8407e03
Ryan Williams <rwilliams@sol-tec.com>
2019-10-16 16:19:18 +0100

    Updated Template with HVHostSetup script

--- 301-nested-vms-in-virtual-network/InstallWindowsFeatures.ps1
+++
@@ -1,3 +0,0 @@
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module Subnet -Force
-Install-WindowsFeature -Name Hyper-V,DHCP,RemoteAccess,Routing -IncludeManagementTools -Restart
