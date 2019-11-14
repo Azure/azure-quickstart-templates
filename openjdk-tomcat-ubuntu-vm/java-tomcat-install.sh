57ead712302401679113a187c4b6386007968545
Lee Stott <leestott@microsoft.com>
2019-11-14 21:38:09 +0000

    Update Azuredeploy
    Add NSG rule for port 8080

--- openjdk-tomcat-ubuntu-vm/java-tomcat-install.sh
+++ openjdk-tomcat-ubuntu-vm/java-tomcat-install.sh
@@ -1,13 +1,19 @@
 # Install Java
+# Install Java
 sudo apt-get -y update
-sudo apt-get install -y $1
+sudo apt-get -y upgrade
+sudo apt-get install -y openjdk-13-jdk
 sudo apt-get -y update --fix-missing
-sudo apt-get install -y $1
+sudo apt-get install -y openjdk-13-jdk
 
 # Install tomcat
-sudo apt-get install -y  $2
+sudo apt-get install -y  tomcat9
+
+# Update and then close
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 if netstat -tulpen | grep 8080
 then
-	exit 0
+exit 0
 fi
