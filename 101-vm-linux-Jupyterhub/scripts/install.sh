21e87fd5fadc11c263439b7a9a7f37f928949de0
jose.martinez <jose.martinez@elastacloud.com>
2019-12-04 09:21:03 +0100

    Modified script

--- 101-vm-linux-Jupyterhub/scripts/install.sh
+++ 101-vm-linux-Jupyterhub/scripts/install.sh
@@ -1,3 +1,2 @@
 !/bin/bash
-
-curl https://raw.githubusercontent.com/jupyterhub/the-littlest-jupyterhub/master/bootstrap/bootstrap.py | sudo python3 - --admin $1 >> /var/log/TLJH_install.log 2>&1
+bootstrap.py | sudo python3 - --admin $1 >> /var/log/TLJH_install.log 2>&1
