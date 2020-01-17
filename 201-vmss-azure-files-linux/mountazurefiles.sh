f007d28b82eeaa26492da8e7e2387c47802d60cf
nishit-github <55799096+nishit-github@users.noreply.github.com>
2020-01-17 23:55:00 +0530

    Update mountazurefiles.sh

--- 201-vmss-azure-files-linux/mountazurefiles.sh
+++ 201-vmss-azure-files-linux/mountazurefiles.sh
@@ -8,12 +8,12 @@
 # $5 - username
 
 # For more details refer to https://azure.microsoft.com/en-us/documentation/articles/storage-how-to-use-files-linux/
-
+sudo -s
 # update package lists
-apt-get -y update
+yum -y update
 
 # install cifs-utils and mount file share
-apt-get install cifs-utils
+yum install cifs-utils
 mkdir $4
 mount -t cifs //$1.file.core.windows.net/$3 $4 -o vers=3.0,username=$1,password=$2,dir_mode=0755,file_mode=0664
 
