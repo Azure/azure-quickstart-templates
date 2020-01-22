2f36acdb7b34d1ef0a10d30f5ee2240b241d3eaf
Brian Moore (AZURE RESOURCE MANAGER) <bmoore@microsoft.com>
2020-01-22 17:25:14 -0600

    fix one error add warning to incomplete test

--- test/arm-ttk/testcases/deploymentTemplate/PublicIP-DNS-Should-Not-Contain-Concat.test.ps1
+++ test/arm-ttk/testcases/deploymentTemplate/PublicIP-DNS-Should-Not-Contain-Concat.test.ps1
@@ -1,10 +1,24 @@
-﻿param(
+﻿<#
+param(
 [Parameter(Mandatory=$true)]
 [PSObject]
 $MainTemplateObject
 )
+#>
 
-exit #skipping this test for now
+Write-Warning "Skipping DNS test, see code comments..."
+exit 
+
+<# 
+
+skipping this test for now, the test is too restrictive, it needs to account for:
+- unique seeds being used as part of the concat() e.g.  concat('base', uniqueString(resourceGroup(),id), copyIndex())
+- a param or var might contain that unique seed, the the template needs to be expanded
+- if we're testing a single file, $MainTemplateObject may be null
+- concat() regex needs to account for whitespace (check other tests for this)
+
+#>
+
 
 # Find all public IP addresses
 $publicIpResources = $MainTemplateObject |
