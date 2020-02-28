73cfbd9b4edef0ff5323816bc6a423abdd58423a
Lee Stott <leestott@microsoft.com>
2020-02-28 13:56:28 -0800

    Update deployment

--- 201-vm-linux-jupyterhub/scripts/install.sh
+++ 201-vm-linux-jupyterhub/scripts/install.sh
@@ -1,3 +1,2 @@
-#!/bin/bash -e	
-
-bootstrap.py | sudo python3 - --admin $1 >> /var/log/TLJH_install.log 2>&1 
+#!/bin/bash -e
+bootstrap.py | sudo python3 - --admin $1 >> /var/log/TLJH_install.log 2>&1
