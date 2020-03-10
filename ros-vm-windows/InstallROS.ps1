a1373d2817015f4d0e438b0bc953415882e5d196
Lior Lustgarten <45467030+lilustga@users.noreply.github.com>
2020-03-09 21:57:43 -0700

    Added ROS2/eloquent to InstallROS.ps1

--- ros-vm-windows/InstallROS.ps1
+++ ros-vm-windows/InstallROS.ps1
@@ -1,9 +1,11 @@
 # download and unzip the ROS to the desired location
 $outputPath = Join-Path $(Get-Location) -ChildPath "output"
-$downloadPath = Join-Path $outputPath -ChildPath "sfx-installer.zip"
+$ros1DownloadPath = Join-Path $outputPath -ChildPath "melodic-sfx-installer.zip"
+$ros2DownloadPath = Join-Path $outputPath -ChildPath "eloquent-sfx-installer.zip"
 $vcRedistPath = Join-Path $outputPath -ChildPath "vc_redist.x64.exe"
 
-$latestBuildUrl = 'https://dev.azure.com/ros-win/ros-win/_apis/build/builds?definitions=54&$top=1&resultFilter=succeeded&api-version=5.1'
+$ros1LatestBuildUrl = 'https://dev.azure.com/ros-win/ros-win/_apis/build/builds?definitions=54&$top=1&resultFilter=succeeded&api-version=5.1'
+$ros2LatestBuildUrl = 'https://dev.azure.com/ros-win/ros-win/_apis/build/builds?definitions=74&$top=1&resultFilter=succeeded&api-version=5.1'
 $vcRedistUrl = 'https://aka.ms/vs/16/release/vc_redist.x64.exe'
 
 New-Item -ItemType directory -Path $outputPath
@@ -15,13 +17,18 @@ do
 {
     try
     {
-        $response = Invoke-RestMethod $latestBuildUrl -Method Get -ContentType 'application/json'
-        $buildId = $response.value[0].id
+        $response1 = Invoke-RestMethod $ros1LatestBuildUrl -Method Get -ContentType 'application/json'
+        $buildId1 = $response1.value[0].id
+        
+        $response2 = Invoke-RestMethod $ros2LatestBuildUrl -Method Get -ContentType 'application/json'
+        $buildId2 = $response2.value[0].id
 
-        $artifactUrl = "https://ros-win.visualstudio.com/bed058dd-46da-4029-bb85-25eae7674d09/_apis/build/builds/$buildId/artifacts?artifactName=sfx-installer&api-version=5.2-preview.5&%24format=zip"
+        $artifactUrl1 = "https://ros-win.visualstudio.com/bed058dd-46da-4029-bb85-25eae7674d09/_apis/build/builds/$buildId1/artifacts?artifactName=sfx-installer&api-version=5.2-preview.5&%24format=zip"
+        $artifactUrl2 = "https://ros-win.visualstudio.com/bed058dd-46da-4029-bb85-25eae7674d09/_apis/build/builds/$buildId2/artifacts?artifactName=sfx-installer&api-version=5.2-preview.5&%24format=zip"
 
         $download = New-Object net.webclient
-        $download.Downloadfile($artifactUrl, $downloadPath)
+        $download.Downloadfile($artifactUrl1, $ros1DownloadPath)
+        $download.Downloadfile($artifactUrl2, $ros2DownloadPath)
         $download.DownloadFile($vcRedistUrl, $vcRedistPath)
         Write-Verbose "Downloaded install files successfully on attempt $retries" -verbose
         break
@@ -36,14 +43,20 @@ do
 }
 while ($retries -le $retryCount)
 
-Expand-Archive -path $downloadPath -destinationpath $outputPath
-$sfxPath = Join-Path $outputPath -ChildPath "sfx-installer/ros-melodic-desktop_full.exe"
+Expand-Archive -path $ros1DownloadPath -destinationpath $outputPath
+$sfxPath1 = Join-Path $outputPath -ChildPath "sfx-installer/ros-melodic-desktop_full.exe"
+
+Expand-Archive -path $ros2DownloadPath -destinationpath $outputPath
+$sfxPath2 = Join-Path $outputPath -ChildPath "sfx-installer/ros-eloquent-desktop.exe"
 
 Write-Verbose "Installing Visual C++ redistributable packages" -verbose
 Start-Process "$vcRedistPath" -ArgumentList "/q","/norestart","/log","vcredistinstall.log" -NoNewWindow -Wait
 
-Write-Verbose "Installing ROS binaries" -verbose
-Start-Process "$sfxPath" -ArgumentList "-oc:\","-y" -NoNewWindow -Wait
+Write-Verbose "Installing ROS1 binaries" -verbose
+Start-Process "$sfxPath1" -ArgumentList "-oc:\","-y" -NoNewWindow -Wait
+
+Write-Verbose "Installing ROS2 binaries" -verbose
+Start-Process "$sfxPath2" -ArgumentList "-oc:\","-y" -NoNewWindow -Wait
 
 # finally enable RemotePS
 Enable-PSRemoting -Force -SkipNetworkProfileCheck
