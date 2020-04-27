c2f45c05bdac0e1503c11d772d1bd5ef71b2b521
Tom Karbowski <tkarbowski@azul.com>
2020-04-27 11:31:14 -0500

    Upgrade to the Zulu April PSU

--- 101-Windows-Java-ZuluOpenJDK/zulu-install.ps1
+++ 101-Windows-Java-ZuluOpenJDK/zulu-install.ps1
@@ -4,17 +4,17 @@
              "Azul_Zulu_OpenJDK-11-JDK", "Azul_Zulu_OpenJDK-11-JRE",
              "Azul_Zulu_OpenJDK-13-JDK", "Azul_Zulu_OpenJDK-13-JRE")
 
- $ZULU_PACKS = @("zulu-7-azure-jdk_7.36.0.5-7.0.252-win_x64.msi",
-                 "zulu-7-azure-jre_7.36.0.5-7.0.252-win_x64.msi",
-                 "zulu-8-azure-jdk_8.44.0.11-8.0.242-win_x64.msi",
-                 "zulu-8-azure-jre_8.44.0.11-8.0.242-win_x64.msi",
-                 "zulu-11-azure-jdk_11.37.17-11.0.6-win_x64.msi",
-                 "zulu-11-azure-jre_11.37.17-11.0.6-win_x64.msi",
-                 "zulu-13-azure-jdk_13.29.9-13.0.2-win_x64.msi",
-                 "zulu-13-azure-jre_13.29.9-13.0.2-win_x64.msi")
+ $ZULU_PACKS = @("zulu-7-azure-jdk_7.38.0.11-7.0.262-win_x64.msi",
+                 "zulu-7-azure-jre_7.38.0.11-7.0.262-win_x64.msi",
+                 "zulu-8-azure-jdk_8.46.0.19-8.0.252-win_x64.msi",
+                 "zulu-8-azure-jre_8.46.0.19-8.0.252-win_x64.msi",
+                 "zulu-11-azure-jdk_11.39.15-11.0.7-win_x64.msi",
+                 "zulu-11-azure-jre_11.39.15-11.0.7-win_x64.msi",
+                 "zulu-13-azure-jdk_13.31.11-13.0.3-win_x64.msi",
+                 "zulu-13-azure-jre_13.31.11-13.0.3-win_x64.msi")
 
  $PACKAGE_DIRS = @("zulu-7", "zulu-7", "zulu-8", "zulu-8", "zulu-11", "zulu-11", "zulu-13", "zulu-13")
- $VERSIONS = @("7u252", "7u252", "8u242", "8u242", "11.0.6", "11.0.6", "13.0.2", "13.0.2")
+ $VERSIONS = @("7u262", "7u262", "8u252", "8u252", "11.0.7", "11.0.7", "13.0.3", "13.0.3")
  $INSTALL_DIRS = @("zulu-7", "zulu-7-jre", "zulu-8", "zulu-8-jre", "zulu-11", "zulu-11-jre", "zulu-13", "zulu-13-jre")
 
  for ($idx = 0; $idx -lt $PARAMS.length; $idx++) {
