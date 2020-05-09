9faf40f42dea78fa306ef1ff183ee2aaacd0e48a
Ihor Leontiev <leontuev@gmail.com>
2020-05-09 13:58:49 +0200

    Simplify choco Install script

--- obs-studio-stream-vm-chocolatey/scripts/ChocoInstall.ps1
+++ obs-studio-stream-vm-chocolatey/scripts/ChocoInstall.ps1
@@ -1,54 +1,25 @@
 #Script based on https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/visual-studio-dev-vm-chocolatey/scripts/SetupChocolatey.ps1
 param([Parameter(Mandatory=$true)][string]$chocoPackages)
 
-# Get username/password & machine name
-$userName = "artifactInstaller"
-[Reflection.Assembly]::LoadWithPartialName("System.Web") | Out-Null
-$password = $([System.Web.Security.Membership]::GeneratePassword(12,4))
-$cn = [ADSI]"WinNT://$env:ComputerName"
-
-# Create new user
-$user = $cn.Create("User", $userName)
-$user.SetPassword($password)
-$user.SetInfo()
-$user.description = "Choco artifact installer"
-$user.SetInfo()
-
-# Add user to the Administrators group
-$group = [ADSI]"WinNT://$env:ComputerName/Administrators,group"
-$group.add("WinNT://$env:ComputerName/$userName")
-
-# Create pwd and new $creds for remoting
-$secPassword = ConvertTo-SecureString $password -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($username)", $secPassword)
-
-# Ensure that current process can run scripts. 
-Enable-PSRemoting -Force -SkipNetworkProfileCheck
-
 #Changing ExecutionPolicy
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
+#Change securoty protocol
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+
 # Install Choco
 $sb = { iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) }
-Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential | Out-Null
+Invoke-Command -ScriptBlock $sb 
 
 $sb = { Set-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -name EnableLua -value 0 }
-Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential
+Invoke-Command -ScriptBlock $sb 
 
 #Install Chocolatey Packages
 $chocoPackages.Split(";") | ForEach {
     $command = "cinst " + $_ + " -y -force"
-    $command | Out-File $LogFile -Append
+    $command 
     $sb = [scriptblock]::Create("$command")
 
     # Use the current user profile
-    Invoke-Command -ScriptBlock $sb -ArgumentList $chocoPackages -ComputerName $env:COMPUTERNAME -Credential $credential | Out-Null
+    Invoke-Command -ScriptBlock $sb -ArgumentList $chocoPackages 
 }
-
-Disable-PSRemoting -Force
-
-# Delete the artifactInstaller user
-$cn.Delete("User", $userName)
-
-# Delete the artifactInstaller user profile
-gwmi win32_userprofile | where { $_.LocalPath -like "*$userName*" } | foreach { $_.Delete() }
