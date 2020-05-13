480878737042716c1d3a6870db91ca499f8b2628
Ihor Leontiev <leontuev@gmail.com>
2020-05-13 20:49:36 +0200

    Modify expan archive options

--- obs-studio-stream-vm-chocolatey/scripts/ChocoInstall.ps1
+++ obs-studio-stream-vm-chocolatey/scripts/ChocoInstall.ps1
@@ -24,7 +24,7 @@ $chocoPackages.Split(";") | ForEach {
 $url = "https://github.com/IhorLeontiev/azure-quickstart-templates/raw/obs-vm/obs-studio-stream-vm-chocolatey/scripts/packages/obs-ndi.zip"
 
 Invoke-WebRequest -Uri $url -OutFile ./obs-ndi.zip
-Expand-Archive -LiteralPath ./obs-ndi.zip DestinationPath ./
+Expand-Archive -Path ./obs-ndi.zip -DestinationPath ./
 
 choco pack
 choco install -y .\obs-ndi.4.9.0.nupkg
