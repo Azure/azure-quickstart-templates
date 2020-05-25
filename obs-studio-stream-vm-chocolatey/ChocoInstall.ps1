465496b4f3647b13e211c4da3651414157e85833
Ihor Leontiev <leontuev@gmail.com>
2020-05-25 19:46:44 +0200

    Added obs-ndi package as it was validated by chocolatey team

--- obs-studio-stream-vm-chocolatey/ChocoInstall.ps1
+++ obs-studio-stream-vm-chocolatey/ChocoInstall.ps1
@@ -1,10 +1,5 @@
 #Script based on https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/visual-studio-dev-vm-chocolatey/scripts/SetupChocolatey.ps1
-param(
-    [Parameter(Mandatory=$true)][string]$artifactsLocation,
-    [Parameter(Mandatory=$true)][string]$artifactsLocationSasToken,
-    [Parameter(Mandatory=$true)][string]$folderName,
-    [Parameter(Mandatory=$true)][string]$fileToInstall,
-    [Parameter(Mandatory=$true)][string]$chocoPackages)
+param([Parameter(Mandatory=$true)][string]$chocoPackages)
 
 Write-Host "File packages URL: $linktopackages"
 
@@ -27,14 +22,3 @@ $chocoPackages.Split(";") | ForEach {
 }
 
 Write-Host "Packages from choco.org were installes"
-#Isntall OBS-NDI package from sources
-
-$source = $artifactsLocation + "\$folderName\$fileToInstall" + $artifactsLocationSasToken
-$dest = "C:\WindowsAzure\$folderName"
-New-Item -Path $dest -ItemType directory
-Invoke-WebRequest $source -OutFile "$dest\$fileToInstall"
-
-Expand-Archive -Path "$dest\$fileToInstall" -DestinationPath ./
-
-choco pack
-choco install -y .\obs-ndi.4.9.0.nupkg
