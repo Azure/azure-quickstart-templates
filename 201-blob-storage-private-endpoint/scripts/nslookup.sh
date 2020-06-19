fea6672d7ac4c83190cd1b96bc9cf6048ab86115
Paolo Salvatori <leprino@hotmail.com>
2020-06-19 14:33:33 +0200

    fixed ARM template

--- 201-blob-storage-private-endpoint/scripts/nslookup.sh
+++ 201-blob-storage-private-endpoint/scripts/nslookup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Variables
-storageAccountName=$1
+blobServicePrimaryEndpoint=$1
 
 # Parameter validation
-if [[ -z storageAccountName ]]; then
-    echo "storageAccountName parameter cannot be null or empty"
+if [[ -z blobServicePrimaryEndpoint ]]; then
+    echo "blobServicePrimaryEndpoint parameter cannot be null or empty"
     exit 1
 fi
 
@@ -18,4 +18,4 @@ sudo apt-get upgrade -y
 # Run nslookup to verify that the <storage-account>.blob.core.windows.net public hostname of the storage account 
 # is properly mapped to <storage-account>.privatelink.blob.core.windows.net by the private DNS zone
 # and the latter mapped to the private address by the A record
-nslookup "$storageAccountName.blob.core.windows.net"
+nslookup $blobServicePrimaryEndpoint
