cbdcc194de279b89044af56ce9f4adfa56390abe
parthakom2 <parthakom@in.ibm.com>
2020-07-06 09:43:00 +0530

    remove commented script

--- ibm-cloud-pak-for-data/scripts/bastionPrep.sh
+++ ibm-cloud-pak-for-data/scripts/bastionPrep.sh
@@ -12,56 +12,6 @@ echo $(date) " - Generating Private keys for use by Ansible for OpenShift Instal
 runuser -l $SUDOUSER -c "echo \"$PRIVATEKEY\" > /home/$SUDOUSER/.ssh/id_rsa"
 runuser -l $SUDOUSER -c "chmod 600 /home/$SUDOUSER/.ssh/id_rsa*"
 
-# Remove RHUI
-
-#rm -f /etc/yum.repos.d/rh-cloud.repo
-sleep 20
-
-# Register Host with Cloud Access Subscription
-echo $(date) " - Register host with Cloud Access Subscription"
-
-#subscription-manager register --force --username="$USERNAME_ORG" --password="$PASSWORD_ACT_KEY" || subscription-manager register --force --activationkey="$PASSWORD_ACT_KEY" --org="$USERNAME_ORG"
-#RETCODE=$?
-
-#if [ $RETCODE -eq 0 ]
-#then
-#    echo "Subscribed successfully"
-#elif [ $RETCODE -eq 64 ]
-#then
-#    echo "This system is already registered."
-#else
-#    echo "Incorrect Username / Password or Organization ID / Activation Key specified"
-#    exit 3
-#fi
-
-#subscription-manager attach --pool=$POOL_ID > attach.log
-# if [ $? -eq 0 ]
-# then
-#     echo "Pool attached successfully"
-# else
-#     grep attached attach.log
-#     if [ $? -eq 0 ]
-#     then
-#         echo "Pool $POOL_ID was already attached and was not attached again."
-#     else
-#         echo "Incorrect Pool ID or no entitlements available"
-#         exit 4
-#     fi
-# fi
-
-# # Disable all repositories and enable only the required ones
-# echo $(date) " - Disabling all repositories and enabling only the required repos"
-
-# subscription-manager repos --disable="*"
-
-#subscription-manager repos \
-#    --enable="rhel-7-server-rpms" \
-#    --enable="rhel-7-server-extras-rpms" \
-#    --enable="rhel-7-server-ose-3.11-rpms" \
-#    --enable="rhel-7-server-ansible-2.6-rpms" \
-#    --enable="rhel-7-fast-datapath-rpms" \
-#    --enable="rh-gluster-3-client-for-rhel-7-server-rpms" \
-#    --enable="rhel-7-server-optional-rpms"
 
 # Update system to latest packages
 echo $(date) " - Update system to latest packages"
