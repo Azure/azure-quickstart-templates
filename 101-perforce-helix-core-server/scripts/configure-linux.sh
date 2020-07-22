e4e2998c43d0226d878c535e9009f7eccb788e7f
Robert Cowham <rcowham@perforce.com>
2020-07-22 12:39:06 +0100

    Use latest CentOS - 7.8 at moment Ensure p4broker/p4p services disabled

--- 101-perforce-helix-core-server/scripts/configure-linux.sh
+++ 101-perforce-helix-core-server/scripts/configure-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script is only tested on CentOS 7.5, RHEL 7.x (7.6+) and Ubuntu 18.04 LTS.
+# This script is only tested on CentOS 7.x (7.8+), RHEL 7.x (7.8+) and Ubuntu 18.04 LTS.
 MOUNTPOINT="/hxdata"
 
 # An set of disks to ignore from partitioning and formatting
@@ -164,7 +164,11 @@ configure_helix() {
     chmod +x reset_sdp.sh
     ./reset_sdp.sh -fast -no_sd > reset_sdp.log 2>&1
 
+    # Make sure p4d is enabled but broker and p4p are not
     systemctl enable p4d_1
+    systemctl disable p4broker_1
+    systemctl disable p4p_1
+
     # Change default port and then generate SSL cert
     sudo -u perforce perl -pi -e "s/P4PORTNUM=1999/P4PORTNUM=$P4PORT/" /p4/common/config/p4_1.vars 
     sudo -u perforce bash -c "source /p4/common/bin/p4_vars 1 && /p4/1/bin/p4d_1 -Gc"
@@ -181,6 +185,7 @@ source /p4/common/bin/p4_vars 1
 p4 trust -y
 p4 -p ssl:`hostname`:$P4PORTNUM trust -y
 p4 user -o | p4 user -i
+p4 protect -o | p4 protect -i
 
 PASSWORD=`cat /p4/common/config/.p4passwd.p4_1.admin`
 echo -e "$PASSWORD\n$PASSWORD" | p4 passwd
