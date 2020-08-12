87e3cb64e2ea87985cce6806773a32e7695d1518
Gary W. Black <gary.black@sas.com>
2020-08-12 12:37:48 -0400

    (AZUREVAVS-10) Fix issue with services not starting
    This commit fixes an issue where some services are not starting on a successful
    deployment.
    
    Also, the loadbalancer_id_or_empty field is set to "blank" to avoid timeouts when
    the VMs are initialized.

--- sas-9.4m7-VA/scripts/run_install_as_user.sh
+++ sas-9.4m7-VA/scripts/run_install_as_user.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-sudo -u $1 bash <<EOF
-/sasshare/depot/setup.sh -quiet -loglevel 2 -responsefile $2
-exit
-EOF
+su - $1 -c "/sasshare/depot/setup.sh -quiet -loglevel 2 -responsefile $2"
