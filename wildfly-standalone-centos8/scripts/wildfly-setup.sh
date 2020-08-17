fdec7a0d0e7712b0adb30190b4288fe8492fe850
SURAJ2093 <suraj.rpai2093@gmail.com>
2020-08-18 00:20:54 +0530

    Changes as per the comments in PR

--- wildfly-standalone-centos8/scripts/wildfly-setup.sh
+++ wildfly-standalone-centos8/scripts/wildfly-setup.sh
@@ -33,11 +33,11 @@ echo "systemctl restart sshd" | adddate >> wildfly.install.log
 systemctl restart sshd | adddate >> wildfly.install.log 2>&1
 
 echo "Sample app deploy..." | adddate >> wildfly.install.log
-echo "git clone https://github.com/Suraj2093/dukes.git" | adddate >> wildfly.install.log
-git clone https://github.com/Suraj2093/dukes.git >> wildfly.install.log 2>&1
+echo "git clone https://github.com/Azure/azure-quickstart-templates.git" | adddate >> wildfly.install.log
+git clone https://github.com/Azure/azure-quickstart-templates.git >> wildfly.install.log 2>&1
 flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! Git clone Failed" | adddate >> wildfly.install.log; exit $flag;  fi
-echo "/bin/cp -rf ./dukes/target/JBoss-EAP_on_Azure.war ./wildfly-$WILDFLY_RELEASE.Final/standalone/deployments/" | adddate >> wildfly.install.log
-/bin/cp -rf ./dukes/target/JBoss-EAP_on_Azure.war ./wildfly-$WILDFLY_RELEASE.Final/standalone/deployments/ | adddate >> wildfly.install.log 2>&1
+echo "/bin/cp -rf ./azure-quickstart-templates/wildfly-standalone-centos8/scripts/JBoss-EAP_on_Azure.war ./wildfly-$WILDFLY_RELEASE.Final/standalone/deployments/" | adddate >> wildfly.install.log
+/bin/cp -rf ./azure-quickstart-templates/wildfly-standalone-centos8/scripts/JBoss-EAP_on_Azure.war ./wildfly-$WILDFLY_RELEASE.Final/standalone/deployments/ | adddate >> wildfly.install.log 2>&1
 
 echo "Configuring WILDFLY managment user..." | adddate >> wildfly.install.log
 echo "./wildfly-$WILDFLY_RELEASE.Final/bin/add-user.sh -u WILDFLY_USER -p WILDFLY_PASSWORD -g 'guest,mgmtgroup'" | adddate >> wildfly.install.log
