954dd231d4d326a0fb00c40c0ac8f8a1fb3d8f09
SURAJ2093 <suraj.rpai2093@gmail.com>
2020-08-19 22:06:04 +0530

    Minor change in script

--- wildfly-standalone-centos8/scripts/wildfly-setup.sh
+++ wildfly-standalone-centos8/scripts/wildfly-setup.sh
@@ -35,7 +35,7 @@ systemctl restart sshd | adddate >> wildfly.install.log 2>&1
 echo "Sample app deploy..." | adddate >> wildfly.install.log
 echo "wget https://github.com/Azure/azure-quickstart-templates/raw/master/wildfly-standalone-centos8/scripts/JBoss-EAP_on_Azure.war" | adddate >> wildfly.install.log
 wget https://github.com/Azure/azure-quickstart-templates/raw/master/wildfly-standalone-centos8/scripts/JBoss-EAP_on_Azure.war >> wildfly.install.log 2>&1
-flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! Git clone Failed" | adddate >> wildfly.install.log; exit $flag;  fi
+flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! Sample Application Download Failed" | adddate >> wildfly.install.log; exit $flag;  fi
 echo "/bin/cp -rf ./JBoss-EAP_on_Azure.war ./wildfly-$WILDFLY_RELEASE.Final/standalone/deployments/" | adddate >> wildfly.install.log
 /bin/cp -rf ./JBoss-EAP_on_Azure.war ./wildfly-$WILDFLY_RELEASE.Final/standalone/deployments/ | adddate >> wildfly.install.log 2>&1
 
