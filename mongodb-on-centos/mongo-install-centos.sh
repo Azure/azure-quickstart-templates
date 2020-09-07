61ecab0bfd9b122143f5b7dfec4e48f1e7eed77c
Mickaël Mottet <mickael.mottet@hotmail.com>
2020-09-07 10:50:52 +0200

    Fix deployment

--- mongodb-on-centos/mongo-install-centos.sh
+++ mongodb-on-centos/mongo-install-centos.sh
@@ -6,12 +6,12 @@
 
 #sudo chmod 777 /etc/yum.repos.d/mongodb.repo
 touch /etc/yum.repos.d/mongodb.repo
-echo "[mongodb-org-3.2]" >> /etc/yum.repos.d/mongodb.repo
+echo "[mongodb-org-4.4]" >> /etc/yum.repos.d/mongodb.repo
 echo "name=MongoDB Repository" >> /etc/yum.repos.d/mongodb.repo
-echo "baseurl=http://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/3.2/x86_64/" >> /etc/yum.repos.d/mongodb.repo
+echo "baseurl=http://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/4.4/x86_64/" >> /etc/yum.repos.d/mongodb.repo
 echo "gpgcheck=1" >> /etc/yum.repos.d/mongodb.repo
 echo "enabled=1" >> /etc/yum.repos.d/mongodb.repo
-echo "gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc" >> /etc/yum.repos.d/mongodb.repo
+echo "gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc" >> /etc/yum.repos.d/mongodb.repo
 
 # Install updates
 yum -y update
