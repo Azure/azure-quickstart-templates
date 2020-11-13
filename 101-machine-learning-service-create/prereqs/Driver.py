b1d0014d9146ccafef2b927b629ee76610ee4682
Keerthi Adusumilli <keadusum@microsoft.com>
2020-11-13 11:57:15 -0800

    also upload driver.py file

---
+++ 101-machine-learning-service-create/prereqs/Driver.py
@@ -0,0 +1,27 @@
+import os
+import pickle
+import json
+import numpy
+import joblib
+from sklearn.linear_model import Ridge
+from azureml.core.model import Model
+
+def init():
+    global model
+    # note here "sklearn_regression_model.pkl" is the name of the model registered under
+    # this is a different behavior than before when the code is run locally, even though the code is the same.
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'sklearn_regression_model.pkl')
+    # deserialize the model file back into a sklearn model
+    model = joblib.load(model_path)
+
+# note you can pass in multiple rows for scoring
+def run(raw_data):
+    try:
+        data = json.loads(raw_data)['data']
+        data = numpy.array(data)
+        result = model.predict(data)
+        # you can return any data type as long as it is JSON-serializable
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+        return error
