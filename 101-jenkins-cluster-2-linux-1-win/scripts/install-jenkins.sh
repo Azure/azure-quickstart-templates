6d24e6fdf7655b77dd48d7c1749c34145379be59
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2020-11-27 12:58:01 +0530

    scripts updated

--- 101-jenkins-cluster-2-linux-1-win/scripts/install-jenkins.sh
+++ 101-jenkins-cluster-2-linux-1-win/scripts/install-jenkins.sh
@@ -1,9 +1,16 @@
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
-sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
 apt update -y
-apt install openjdk-8-jdk git maven unzip -y
+apt install openjdk-8-jdk git maven docker.io unzip -y
 apt install jenkins -y
 systemctl enable jenkins
+systemctl enable docker
+usermod -aG docker jenkins
+## Create the TOOLS directory 
+## Create TOOLS directory for jenkins
+mkdir /opt/tools/
+cd /opt/tools
 ## Install solar scanner
 wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.2.0.1873-linux.zip
 unzip sonar-scanner-cli-4.2.0.1873-linux.zip
@@ -18,4 +25,10 @@ ulimit -u 4096
 # Download SonarQube 7.1 (Compatible with JDK 8)
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.1.zip
 unzip sonarqube-7.1.zip
-
+groupadd sonar
+sudo useradd -c "user to run SonarQube" -d /opt/tools/sonarqube-7.1 -g sonar sonar 
+cd sonarqube-7.1/bin/linux-x86-64
+# set RUN_AS_USER=sonar
+sed -i 's/#RUN_AS_USER=/RUN_AS_USER=sonar/g' sonar.sh
+## Change OWNER of tools to "jenkins"
+chown -R jenkins:jenkins /opt/tools/
