d6becf7a36b9d70bd2da35296b25627ab886eaf9
Julien CLEMENT <julien.clement@epita.fr>
2020-12-17 09:58:52 +0100

    Script retrieves config and start server
    Signed-off-by: Julien CLEMENT <julien.clement@epita.fr>

--- alsid-syslog-proxy/setup.sh
+++ alsid-syslog-proxy/setup.sh
@@ -1,1 +1,5 @@
-touch oui
+wget https://raw.githubusercontent.com/AlsidOfficial/azure-quickstart-templates/alsid-ARM/alsid-syslog-proxy/rsyslog.conf
+
+mv rsyslog.conf /etc/
+
+systemctl restart rsyslog
