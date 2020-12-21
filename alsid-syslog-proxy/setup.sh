4f22cbcaf5e3af624e710803eed90bbff43d9811
Julien CLEMENT <julien.clement@epita.fr>
2020-12-21 12:01:56 +0100

    Add debug log
    Signed-off-by: Julien CLEMENT <julien.clement@epita.fr>

--- alsid-syslog-proxy/setup.sh
+++ alsid-syslog-proxy/setup.sh
@@ -2,6 +2,8 @@ wget https://raw.githubusercontent.com/AlsidOfficial/azure-quickstart-templates/
 
 mv rsyslog.conf /etc/
 
-service rsyslog restart
+echo $WORKSPACE_ID $PRIMARYKEY
 
 wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sh onboard_agent.sh -w $WORKSPACE_ID -s $PRIMARYKEY -d opinsights.azure.com
+
+service rsyslog restart
