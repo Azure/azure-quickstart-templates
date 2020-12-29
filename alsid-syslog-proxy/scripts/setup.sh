d3484225748e7dc59dfaff400ad443e407d629d8
Julien CLEMENT <julien.clement@epita.fr>
2020-12-29 15:45:04 +0100

    Add certificate generation in setup
    Signed-off-by: Julien CLEMENT <julien.clement@epita.fr>

--- alsid-syslog-proxy/scripts/setup.sh
+++ alsid-syslog-proxy/scripts/setup.sh
@@ -1,6 +1,7 @@
 WORKSPACE_ID=$1
 PRIMARYKEY=$2
 ARTIFACTS_LOCATION=$3
+DNS_NAME=$4
 
 wget $ARTIFACTS_LOCATION/rsyslog.conf
 
@@ -9,5 +10,16 @@ mv rsyslog.conf /etc/
 echo $WORKSPACE_ID $PRIMARYKEY
 
 wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sh onboard_agent.sh -w $WORKSPACE_ID -s $PRIMARYKEY -d opinsights.azure.com
+
+apt install snapd
+
+snap install core
+snap refresh core
+
+snap install --classic certbot
+
+ln -s /snap/bin/certbot /usr/bin/certbot
+
+sudo certbot certonly --standalone --register-unsafely-without-email --domain $DNS_NAME
 
 service rsyslog restart
