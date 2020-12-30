84a64a0f7dbc1fa52678c8032f96bcce60009e73
Julien CLEMENT <julien.clement@epita.fr>
2020-12-30 15:21:34 +0100

    Move certificates in rsyslog.d and add comment in setup.sh
    Signed-off-by: Julien CLEMENT <julien.clement@epita.fr>

--- alsid-syslog-proxy/scripts/setup.sh
+++ alsid-syslog-proxy/scripts/setup.sh
@@ -3,23 +3,37 @@ PRIMARYKEY=$2
 ARTIFACTS_LOCATION=$3
 DNS_NAME=$4
 
+CERTS_PATH=$/etc/letsencrypt/live/$DNS_NAME/
+
+
+# Install required packages
+apt install rsyslog-gnutls # For TLS support on rsyslog
+
+apt install snapd # Recommended to install certbot
+snap install core
+snap refresh core
+snap install --classic certbot # To generate TLS certificates
+
+
+# Get rsyslog configuration
 wget $ARTIFACTS_LOCATION/rsyslog.conf
 
 mv rsyslog.conf /etc/
 
-echo $WORKSPACE_ID $PRIMARYKEY
 
+# Install Sentinel agent
 wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sh onboard_agent.sh -w $WORKSPACE_ID -s $PRIMARYKEY -d opinsights.azure.com
 
-apt install snapd
 
-snap install core
-snap refresh core
-
-snap install --classic certbot
-
+# Generate TLS certificates
 ln -s /snap/bin/certbot /usr/bin/certbot
 
-sudo certbot certonly --standalone --register-unsafely-without-email -n --agree-tos --domain $DNS_NAME
+certbot certonly --standalone --register-unsafely-without-email -n --agree-tos --domain $DNS_NAME
 
+
+# Move certificates in rsyslog config
+cp $CERTS_PATH/cert.pem $CERTS_PATH/chain.pem $CERTS_PATH/privkey.pem /etc/rsyslog.d/
+
+
+# Restart rsyslog service
 service rsyslog restart
