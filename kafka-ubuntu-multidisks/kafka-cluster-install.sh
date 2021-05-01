4fad468cf5a7e547e993cd6c378859f809bc2baa
sameera-spektra <sameira@gmail.com>
2021-05-02 00:21:06 +0530

    chck jave version

--- kafka-ubuntu-multidisks/kafka-cluster-install.sh
+++ kafka-ubuntu-multidisks/kafka-cluster-install.sh
@@ -129,6 +129,7 @@ install_java()
     echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
     echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
     apt-get -y install oracle-java7-installer
+	java -version
 }
 
 # Expand a list of successive IP range defined by a starting address prefix (e.g. 10.0.0.1) and the number of machines in the range
