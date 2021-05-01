9ecdb7e33506fcb0a88aa3dcd00cc50d63fdd7ee
sameera-spektra <sameira@gmail.com>
2021-05-01 22:55:46 +0530

    corrected zookeeper download path & updated version references

--- kafka-ubuntu-multidisks/kafka-cluster-install.sh
+++ kafka-ubuntu-multidisks/kafka-cluster-install.sh
@@ -138,7 +138,7 @@ expand_ip_range_for_server_properties() {
     IFS='-' read -a HOST_IPS <<< "$1"
     for (( n=0 ; n<("${HOST_IPS[1]}"+0) ; n++))
     do
-        echo "server.$(expr ${n} + 1)=${HOST_IPS[0]}${n}:2888:3888" >> zookeeper-3.4.6/conf/zoo.cfg
+        echo "server.$(expr ${n} + 1)=${HOST_IPS[0]}${n}:2888:3888" >> zookeeper/conf/zoo.cfg
     done
 }
 
@@ -163,22 +163,22 @@ install_zookeeper()
 {
 	mkdir -p /var/lib/zookeeper
 	cd /var/lib/zookeeper
-	wget "http://apache.cs.utah.edu/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz"
-	tar -xvf "zookeeper-3.4.6.tar.gz"
+	wget "http://mirrors.ukfast.co.uk/sites/ftp.apache.org/zookeeper/zookeeper-3.5.9/apache-zookeeper-3.5.9.tar.gz"
+	tar -xvf "zookeeper-3.5.9.tar.gz" "zookeeper"
 
-	touch zookeeper-3.4.6/conf/zoo.cfg
+	touch zookeeper/conf/zoo.cfg
 
-	echo "tickTime=2000" >> zookeeper-3.4.6/conf/zoo.cfg
-	echo "dataDir=/var/lib/zookeeper" >> zookeeper-3.4.6/conf/zoo.cfg
-	echo "clientPort=2181" >> zookeeper-3.4.6/conf/zoo.cfg
-	echo "initLimit=5" >> zookeeper-3.4.6/conf/zoo.cfg
-	echo "syncLimit=2" >> zookeeper-3.4.6/conf/zoo.cfg
+	echo "tickTime=2000" >> zookeeper/conf/zoo.cfg
+	echo "dataDir=/var/lib/zookeeper" >> zookeeper/conf/zoo.cfg
+	echo "clientPort=2181" >> zookeeper/conf/zoo.cfg
+	echo "initLimit=5" >> zookeeper/conf/zoo.cfg
+	echo "syncLimit=2" >> zookeeper/conf/zoo.cfg
 	# OLD Test echo "server.1=${ZOOKEEPER_IP_PREFIX}:2888:3888" >> zookeeper-3.4.6/conf/zoo.cfg
 	$(expand_ip_range_for_server_properties "${ZOOKEEPER_IP_PREFIX}-${INSTANCE_COUNT}")
 
 	echo $(($1+1)) >> /var/lib/zookeeper/myid
 
-	zookeeper-3.4.6/bin/zkServer.sh start
+	zookeeper-3.5.9/bin/zkServer.sh start
 }
 
 # Setup datadisks
