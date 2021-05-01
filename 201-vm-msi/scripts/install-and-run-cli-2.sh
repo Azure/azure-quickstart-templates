4ee69f7f371d70b7520391c2c7386be494547a61
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-01 08:01:45 +0300

    trying to use different current directory refrerence notation

--- 201-vm-msi/scripts/install-and-run-cli-2.sh
+++ 201-vm-msi/scripts/install-and-run-cli-2.sh
@@ -46,7 +46,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update -y
 sudo apt install -y docker-ce
 sudo systemctl start docker
-sudo docker run -v `pwd`:/scripts --network='host' \
+sudo docker run -v $(pwd):/scripts --network='host' \
 -e STORAGE_ACCOUNT=${storage_account} \
 -e CONTAINER_NAME=${container_name} \
 -e RESOURCE_GROUP=${resource_group} \
