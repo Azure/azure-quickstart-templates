876ffa1e6d194f8f698901add6829a821c840d9a
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-01 23:24:36 +0300

    double quotes

--- 201-vm-msi/scripts/install-and-run-cli-2.sh
+++ 201-vm-msi/scripts/install-and-run-cli-2.sh
@@ -41,4 +41,4 @@ done
 
 sudo apt-get update
 sudo apt-get install -y docker.io
-sudo docker run -v `pwd`:/scripts --network=host -e STORAGE_ACCOUNT=${storage_account} -e CONTAINER_NAME=${container_name} -e RESOURCE_GROUP=${resource_group} -e PORT=${PORT} ${docker_image} /bin/sh '/scripts/${script_file}'
+sudo docker run -v `pwd`:/scripts --network=host -e STORAGE_ACCOUNT=${storage_account} -e CONTAINER_NAME=${container_name} -e RESOURCE_GROUP=${resource_group} -e PORT=${PORT} ${docker_image} /bin/sh "/scripts/${script_file}"
