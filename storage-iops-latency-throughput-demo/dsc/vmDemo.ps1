43c9a906c9221d298894916cdda4de1688334c54
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-02 07:59:52 +0300

    trying to actually install chocolatey

--- storage-iops-latency-throughput-demo/dsc/vmDemo.ps1
+++ storage-iops-latency-throughput-demo/dsc/vmDemo.ps1
@@ -5,8 +5,6 @@ Param(
 	[string]$NodeName = 'localhost'
 )
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
 Import-DscResource -Module cChoco
 Import-DscResource -Module xStorage,xPSDesiredStateConfiguration
 
