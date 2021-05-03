dbc47d147c864b667b5dbb57b8614192d0a47231
Javid Salmanov <javid.salmanov@outlook.com>
2021-05-03 10:01:09 +0200

    Update VM size and script

--- 201-dependency-between-scripts-using-extensions/mongo-configure-ubuntu.sh
+++ 201-dependency-between-scripts-using-extensions/mongo-configure-ubuntu.sh
@@ -1,3 +1,4 @@
 # Configure mongodb.list file with the correct location
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
--- 201-dependency-between-scripts-using-extensions/mongo-install-ubuntu.sh
+++ 201-dependency-between-scripts-using-extensions/mongo-install-ubuntu.sh
@@ -2,4 +2,4 @@
 sudo apt-get -y update
 
 #Install Mongo DB
-sudo apt-get install -y mongodb-10gen
+sudo apt-get install -y mongodb-org
