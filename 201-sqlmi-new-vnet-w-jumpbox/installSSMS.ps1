52018e7323c52e6d39070369dbbd4311b56ff5e1
Srđan Božović <srdjan.bozovic@live.com>
2021-05-04 16:05:33 +0200

    Fix issue in installSSMS.ps1 (#9497)
    * Update installSSMS.ps1
    
    * Update metadata.json
    
    * Update README.md
    
    * Update azuredeploy.json

--- 201-sqlmi-new-vnet-w-jumpbox/installSSMS.ps1
+++ 201-sqlmi-new-vnet-w-jumpbox/installSSMS.ps1
@@ -1,2 +1,3 @@
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+iex ((new-object net.webclient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install sql-server-management-studio -y
