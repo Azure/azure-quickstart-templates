9c5cc993dfbc08843ddf93eec7a6c2224d9468b0
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-06 09:53:14 +0300

    trying to fix zfs issue

--- ubuntu-netdisk-setup/script/install.sh
+++ ubuntu-netdisk-setup/script/install.sh
@@ -175,7 +175,7 @@ export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/
 sudo apt-get update
 sudo apt-get install -y python2.7 python-pil sudo python-pip python-setuptools python-imaging python-mysqldb python-ldap python-urllib3 \
 openjdk-8-jre memcached python-memcache pwgen curl openssl poppler-utils libpython2.7 libreoffice \
-libreoffice-script-provider-python ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy nginx python-requests zfs-fuse
+libreoffice-script-provider-python ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy nginx python-requests zfs-fuse &> /dev/null
 
 # -------------------------------------------
 # Create seafile-data with the help of ZFS
