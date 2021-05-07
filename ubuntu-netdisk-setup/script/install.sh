a61e5ec2b155bf7e2d0f4e5faabe87645ac90acd
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-07 04:19:57 +0300

    trying to fix dpkg

--- ubuntu-netdisk-setup/script/install.sh
+++ ubuntu-netdisk-setup/script/install.sh
@@ -159,6 +159,13 @@ then
   echo "  Aborting because directory /opt/seafile/ already exist" ; exit 1
 fi
 
+export DEBIAN_FRONTEND=noninteractive
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
+
 # -------------------------------------------
 # Additional requirements
 # -------------------------------------------
