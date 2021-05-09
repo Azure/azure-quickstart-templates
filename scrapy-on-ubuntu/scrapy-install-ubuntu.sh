2234b7e9a796f39d211e6f1ea2fce45186577a8f
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-10 01:58:32 +0300

    trying to install using pip

--- scrapy-on-ubuntu/scrapy-install-ubuntu.sh
+++ scrapy-on-ubuntu/scrapy-install-ubuntu.sh
@@ -1,4 +1,5 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 627220E7
 echo 'deb http://archive.scrapy.org/ubuntu scrapy main' | sudo tee /etc/apt/sources.list.d/scrapy.list
-sudo apt-get update && sudo apt-get -y install scrapy-0.24
+sudo apt-get install -y build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev python-pip libjpeg-dev
+pip install scrapy
 scrapy runspider $1
