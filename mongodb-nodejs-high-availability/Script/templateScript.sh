4aa70ca2c5eadcb0c6002fd8f95fabde398b454e
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-10 06:29:07 +0300

    adding the script from the working mongo template

--- mongodb-nodejs-high-availability/Script/templateScript.sh
+++ mongodb-nodejs-high-availability/Script/templateScript.sh
@@ -1,8 +1,10 @@
-
-cd /
 sudo mv /var/lib/waagent/Microsoft.Azure.Extensions.CustomScript-2.0.0/download/0/app.js /opt/app.js
 
+# Install updates
 sudo apt-get -y update
+
+# Modified tcp keepalive according to https://docs.mongodb.org/ecosystem/platforms/windows-azure/
+sudo bash -c "sudo echo net.ipv4.tcp_keepalive_time = 120 >> /etc/sysctl.conf"
 
 sudo apt-get -y install nodejs
 sudo apt-get -y install npm
