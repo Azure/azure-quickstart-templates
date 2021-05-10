47a9e044cc0c3791fe80cd227c58da62867e850c
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-10 05:56:16 +0300

    fixing scrapy install

--- scrapy-on-ubuntu/scrapy-install-ubuntu.sh
+++ scrapy-on-ubuntu/scrapy-install-ubuntu.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 sudo apt-get -y update
-sudo apt-get -y install -y build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev python-pip libjpeg-dev
-pip install Scrapy
-scrapy runspider $1
+sudo apt-get install -y python3 python-dev python3-dev build-essential libssl-dev libffi-dev libxml2-dev libxslt-dev python-pip
+
+sudo pip3 install scrapy
