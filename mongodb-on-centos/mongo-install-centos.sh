78785df2c67790856b9d499525deaa083093ee02
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-15 23:55:08 +0300

    trying new mongo version

--- mongodb-on-centos/mongo-install-centos.sh
+++ mongodb-on-centos/mongo-install-centos.sh
@@ -1,25 +1,14 @@
+touch /etc/yum.repos.d/mongodb-org-4.4.repo
 
-#Modify Sudoers file to not require tty for shell script execution on CentOS
-# sudo sed -i '/Defaults[[:space:]]\+requiretty/s/^/#/' /etc/sudoers
+echo "[mongodb-org-4.4]" >> /etc/yum.repos.d/mongodb-org-4.4.repo
+echo "name=MongoDB Repository" >> /etc/yum.repos.d/mongodb-org-4.4.repo
+echo "baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/4.4/x86_64/" >> /etc/yum.repos.d/mongodb-org-4.4.repo
+echo "gpgcheck=1" >> /etc/yum.repos.d/mongodb-org-4.4.repo
+echo "enabled=1" >> /etc/yum.repos.d/mongodb-org-4.4.repo
+echo "gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc" >> /etc/yum.repos.d/mongodb-org-4.4.repo
 
-# Enable write access to the mongodb.repo and configure it for installation
-
-#sudo chmod 777 /etc/yum.repos.d/mongodb.repo
-touch /etc/yum.repos.d/mongodb.repo
-echo "[mongodb-org-3.2]" >> /etc/yum.repos.d/mongodb.repo
-echo "name=MongoDB Repository" >> /etc/yum.repos.d/mongodb.repo
-echo "baseurl=http://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/3.2/x86_64/" >> /etc/yum.repos.d/mongodb.repo
-echo "gpgcheck=1" >> /etc/yum.repos.d/mongodb.repo
-echo "enabled=1" >> /etc/yum.repos.d/mongodb.repo
-echo "gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc" >> /etc/yum.repos.d/mongodb.repo
-
-# Install updates
 yum -y update
 
-# Install SELinux management tools and add rule for default port for mongod service
-yum install -y policycoreutils-python
-semanage port -a -t mongod_port_t -p tcp 27017
-
-# Install Mongo DB and start service
 yum install -y mongodb-org
+
 service mongod start
