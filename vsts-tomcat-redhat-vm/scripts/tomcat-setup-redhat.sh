6f0db32363ae97f5ac210281bf40cd7892dbe0b5
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-17 01:10:37 +0300

    removing sudo as rhel has an issue with tty

--- vsts-tomcat-redhat-vm/scripts/tomcat-setup-redhat.sh
+++ vsts-tomcat-redhat-vm/scripts/tomcat-setup-redhat.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 # Install Apache2, Tomcat7 and then build mod-jk package
-sudo yum install -y httpd
-sudo yum install -y tomcat 
-sudo yum install -y tomcat-webapps tomcat-admin-webapps 
-sudo yum install -y gcc 
-sudo yum install -y gcc-c++ 
-sudo yum install -y httpd-devel 
-sudo yum install -y make 
-sudo wget http://archive.apache.org/dist/tomcat/tomcat-connectors/jk/tomcat-connectors-1.2.41-src.tar.gz 
-sudo tar xvfz tomcat-connectors-1.2.41-src.tar.gz 
-sudo $PWD/tomcat-connectors-1.2.41-src/native/configure --with-apxs=/usr/bin/apxs 
-sudo make -C $PWD/tomcat-connectors-1.2.41-src/native 
-sudo make install -C $PWD/tomcat-connectors-1.2.41-src/native 
-sudo groupadd --system tomcat
-sudo useradd -d /usr/share/tomcat -r -s /bin/false -g tomcat tomcat
+yum install -y httpd
+yum install -y tomcat 
+yum install -y tomcat-webapps tomcat-admin-webapps 
+yum install -y gcc 
+yum install -y gcc-c++ 
+yum install -y httpd-devel 
+yum install -y make 
+wget http://archive.apache.org/dist/tomcat/tomcat-connectors/jk/tomcat-connectors-1.2.41-src.tar.gz 
+tar xvfz tomcat-connectors-1.2.41-src.tar.gz 
+$PWD/tomcat-connectors-1.2.41-src/native/configure --with-apxs=/usr/bin/apxs 
+make -C $PWD/tomcat-connectors-1.2.41-src/native 
+make install -C $PWD/tomcat-connectors-1.2.41-src/native 
+groupadd --system tomcat
+useradd -d /usr/share/tomcat -r -s /bin/false -g tomcat tomcat
 
 # Create a mod_jk config file
 
@@ -36,7 +36,7 @@ JkOptions +ForwardKeySize +ForwardURICompat -ForwardDirectories
 JkRequestLogFormat \"%w %V %T\"
 # Mount your applications
 JkMount /* worker1
-JkShmFile /var/run/mod_jk/jk-runtime-status" | sudo tee /etc/httpd/conf/mod_jk.conf
+JkShmFile /var/run/mod_jk/jk-runtime-status" | tee /etc/httpd/conf/mod_jk.conf
 
 # Create mod_jk workers file
 echo "\
@@ -49,53 +49,53 @@ worker.worker1.socket_keepalive=true
 worker.worker1.lbfactor=1
 worker.worker1.connection_pool_size=50
 worker.worker1.connect_timeout=5000
-worker.worker1.prepost_timeout=5000" | sudo tee /etc/httpd/conf/workers.properties
+worker.worker1.prepost_timeout=5000" | tee /etc/httpd/conf/workers.properties
 
 # Update httpd conf file with server name and mod_jk config file name
-sudo cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/ORIG_httpd.conf
-sudo sed -i 's,#ServerName,ServerName,g' /etc/httpd/conf/httpd.conf
-sudo sed -i "s,www.example.com:80,`hostname`:80,g" /etc/httpd/conf/httpd.conf
+cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/ORIG_httpd.conf
+sed -i 's,#ServerName,ServerName,g' /etc/httpd/conf/httpd.conf
+sed -i "s,www.example.com:80,`hostname`:80,g" /etc/httpd/conf/httpd.conf
 echo "\
 # Include mod_jk's specific configuration file
-Include conf/mod_jk.conf" | sudo tee -a /etc/httpd/conf/httpd.conf
+Include conf/mod_jk.conf" | tee -a /etc/httpd/conf/httpd.conf
 
 # Update the server.xml file to specify the mod_jk worker
-sudo cp /usr/share/tomcat/conf/server.xml /usr/share/tomcat/conf/ORIG_server.xml
-sudo sed -i 's,"localhost">,"localhost" jvmRoute="worker1">,g' /usr/share/tomcat/conf/server.xml
+cp /usr/share/tomcat/conf/server.xml /usr/share/tomcat/conf/ORIG_server.xml
+sed -i 's,"localhost">,"localhost" jvmRoute="worker1">,g' /usr/share/tomcat/conf/server.xml
 
 # Update the permissions on the Tomcat webapps and install directory
-sudo chown -R tomcat:tomcat /var/lib/tomcat/webapps
-sudo chown tomcat:tomcat /usr/share/tomcat
-sudo chown tomcat:tomcat /var/lib/tomcat
+chown -R tomcat:tomcat /var/lib/tomcat/webapps
+chown tomcat:tomcat /usr/share/tomcat
+chown tomcat:tomcat /var/lib/tomcat
 
 # Set the default umask for Tomcat
-sudo cp /usr/libexec/tomcat/server /usr/libexec/tomcat/ORIG_server
-sudo sed -i 's,run start,umask 002\n  run start,g' /usr/libexec/tomcat/server
-sudo systemctl daemon-reload 
+cp /usr/libexec/tomcat/server /usr/libexec/tomcat/ORIG_server
+sed -i 's,run start,umask 002\n  run start,g' /usr/libexec/tomcat/server
+systemctl daemon-reload 
 
 # Configure SELinux to allow mod_jk to work
-sudo yum install -y policycoreutils-python 
-sudo mkdir /var/run/mod_jk
-sudo /usr/sbin/semanage fcontext -a -t httpd_var_run_t "/var/run/mod_jk(/.*)?" 
+yum install -y policycoreutils-python 
+mkdir /var/run/mod_jk
+/usr/sbin/semanage fcontext -a -t httpd_var_run_t "/var/run/mod_jk(/.*)?" 
 
 # Remove unnecessary http modules that create warnings
-sudo cp /etc/httpd/conf.modules.d/00-proxy.conf /etc/httpd/conf.modules.d/ORIG_00-proxy.conf
-sudo sed -i 's,LoadModule lbmethod_heartbeat,# LoadModule lbmethod_heartbeat,g' /etc/httpd/conf.modules.d/00-proxy.conf
+cp /etc/httpd/conf.modules.d/00-proxy.conf /etc/httpd/conf.modules.d/ORIG_00-proxy.conf
+sed -i 's,LoadModule lbmethod_heartbeat,# LoadModule lbmethod_heartbeat,g' /etc/httpd/conf.modules.d/00-proxy.conf
 
 #Configure the system to run httpd and tomcat every time the server is booted:
-sudo systemctl enable httpd  
-sudo systemctl enable tomcat  
+systemctl enable httpd  
+systemctl enable tomcat  
 
 # Restart the Tomcat7 and Apache2 servers:
-sudo service httpd start  
-sudo service tomcat start  
+service httpd start  
+service tomcat start  
 
 # Open Red Hat software firewall for port 80:
-sudo firewall-cmd --zone=public --add-port=80/tcp --permanent  
-sudo firewall-cmd --reload  
+firewall-cmd --zone=public --add-port=80/tcp --permanent  
+firewall-cmd --reload  
 
 # Setup permissions for the Tomcat manager
-sudo mv /etc/tomcat/tomcat-users.xml /etc/tomcat/ORIG_tomcat-users.xml
+mv /etc/tomcat/tomcat-users.xml /etc/tomcat/ORIG_tomcat-users.xml
 
 echo "\
 <?xml version='1.0' encoding='utf-8'?>
@@ -107,63 +107,63 @@ echo "\
 <role rolename=\"admin-gui\"/>
 <user username=\"tomcat\" password=\"tomcat\" roles=\"tomcat\"/>
 <user username=\"$2\" password=\"$3\" roles=\"tomcat,manager-script,manager-gui,admin-gui\"/>
-</tomcat-users>" | sudo tee -a /tmp/tomcat-users.xml
+</tomcat-users>" | tee -a /tmp/tomcat-users.xml
 
-sudo mv /tmp/tomcat-users.xml /etc/tomcat
-sudo chown root:tomcat /etc/tomcat/tomcat-users.xml
-sudo chmod 0640 /etc/tomcat/tomcat-users.xml
+mv /tmp/tomcat-users.xml /etc/tomcat
+chown root:tomcat /etc/tomcat/tomcat-users.xml
+chmod 0640 /etc/tomcat/tomcat-users.xml
 
 # Restart httpd and tomcat servers
-sudo service tomcat restart 
-sudo service httpd restart 
+service tomcat restart 
+service httpd restart 
 
 # Update SSHd config to not use passwords and set default umask to be 002
-sudo cp /etc/ssh/sshd_config /etc/ssh/ORIG_sshd_config
-sudo sed -i 's,PasswordAuthentication yes,PasswordAuthentication no,g' /etc/ssh/sshd_config
-echo "Match User "$1 | sudo tee -a /etc/ssh/sshd_config
-echo "    ForceCommand internal-sftp -u 002" | sudo tee -a /etc/ssh/sshd_config
+cp /etc/ssh/sshd_config /etc/ssh/ORIG_sshd_config
+sed -i 's,PasswordAuthentication yes,PasswordAuthentication no,g' /etc/ssh/sshd_config
+echo "Match User "$1 | tee -a /etc/ssh/sshd_config
+echo "    ForceCommand internal-sftp -u 002" | tee -a /etc/ssh/sshd_config
 
 # Change group of user to same as Tomcat 
-sudo gpasswd -d $1 $1 
-sudo gpasswd -a $1 tomcat 
-sudo usermod -g tomcat $1 
+gpasswd -d $1 $1 
+gpasswd -a $1 tomcat 
+usermod -g tomcat $1 
 
 
 # Configure the default umask for SSH to enable RW for user and group
-sudo cp /etc/pam.d/sshd /etc/pam.d/ORIG_sshd
-echo "session optional pam_umask.so umask=002" | sudo tee -a /etc/pam.d/sshd
+cp /etc/pam.d/sshd /etc/pam.d/ORIG_sshd
+echo "session optional pam_umask.so umask=002" | tee -a /etc/pam.d/sshd
 
 # Then start the SSH daemon:
-sudo systemctl daemon-reload 
-sudo systemctl start sshd.service 
-sudo systemctl enable sshd.service 
+systemctl daemon-reload 
+systemctl start sshd.service 
+systemctl enable sshd.service 
 
 # Open Red Hat software firewall for port 22:
-sudo firewall-cmd --zone=public --add-port=22/tcp --permanent 
-sudo firewall-cmd --reload 
+firewall-cmd --zone=public --add-port=22/tcp --permanent 
+firewall-cmd --reload 
 
 # Create an RSA public and private key for SSH
-sudo mkdir /home/$1/.ssh
-sudo ssh-keygen -q -N $4 -f /home/$1/.ssh/id_rsa 
-sudo cp /home/$1/.ssh/id_rsa.pub authorized_keys
-sudo chown -R $1:tomcat /home/$1/.ssh
-echo "SSH User name:  "$1 | sudo tee /home/$1/vsts_ssh_info
-echo "SSH passphrase: "$4 | sudo tee /home/$1/vsts_ssh_info
-echo "SSH Private key:" | sudo tee /home/$1/vsts_ssh_info
-cat id_rsa | sudo tee /home/$1/vsts_ssh_info
-sudo chown $1:tomcat /home/$1/vsts_ssh_info
+mkdir /home/$1/.ssh
+ssh-keygen -q -N $4 -f /home/$1/.ssh/id_rsa 
+cp /home/$1/.ssh/id_rsa.pub authorized_keys
+chown -R $1:tomcat /home/$1/.ssh
+echo "SSH User name:  "$1 | tee /home/$1/vsts_ssh_info
+echo "SSH passphrase: "$4 | tee /home/$1/vsts_ssh_info
+echo "SSH Private key:" | tee /home/$1/vsts_ssh_info
+cat id_rsa | tee /home/$1/vsts_ssh_info
+chown $1:tomcat /home/$1/vsts_ssh_info
 
 
 echo "Installing and Configuring FTP" 
 
 # Install the vsftp package (i.e. FTP) and create some needed directories:
-sudo yum install -y vsftpd 
-sudo mkdir /var/run/vsftpd
-sudo mkdir /var/run/vsftpd/empty
+yum install -y vsftpd 
+mkdir /var/run/vsftpd
+mkdir /var/run/vsftpd/empty
 
 
 # Generate an SSL self-signed certificate:
-sudo mkdir /etc/ssl/private
+mkdir /etc/ssl/private
 # Generate SSL self-signed certificate
 echo "\
 US
@@ -172,13 +172,13 @@ Raleigh
 Microsoft
 Team Services
 $1
-$1@" | sudo tee /tmp/info.txt
+$1@" | tee /tmp/info.txt
 
-cat /tmp/info.txt | sudo openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem
-sudo rm -f /tmp/info.txt
+cat /tmp/info.txt | openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem
+rm -f /tmp/info.txt
 
 # Backup original and create a new vsftp conf file
-sudo cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/ORIG_vsftpd.conf
+cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/ORIG_vsftpd.conf
 echo "\
 listen=NO
 listen_ipv6=YES
@@ -210,30 +210,30 @@ debug_ssl=YES
 pasv_enable=YES
 pasv_address=`wget http://ipinfo.io/ip -qO -`
 pasv_min_port=13450
-pasv_max_port=13454" | sudo tee /tmp/vsftpd.conf
+pasv_max_port=13454" | tee /tmp/vsftpd.conf
 
-sudo mv /tmp/vsftpd.conf /etc/vsftpd
-sudo chmod 0600 /etc/vsftpd/vsftpd.conf
+mv /tmp/vsftpd.conf /etc/vsftpd
+chmod 0600 /etc/vsftpd/vsftpd.conf
 
 # Configure SELinux to use Linux ACL's for file protection
-sudo setsebool -P allow_ftpd_full_access 1 
+setsebool -P allow_ftpd_full_access 1 
 
 # Open the ftp ports on the Red Hat software firewall:
-sudo firewall-cmd --zone=public --add-port=21/tcp --permanent 
-sudo firewall-cmd --zone=public --add-port=13450/tcp --permanent 
-sudo firewall-cmd --zone=public --add-port=13451/tcp --permanent 
-sudo firewall-cmd --zone=public --add-port=13452/tcp --permanent 
-sudo firewall-cmd --zone=public --add-port=13453/tcp --permanent 
-sudo firewall-cmd --zone=public --add-port=13454/tcp --permanent 
-sudo firewall-cmd --reload 
+firewall-cmd --zone=public --add-port=21/tcp --permanent 
+firewall-cmd --zone=public --add-port=13450/tcp --permanent 
+firewall-cmd --zone=public --add-port=13451/tcp --permanent 
+firewall-cmd --zone=public --add-port=13452/tcp --permanent 
+firewall-cmd --zone=public --add-port=13453/tcp --permanent 
+firewall-cmd --zone=public --add-port=13454/tcp --permanent 
+firewall-cmd --reload 
 
 # Seeing a race condition timing error so sleep to deplay
 sleep 20
 
 # Restart the ftp service:
-sudo service vsftpd restart 
-sudo systemctl enable vsftpd 
+service vsftpd restart 
+systemctl enable vsftpd 
 
-sudo chown $1:tomcat /home/$1/install.progress.txt
+chown $1:tomcat /home/$1/install.progress.txt
 chown $1:tomcat /home/$1/install.out.txt
 
