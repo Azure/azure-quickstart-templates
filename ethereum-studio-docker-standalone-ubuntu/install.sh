b27324072c52fc38c10832dc9ce0799c9f49eaa5
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-17 21:55:05 +0300

    fixing docker

--- ethereum-studio-docker-standalone-ubuntu/install.sh
+++ ethereum-studio-docker-standalone-ubuntu/install.sh
@@ -8,6 +8,4 @@ sudo apt-get install -y docker.io
 sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
 sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
 
-docker pull ethercamp/ide
-
-docker run ethercamp/ide --env MODE=standalone -p 80:8181 -p 8555:8555 -p 8080:8080
+docker run --env MODE=standalone -p 80:8181 -p 8555:8555 -p 8080:8080 ethercamp/ide
