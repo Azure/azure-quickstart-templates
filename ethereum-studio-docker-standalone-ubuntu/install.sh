b2a65ec2208d6ec8155de8813386f1dcde59273e
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-17 23:35:39 +0300

    installing docker

--- ethereum-studio-docker-standalone-ubuntu/install.sh
+++ ethereum-studio-docker-standalone-ubuntu/install.sh
@@ -2,10 +2,11 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt-get -y update
-sudo apt-get install -y docker.io
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
-sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
+# retrieve latest package updates
+apt-get update
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get install -y docker-ce
 
-docker run --env MODE=standalone -p 80:8181 -p 8555:8555 -p 8080:8080 ethercamp/ide
+docker run -d --env MODE=standalone -p 80:8181 -p 8555:8555 -p 8080:8080 ethercamp/ide
