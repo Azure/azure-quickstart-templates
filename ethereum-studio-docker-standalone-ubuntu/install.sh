fbfbbe4eeda8981dcd087604176a1b1f35eea752
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-18 01:12:08 +0300

    stilll trying to install docker

--- ethereum-studio-docker-standalone-ubuntu/install.sh
+++ ethereum-studio-docker-standalone-ubuntu/install.sh
@@ -9,10 +9,4 @@ apt-get update
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get install -y docker-ce
 
-# install dependencies
-apt-get install -y linux-image-extra-$(uname -r) libsqlite3-dev
-
-# configure Docker for AUFS
-sed -i 's/^#DOCKER_OPTS.*/DOCKER_OPTS="-s aufs"/' /etc/default/docker
-
 docker run -d --env MODE=standalone -p 80:8181 -p 8555:8555 -p 8080:8080 ethercamp/ide
