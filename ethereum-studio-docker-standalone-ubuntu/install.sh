6688fa5dd8799929376dbe87348f6dbff45600d2
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-21 23:30:12 +0300

    trying to fix docker install

--- ethereum-studio-docker-standalone-ubuntu/install.sh
+++ ethereum-studio-docker-standalone-ubuntu/install.sh
@@ -2,21 +2,19 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+apt-get update
+
+apt-get -y install \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# retrieve latest package updates
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
 apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get -y install docker-ce
 
 docker run -d --env MODE=standalone -p 80:8181 -p 8555:8555 -p 8080:8080 ethercamp/ide
