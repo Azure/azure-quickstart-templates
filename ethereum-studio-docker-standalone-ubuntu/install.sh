9885e6a8c5b2561efc38a6d7816180c8413af6c3
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-21 23:14:50 +0300

    trying different docker install procedure

--- ethereum-studio-docker-standalone-ubuntu/install.sh
+++ ethereum-studio-docker-standalone-ubuntu/install.sh
@@ -6,7 +6,17 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # retrieve latest package updates
 apt-get update
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get install -y docker-ce
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 docker run -d --env MODE=standalone -p 80:8181 -p 8555:8555 -p 8080:8080 ethercamp/ide
