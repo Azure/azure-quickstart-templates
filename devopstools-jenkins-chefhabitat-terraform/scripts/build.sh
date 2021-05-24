132c91a2923c2aea901edffa29f3528a6e1b5f23
olga-spektra <ssi_okuzina@qbsol.com>
2021-05-24 23:32:33 +0300

    fixing scripts

--- devopstools-jenkins-chefhabitat-terraform/scripts/build.sh
+++ devopstools-jenkins-chefhabitat-terraform/scripts/build.sh
@@ -8,18 +8,18 @@ apt-get update
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update
-apt-get -y install ca-certificates curl apt-transport-https lsb-release gnupg
+sudo pt-get update
+sudo apt-get -y install ca-certificates curl apt-transport-https lsb-release gnupg
 
-curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
 
 AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | tee /etc/apt/sources.list.d/azure-cli.list
-apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
 
-apt get update
+sudo apt get update
 
-apt-get -y install azure-cli openjdk-8-jdk
+sudo apt-get -y install azure-cli openjdk-8-jdk
 
 addgroup hab
 useradd -g hab hab
