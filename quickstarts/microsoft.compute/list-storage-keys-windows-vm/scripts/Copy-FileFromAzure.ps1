138c09e161447344f418941801164f94d1891396
Vamshi Krishna Badri <vamsi.badri@spektrasystems.com>
2021-07-08 20:50:17 +0530

    Update Copy-FileFromAzure.ps1

--- quickstarts/microsoft.compute/list-storage-keys-windows-vm/scripts/Copy-FileFromAzure.ps1
+++ quickstarts/microsoft.compute/list-storage-keys-windows-vm/scripts/Copy-FileFromAzure.ps1
@@ -6,6 +6,6 @@ param (
 )
 
 $source = $artifactsLocation + "\$folderName\$fileToInstall" + $artifactsLocationSasToken
-$dest = "C:\WindowsAzure\$folderName"
+$dest = "C:\WindowsAzure\$folderName\"
 New-Item -Path $dest -ItemType directory
 Invoke-WebRequest $source -OutFile "$dest\$fileToInstall"
