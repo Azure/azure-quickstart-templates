eb64f633139cf547fec8b3418e9962fe298bc0b8
Akhil Thomas <akhilthomas011@gmail.com>
2021-07-19 13:52:27 +0530

    Accomodate trailing slash in _artifactsLocation

--- quickstarts/microsoft.compute/list-storage-keys-windows-vm/scripts/Copy-FileFromAzure.ps1
+++ quickstarts/microsoft.compute/list-storage-keys-windows-vm/scripts/Copy-FileFromAzure.ps1
@@ -5,7 +5,7 @@ param (
     [string]$fileToInstall
 )
 
-$source = $artifactsLocation + "\$folderName\$fileToInstall" + $artifactsLocationSasToken
+$source = "{0}{1}\{2}\{3}" -f $artifactsLocation,$folderName,$fileToInstall,$artifactsLocationSasToken
 $dest = "C:\WindowsAzure\scripts"
 New-Item -Path $dest -ItemType "directory" -Force
 Invoke-WebRequest $source -OutFile "$dest\$fileToInstall"
