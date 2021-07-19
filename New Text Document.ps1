87bba8bbc1143fcfaca741e8bc6ee349a3f77a0b
MadhanRajAnandarao <86705318+MadhanRajAnandarao@users.noreply.github.com>
2021-07-19 21:00:27 +0530

    Add files via upload

---
+++ New Text Document.ps1
@@ -0,0 +1,10 @@
+
+
+#$invocation = $MyInvocation.MyCommand.Path
+#$directorypath = Split-Path $invocation
+#$templateFilePath=$directorypath+"\..\.m2/deploy.Json"
+$templateFilePath="C:\Users\s9zcbg\.m2\deploy.Json"
+
+
+
+New-AzureRmResourceGroupDeployment -Name "servicealert" -ResourceGroupName "madhantest" -TemplateFile $templateFilePath
