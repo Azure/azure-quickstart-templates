8b36b8c655eaff59e1df632bac98ed0266bdcde2
Stephen Weatherford <stephen.weatherford@microsoft.com>
2021-07-21 14:17:17 -0700

    tests not running

--- test/ci-tests/Validate-DeploymentFile.Tests.ps1
+++ test/ci-tests/Validate-DeploymentFile.Tests.ps1
@@ -3,6 +3,8 @@
         $ErrorActionPreference = 'Stop'
         $dataFolder = "$(Split-Path $PSCommandPath -Parent)/data/validate-deploymentfile-tests"
 
+        Import-Module "$(Split-Path $PSCommandPath -Parent)/../ci-scripts/Local.psm1" -Force
+        
         function Validate-DeploymentFile(
             [string][Parameter(Mandatory = $true)]$SampleFolder,
             [string][Parameter(Mandatory = $true)]$TemplateFileName,
@@ -26,47 +28,46 @@
             $vars = Find-VarsFromWriteHostOutput $buildHostOutput
             $resultBicepBuild = $vars["RESULT_BICEP_BUILD"]
 
-            $resultBicepBuild
-            -SampleFolder $folder `
-                -TemplateFileName "main.bicep"
             $resultBicepBuild | Should -Be "PASS"
         }
+    }
 
-        It 'gives FAIL status if bicep has errors' {
-            $folder = "$dataFolder/bicep-error"
-            $resultBicepBuild = Validate-DeploymentFile `
-                -SampleFolder $folder `
-                -TemplateFileName "main.bicep"
-            $resultBicepBuild | Should -Be "FAIL"
-        }
+    It 'gives FAIL status if bicep has errors' {
+        $folder = "$dataFolder/bicep-error"
+        $resultBicepBuild = Validate-DeploymentFile `
+            -SampleFolder $folder `
+            -TemplateFileName "main.bicep"
+        $resultBicepBuild | Should -Be "FAIL"
+    }
 
-        It 'gives FAIL status if bicep has linter warnings' {
-            $folder = "$dataFolder/bicep-linter-warnings"
-            $resultBicepBuild = Validate-DeploymentFile `
-                -SampleFolder $folder `
-                -TemplateFileName "main.bicep"
-            $resultBicepBuild | Should -Be "FAIL"
-        }
+    It 'gives FAIL status if bicep has linter warnings' {
+        $folder = "$dataFolder/bicep-linter-warnings"
+        $resultBicepBuild = Validate-DeploymentFile `
+            -SampleFolder $folder `
+            -TemplateFileName "main.bicep"
+        $resultBicepBuild | Should -Be "FAIL"
+    }
 
-        It 'gives FAIL status if bicep has compiler warnings' {
-            $folder = "$dataFolder/bicep-compiler-warnings"
-            $resultBicepBuild = Validate-DeploymentFile `
-                -SampleFolder $folder `
-                -TemplateFileName "main.bicep"
-            $resultBicepBuild | Should -Be "FAIL"
-        }
+    It 'gives FAIL status if bicep has compiler warnings' {
+        $folder = "$dataFolder/bicep-compiler-warnings"
+        $resultBicepBuild = Validate-DeploymentFile `
+            -SampleFolder $folder `
+            -TemplateFileName "main.bicep"
+        $resultBicepBuild | Should -Be "FAIL"
+    }
 
-        It 'gives empty status if not bicep' {
-            $folder = "$dataFolder/json-success"
-            $resultBicepBuild = Validate-DeploymentFile `
-                -SampleFolder $folder `
-                -TemplateFileName "azuredeploy.json"
-            $resultBicepBuild | Should -Be $null
-        }
-    }       $folder = "$dataFolder/json-success"
+    It 'gives empty status if not bicep' {
+        $folder = "$dataFolder/json-success"
         $resultBicepBuild = Validate-DeploymentFile `
             -SampleFolder $folder `
             -TemplateFileName "azuredeploy.json"
         $resultBicepBuild | Should -Be $null
+    }
+    It 'gives success status if no errors' {
+        $folder = "$dataFolder/bicep-success"
+        $resultBicepBuild = Validate-DeploymentFile `
+            -SampleFolder $folder `
+            -TemplateFileName "azuredeploy.json"
+        $resultBicepBuild | Should -Be "PASS"
     }
 }
