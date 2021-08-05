bee94c94330081ad72cf0dfe6542c6ff5dc7633c
guptar <guptar@microsoft.com>
2021-08-04 18:12:11 -0700

    Fix new line escaping

--- application-workloads/django/sqldb-django-on-ubuntu/install_django.sh
+++ application-workloads/django/sqldb-django-on-ubuntu/install_django.sh
@@ -53,13 +53,13 @@ def home(request):
     conn.commit()
     cursor.execute('SELECT * FROM votes')
 
-    result = '<table style=\"width:100%\">\n'
+    result = '<table style=\"width:100%\">\\n'
     row = cursor.fetchone()
     while row:
-        result += f'<tr>\n<th>{row[0]}: {row[1]} votes</th>\n</tr>\n'
+        result += f'<tr>\\n<th>{row[0]}: {row[1]} votes</th>\\n</tr>\\n'
         row = cursor.fetchone()
     result += '</table>'
-    html = f'<html>\n<body>\n{result}\n</body>\n<html>'
+    html = f'<html>\\n<body>\\n{result}\\n</body>\\n<html>'
     return HttpResponse(html)" | tee /var/www/helloworld/helloworld/views.py
 
 # Update urls.py
