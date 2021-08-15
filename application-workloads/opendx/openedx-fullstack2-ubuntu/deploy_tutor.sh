286bc2fb571c5b8232e44ffe86191cc81cc5b239
CeciFan <38756147+CeciFan@users.noreply.github.com>
2021-08-15 17:36:08 +0100

    Delete application-workloads/opendx/openedx-fullstack2-ubuntu directory

--- application-workloads/opendx/openedx-fullstack2-ubuntu/deploy_tutor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "deploy tutor"
-#configure tutor
-tutor local quickstart
-
-echo "create admin user, can log in with edx@example.com"
-tutor local createuser --staff --superuser admin edx@example.com
-
-
