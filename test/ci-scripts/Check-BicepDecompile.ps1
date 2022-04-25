dcf50fd890c213e428ad95069af922081a8a7703
Brian Moore (Microsoft) <bmoore@microsoft.com>
2022-04-25 11:25:55 -0500

    change warning to error

--- test/ci-scripts/Check-BicepDecompile.ps1
+++ test/ci-scripts/Check-BicepDecompile.ps1
@@ -18,7 +18,7 @@ foreach ($f in $bicepFiles) {
     # check for use of _var, _resource, _param - raw output from decompile
     $bicepText | Select-String -Pattern "resource \w{1,}_resource | \w{1,}_var | \w{1,}_param | \w{1,}_id" -AllMatches |
     foreach-object { $_.Matches } | foreach-object {
-        Write-Warning "$($f.Name) may contain raw output from decompile, please clean up: $($_.Value)"
+        Write-Error "$($f.Name) may contain raw output from decompile, please clean up: $($_.Value)"
         # write the environment var
         Write-Host "##vso[task.setvariable variable=label.decompile.clean-up.needed]$true"
     }
