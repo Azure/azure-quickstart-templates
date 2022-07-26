f31f381d4f8f2de6076acb6077032ed50e410390
Daniel Ciborowski <dciborow@microsoft.com>
2022-07-26 18:01:36 -0400

    Update container_deploy.sh

--- quickstarts/microsoft.containerregistry/container-registry-hidden-marketplace/scripts/container_deploy.sh
+++ quickstarts/microsoft.containerregistry/container-registry-hidden-marketplace/scripts/container_deploy.sh
@@ -1,9 +1,3 @@
-SUBSCRIPTION_ID='edf507a2-6235-46c5-b560-fd463ba2e771'
-PUBLISHER='microsoftcorporation1590077852919'
-OFFER='horde-storage-container-preview'
-PLAN='storage-container-test'
-CONFIG_GUID='1dedfbed-4caa-42e8-bc0c-4e7d77707117'
-
 AGREEMENT_URL="https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/providers/Microsoft.Marketplace/offerTypes/microsoft-container/publishers/$PUBLISHER/offers/$OFFER/plans/$PLAN/agreements/current"
 CONFIG_URL="https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/providers/Microsoft.Marketplace/offerTypes/microsoft-container/publishers/$PUBLISHER/offers/$OFFER/plans/$PLAN/configs/config$CONFIG_GUID"
 IMPORT_URL="https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/providers/Microsoft.Marketplace/offerTypes/microsoft-container/publishers/$PUBLISHER/offers/$OFFER/plans/$PLAN/configs/config$CONFIG_GUID/importImage"
