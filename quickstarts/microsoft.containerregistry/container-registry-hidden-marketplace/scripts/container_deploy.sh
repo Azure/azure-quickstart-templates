d8676a227d4b02415043df9327a2062f808543d8
Daniel Ciborowski <dciborow@microsoft.com>
2022-08-02 19:19:24 -0400

    Update container_deploy.sh

--- quickstarts/microsoft.containerregistry/container-registry-hidden-marketplace/scripts/container_deploy.sh
+++ quickstarts/microsoft.containerregistry/container-registry-hidden-marketplace/scripts/container_deploy.sh
@@ -3,12 +3,10 @@ CONFIG_URL="https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/provider
 IMPORT_URL="https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/providers/Microsoft.Marketplace/offerTypes/microsoft-container/publishers/$PUBLISHER/offers/$OFFER/plans/$PLAN/configs/config$CONFIG_GUID/importImage"
 
 az rest --uri AGREEMENT_URL --method get \
-  --output-file agreement.json
-az rest --uri AGREEMENT_URL --method put \
-  --body @agreement.json
-
-az rest --uri $CONFIG_URL --method put \
-  --body "{\"targetAcr\":\"$ACR_TARGET\",\"autoUpdate\":$ACR_AUTO_UPDATE,\"resourceGroup\":\"$ACR_RG\",\"tagOrDigest\":\"$ACR_TAG\"}"
-
-az rest --uri IMPORT_URL --method put \
+  --output-file agreement.json \
+&& az rest --uri AGREEMENT_URL --method put \
+  --body @agreement.json \
+&& az rest --uri $CONFIG_URL --method put \
+  --body "{\"targetAcr\":\"$ACR_TARGET\",\"autoUpdate\":$ACR_AUTO_UPDATE,\"resourceGroup\":\"$ACR_RG\",\"tagOrDigest\":\"$ACR_TAG\"}" \
+&& az rest --uri IMPORT_URL --method put \
   --body "{\"registryResourceId\":\"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$ACR_RG/providers/Microsoft.ContainerRegistry/registries/$ACR_TARGET\"}"
