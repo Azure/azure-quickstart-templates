11d5726eea73d2103e9be914c9293a2a5f6af322
azure-quickstart-templates pipeline <azure-quickstart-templates@noreply.github.com>
2022-08-26 16:24:16 -0500

    update dsc

--- application-workloads/active-directory/active-directory-new-domain-ha-2-dc/DSC/CreateADPDC.ps1
+++ application-workloads/active-directory/active-directory-new-domain-ha-2-dc/DSC/CreateADPDC.ps1
@@ -1,59 +1,55 @@
-configuration CreateADPDC
-{
-   param
-   (
+configuration CreateADPDC 
+{ 
+    param 
+    ( 
         [Parameter(Mandatory)]
         [String]$DomainName,
 
         [Parameter(Mandatory)]
         [System.Management.Automation.PSCredential]$Admincreds,
 
-        [Int]$RetryCount=20,
-        [Int]$RetryIntervalSec=30
-    )
-
+        [Int]$RetryCount = 20,
+        [Int]$RetryIntervalSec = 30
+    ) 
+    
     Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking, PSDesiredStateConfiguration, xPendingReboot
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
-    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
-    $InterfaceAlias=$($Interface.Name)
+    $Interface = Get-NetAdapter | Where Name -Like "Ethernet*" | Select-Object -First 1
+    $InterfaceAlias = $($Interface.Name)
 
     Node localhost
     {
-        LocalConfigurationManager
-        {
+        LocalConfigurationManager {
             RebootNodeIfNeeded = $true
         }
 
-        WindowsFeature DNS
-        {
-            Ensure = "Present"
-            Name = "DNS"
+        WindowsFeature DNS { 
+            Ensure = "Present" 
+            Name   = "DNS"		
         }
 
-        Script EnableDNSDiags
-        {
-      	    SetScript = {
+        Script EnableDNSDiags {
+            SetScript  = { 
                 Set-DnsServerDiagnostics -All $true
-                Write-Verbose -Verbose "Enabling DNS client diagnostics"
+                Write-Verbose -Verbose "Enabling DNS client diagnostics" 
             }
-            GetScript =  { @{} }
+            GetScript  = { @{} }
             TestScript = { $false }
+            DependsOn  = "[WindowsFeature]DNS"
+        }
+
+        WindowsFeature DnsTools {
+            Ensure    = "Present"
+            Name      = "RSAT-DNS-Server"
             DependsOn = "[WindowsFeature]DNS"
         }
 
-        WindowsFeature DnsTools
-        {
-            Ensure = "Present"
-            Name = "RSAT-DNS-Server"
-            DependsOn = "[WindowsFeature]DNS"
-        }
-
-        xDnsServerAddress DnsServerAddress
-        {
-            Address        = '127.0.0.1'
+        xDnsServerAddress DnsServerAddress 
+        { 
+            Address        = '127.0.0.1' 
             InterfaceAlias = $InterfaceAlias
             AddressFamily  = 'IPv4'
-            DependsOn = "[WindowsFeature]DNS"
+            DependsOn      = "[WindowsFeature]DNS"
         }
 
         xWaitforDisk Disk2
@@ -64,47 +60,39 @@ configuration CreateADPDC
         }
 
         xDisk ADDataDisk {
-            DiskNumber = 2
+            DiskNumber  = 2
             DriveLetter = "F"
-            DependsOn = "[xWaitForDisk]Disk2"
+            DependsOn   = "[xWaitForDisk]Disk2"
         }
 
-        WindowsFeature ADDSInstall
-        {
-            Ensure = "Present"
-            Name = "AD-Domain-Services"
-            DependsOn="[WindowsFeature]DNS"
-        }
+        WindowsFeature ADDSInstall { 
+            Ensure    = "Present" 
+            Name      = "AD-Domain-Services"
+            DependsOn = "[WindowsFeature]DNS" 
+        } 
 
-        WindowsFeature ADDSTools
-        {
-            Ensure = "Present"
-            Name = "RSAT-ADDS-Tools"
+        WindowsFeature ADDSTools {
+            Ensure    = "Present"
+            Name      = "RSAT-ADDS-Tools"
             DependsOn = "[WindowsFeature]ADDSInstall"
         }
 
-        WindowsFeature ADAdminCenter
-        {
-            Ensure = "Present"
-            Name = "RSAT-AD-AdminCenter"
-            DependsOn = "[WindowsFeature]ADDSTools"
+        WindowsFeature ADAdminCenter {
+            Ensure    = "Present"
+            Name      = "RSAT-AD-AdminCenter"
+            DependsOn = "[WindowsFeature]ADDSInstall"
         }
-
-        xADDomain FirstDS
+         
+        xADDomain FirstDS 
         {
-            DomainName = $DomainName
+            DomainName                    = $DomainName
             DomainAdministratorCredential = $DomainCreds
             SafemodeAdministratorPassword = $DomainCreds
-            DatabasePath = "F:\NTDS"
-            LogPath = "F:\NTDS"
-            SysvolPath = "F:\SYSVOL"
-            DependsOn = @("[WindowsFeature]ADDSInstall", "[xDisk]ADDataDisk")
-        }
+            DatabasePath                  = "F:\NTDS"
+            LogPath                       = "F:\NTDS"
+            SysvolPath                    = "F:\SYSVOL"
+            DependsOn                     = @("[xDisk]ADDataDisk", "[WindowsFeature]ADDSInstall")
+        } 
 
-        xPendingReboot RebootAfterPromotion{
-            Name = "RebootAfterPromotion"
-            DependsOn = "[xADDomain]FirstDS"
-        }
-
-   }
-}
+    }
+} 
